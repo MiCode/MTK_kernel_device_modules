f1e7ea58f9412570ded409825b980092c6f4a2e6
yan-hong.zhang <yan-hong.zhang@mediatek.com>
2024-01-27 18:40:27 +0800

    [ALPS08606670] gpueb: mt6989: Add api for clear gpueb to apmcu irq
    Add api for clear gpueb to apmcu IRQ[1].
    
    MTK-Commit-Id: f6bdf0c5265833bddc6c9f008ffc20c4add786f7
    
    CR-Id: ALPS08606670
    Feature: GPUEB firmware
    Change-Id: I3f669c0ba50e553b6f9fb3cb708f4cbc731fef8a
    Signed-off-by: yan-hong.zhang <yan-hong.zhang@mediatek.com>

--- drivers/gpu/mediatek/gpueb/include/gpueb_ipi.h
+++ drivers/gpu/mediatek/gpueb/include/gpueb_ipi.h
@@ -28,6 +28,7 @@ int gpueb_get_send_PIN_ID_by_name(char *send_PIN_name);
 int gpueb_get_recv_PIN_ID_by_name(char *recv_PIN_name);
 void *get_gpueb_ipidev(void);
 int get_gpueb_slot_size(void);
+void gpueb_clr_mbox1_irq(void);
 
 #if IPI_TEST
 int gpueb_ipi_test_init(void);
