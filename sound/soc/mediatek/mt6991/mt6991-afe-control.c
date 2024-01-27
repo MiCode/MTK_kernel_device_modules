66baa30112397ed76a4f56a959418aec17d1f783
yujie.xiao <yujie.xiao@mediatek.com>
2024-01-27 18:42:48 +0800

    [ALPS08606670] audio: add mt6991 afe driver
    1. Initial mt6991 audio afe folder, register and Kconfig
    2. MEMIF:
       add DL26, VUL26, CM2, DL_4CH and relateive dapm routes
       update VUL24 to VUL26 out on use control,
       add DL pubf, min len, max len
    3. Interconn: update to 32bit
    4. EDTM/I2S:
       refactor edtm control flow by using variable not hard code
       add i2s in/out 3, I2S4IN 32ch, i2s6 dapm, etdm pad top control
       remove unuse struct variable
    5. IRQ: update new irq usage
    6. Config: add SKIP_SB relative setting
    7. update mtkaif control reg to VLP domain
    8. Sinegen: update sinegen reg map
    9. CLK: update clk.h address and nameing to VLP domain
    9. mtkaif: update clk pointer in mtkaif calib
    10. update audio clock to vlp and cksys
    11. merge gain table from rayas patch
    12. remove switch clk mux when copy data from leroy patch
    
    MTK-Commit-Id: 512fae3172ce99900b5c73e299b07d0420334bae
    
    Signed-off-by: yujie.xiao <yujie.xiao@mediatek.com>
    CR-Id: ALPS08606670
    Feature: alsa_sound_soc
    Change-Id: I07f6440193f0168da5884b0b8979904b514ddc3b
    (cherry-pick from 32cfe1e53853cdae46f0981dca7e6ddba70627c1)

---
+++ sound/soc/mediatek/mt6991/mt6991-afe-control.c
@@ -0,0 +1,183 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ *  MediaTek ALSA SoC Audio Control
+ *
+ *  Copyright (c) 2023 MediaTek Inc.
+ *  Author: Yujie Xiao <yujie.xiao@mediatek.com>
+ */
+
+#include "mt6991-afe-common.h"
+#include <linux/pm_runtime.h>
+
+#include "mtk-sram-manager.h"
+
+/* don't use this directly if not necessary */
+static struct mtk_base_afe *local_afe;
+
+int mt6991_set_local_afe(struct mtk_base_afe *afe)
+{
+	local_afe = afe;
+	return 0;
+}
+
+unsigned int mt6991_general_rate_transform(struct device *dev,
+		unsigned int rate)
+{
+	switch (rate) {
+	case 8000:
+		return MTK_AFE_IPM2P0_RATE_8K;
+	case 11025:
+		return MTK_AFE_IPM2P0_RATE_11K;
+	case 12000:
+		return MTK_AFE_IPM2P0_RATE_12K;
+	case 16000:
+		return MTK_AFE_IPM2P0_RATE_16K;
+	case 22050:
+		return MTK_AFE_IPM2P0_RATE_22K;
+	case 24000:
+		return MTK_AFE_IPM2P0_RATE_24K;
+	case 32000:
+		return MTK_AFE_IPM2P0_RATE_32K;
+	case 44100:
+		return MTK_AFE_IPM2P0_RATE_44K;
+	case 48000:
+		return MTK_AFE_IPM2P0_RATE_48K;
+	case 88200:
+		return MTK_AFE_IPM2P0_RATE_88K;
+	case 96000:
+		return MTK_AFE_IPM2P0_RATE_96K;
+	case 176400:
+		return MTK_AFE_IPM2P0_RATE_176K;
+	case 192000:
+		return MTK_AFE_IPM2P0_RATE_192K;
+	/* not support 260K */
+	case 352800:
+		return MTK_AFE_IPM2P0_RATE_352K;
+	case 384000:
+		return MTK_AFE_IPM2P0_RATE_384K;
+	default:
+		dev_info(dev, "%s(), rate %u invalid, use %d!!!\n",
+			 __func__,
+			 rate, MTK_AFE_IPM2P0_RATE_48K);
+		return MTK_AFE_IPM2P0_RATE_48K;
+	}
+}
+
+static unsigned int pcm_rate_transform(struct device *dev,
+				       unsigned int rate)
+{
+	switch (rate) {
+	case 8000:
+		return MTK_AFE_PCM_RATE_8K;
+	case 16000:
+		return MTK_AFE_PCM_RATE_16K;
+	case 32000:
+		return MTK_AFE_PCM_RATE_32K;
+	case 48000:
+		return MTK_AFE_PCM_RATE_48K;
+	default:
+		dev_info(dev, "%s(), rate %u invalid, use %d!!!\n",
+			 __func__,
+			 rate, MTK_AFE_PCM_RATE_32K);
+		return MTK_AFE_PCM_RATE_32K;
+	}
+}
+
+unsigned int mt6991_rate_transform(struct device *dev,
+				   unsigned int rate, int aud_blk)
+{
+	switch (aud_blk) {
+	case MT6991_DAI_PCM_0:
+	case MT6991_DAI_PCM_1:
+		return pcm_rate_transform(dev, rate);
+	default:
+		return mt6991_general_rate_transform(dev, rate);
+	}
+}
+
+int mt6991_dai_set_priv(struct mtk_base_afe *afe, int id,
+			int priv_size, const void *priv_data)
+{
+	struct mt6991_afe_private *afe_priv = afe->platform_priv;
+	void *temp_data;
+
+	temp_data = devm_kzalloc(afe->dev,
+				 priv_size,
+				 GFP_KERNEL);
+	if (!temp_data)
+		return -ENOMEM;
+
+	if (priv_data)
+		memcpy(temp_data, priv_data, priv_size);
+
+	if (id < 0 || id >= MT6991_DAI_NUM) {
+		dev_info(afe->dev, "%s(), invalid DAI id %d\n", __func__, id);
+		return -EINVAL;
+	}
+
+	afe_priv->dai_priv[id] = temp_data;
+
+	return 0;
+}
+
+/* DC compensation */
+int mt6991_enable_dc_compensation(bool enable)
+{
+	if (!local_afe)
+		return -EPERM;
+
+	return 0;
+}
+EXPORT_SYMBOL(mt6991_enable_dc_compensation);
+
+int mt6991_set_lch_dc_compensation(int value)
+{
+	if (!local_afe)
+		return -EPERM;
+
+	return 0;
+}
+EXPORT_SYMBOL(mt6991_set_lch_dc_compensation);
+
+int mt6991_set_rch_dc_compensation(int value)
+{
+	if (!local_afe)
+		return -EPERM;
+
+	return 0;
+}
+EXPORT_SYMBOL(mt6991_set_rch_dc_compensation);
+
+int mt6991_adda_dl_gain_control(bool mute)
+{
+	unsigned int dl_gain_ctl;
+
+	if (!local_afe)
+		return -EPERM;
+
+	if (mute)
+		dl_gain_ctl = MTK_AFE_ADDA_DL_GAIN_MUTE;
+	else
+		dl_gain_ctl = 0xf74ff74f;
+
+	dev_info(local_afe->dev, "%s(), adda_dl_gain %x\n",
+		 __func__, dl_gain_ctl);
+
+	return 0;
+}
+EXPORT_SYMBOL(mt6991_adda_dl_gain_control);
+
+struct audio_swpm_data mt6991_aud_get_power_scenario(void)
+{
+	struct audio_swpm_data test;
+
+	test.adda_mode = 0;
+	test.afe_on = 1;
+	test.channel_num = 4;
+	test.input_device = 0;
+	test.output_device = 2;
+	test.sample_rate = 2;
+	test.user_case = 1;
+	return test;
+}
+EXPORT_SYMBOL_GPL(mt6991_aud_get_power_scenario);
