c9a4918f62ec6985fb8fc1ad3e7aa0c0a11d3eb2
crystal.huang <crystal.huang@mediatek.com>
2024-02-19 14:59:00 +0800

    [ALPS08703810] AudioOffload: remove SKIP_SB_OFFLOAD to enable offload
    remove SKIP_SB_OFFLOAD to enable offload dai link
    
    MTK-Commit-Id: fc813ba85cc3c2802e2cb8b6d2d7b60c07fdae03
    
    Signed-off-by: crystal.huang <crystal.huang@mediatek.com>
    CR-Id: ALPS08703810
    Feature: ADSP Audio Decoder Offload
    Change-Id: I3bcf7cecdd01fa9a41d24bce4e6a80d06d15bc1c

--- sound/soc/mediatek/mt6991/mt6991-afe-common.h
+++ sound/soc/mediatek/mt6991/mt6991-afe-common.h
@@ -40,7 +40,6 @@
 #else
 /* delete define below if your feature don't want to skip */
 #define SKIP_SB_BTCVSD
-#define SKIP_SB_OFFLOAD
 #define SKIP_SB_VOW
 #define SKIP_SB_ULTRA
 #define SKIP_SB_USB_OFFLOAD
