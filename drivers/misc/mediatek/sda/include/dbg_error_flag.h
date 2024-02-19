4cff4a263c731e33960e05cfdc0913e7939998be
Alvin Zhan <alvin.zhan@mediatek.com>
2024-02-19 14:59:19 +0800

    [ALPS08556761] err flag for mt6991
    porting err flag for mt6991 on main trunk.
    
    MTK-Commit-Id: 7ce9d517188b14e3cc3a27bbb3c994d1951ed04d
    
    Signed-off-by: Alvin Zhan <alvin.zhan@mediatek.com>
    CR-Id: ALPS08556761
    Feature: dbgtop_drm
    Change-Id: If65900b1e89330c7537490aad4b43d3fa9038427

--- drivers/misc/mediatek/sda/include/dbg_error_flag.h
+++ drivers/misc/mediatek/sda/include/dbg_error_flag.h
@@ -55,6 +55,7 @@ struct DBG_ERROR_FLAG_DESC {
 };
 
 enum DBG_ERROR_FLAG_ENUM {
+	VLP_TRACE_HALT_IRQ,
 	INFRA_LASTBUS_TIMEOUT,
 	PERI_LASTBUS_TIMEOUT,
 	DRAM_MD32_WDT_EVENT_CH_A,
@@ -65,6 +66,8 @@ enum DBG_ERROR_FLAG_ENUM {
 	INFRA_SMMU_NS_IRQ,
 	AP_TRACKER_TIMEOUT,
 	INFRA_TRACKER_TIMEOUT,
+	SLOW_INFRA_TRACKER_TIMEOUT,
+	FAST_INFRA_TRACKER_TIMEOUT,
 	VLP_TRACKER_TIMEOUT,
 	MCU_TO_SOC_DFD_EVENT,
 	APU_SMMU_IRQ,
@@ -77,6 +80,7 @@ enum DBG_ERROR_FLAG_ENUM {
 	MCU2EMI_M0_PARITY,
 	MCU2INFRA_REG_PARITY,
 	INFRA_L3_CACHE2MCU_PARITY,
+	GPUEB_PARITY_FAIL,
 	EMI_PARITY_CEN,
 	EMI_PARITY_SUB_CEN,
 	EMI_PARITY_CHAN1,
@@ -87,9 +91,132 @@ enum DBG_ERROR_FLAG_ENUM {
 	DRAMC_ERROR_FLAG_CH_B,
 	DRAMC_ERROR_FLAG_CH_C,
 	DRAMC_ERROR_FLAG_CH_D,
+	SOC_DBG_ERR_FLAG_WDT_IRQ,
+	MMU_TO_SOC_DFD_TRIGGER_EVENT,
+	MFG2SOC_DFD_SMMU_TRIGGER_EVENT,
 	DBG_ERROR_FLAG_TOTAL,
 };
-extern struct DBG_ERROR_FLAG_DESC dbg_error_flag_desc[];
+
+struct DBG_ERROR_FLAG_DESC dbg_error_flag_desc[DBG_ERROR_FLAG_TOTAL] = {
+	[VLP_TRACE_HALT_IRQ] = {
+		.mask_name = "vlp-trace-halt-irq-mask",
+	},
+	[INFRA_LASTBUS_TIMEOUT] = {
+		.mask_name = "infra-lastbus-timeout-mask",
+	},
+	[PERI_LASTBUS_TIMEOUT] = {
+		.mask_name = "peri-lastbus-timeout-mask",
+	},
+	[DRAM_MD32_WDT_EVENT_CH_A] = {
+		.mask_name = "dram-md32-wdt-event-ch-a-mask",
+	},
+	[DRAM_MD32_WDT_EVENT_CH_B] = {
+		.mask_name = "dram-md32-wdt-event-ch-b-mask",
+	},
+	[DRAM_MD32_WDT_EVENT_CH_C] = {
+		.mask_name = "dram-md32-wdt-event-ch-c-mask",
+	},
+	[DRAM_MD32_WDT_EVENT_CH_D] = {
+		.mask_name = "dram-md32-wdt-event-ch-d-mask",
+	},
+	[INFRA_SMMU_IRQ] = {
+		.mask_name = "infra-smmu-irq-mask",
+	},
+	[INFRA_SMMU_NS_IRQ] = {
+		.mask_name = "infra-smmu-ns-irq-mask",
+	},
+	[AP_TRACKER_TIMEOUT] = {
+		.mask_name = "ap-tracker-timeout-mask",
+	},
+	[INFRA_TRACKER_TIMEOUT] = {
+		.mask_name = "infra-tracker-timeout-mask",
+	},
+	[VLP_TRACKER_TIMEOUT] = {
+		.mask_name = "vlp-tracker-timeout-mask",
+	},
+	[SLOW_INFRA_TRACKER_TIMEOUT] = {
+	.mask_name = "slow-infra-tracker-timeout-mask",
+	},
+	[FAST_INFRA_TRACKER_TIMEOUT] = {
+	.mask_name = "fast-infra-tracker-timeout-mask",
+	},
+	[MCU_TO_SOC_DFD_EVENT] = {
+		.mask_name = "mcu-to-soc-dfd-event-mask",
+	},
+	[APU_SMMU_IRQ] = {
+		.mask_name = "apu-smmu-irq-mask",
+	},
+	[MFG_TO_SOC_DFD_EVENT] = {
+		.mask_name = "mfg-to-soc-dfd-event-mask",
+	},
+	[MMINFRA_SMMU_IRQ] = {
+		.mask_name = "mminfra-smmu-irq-mask",
+	},
+	[MFG_TO_EMI_SLV_PARITY] = {
+		.mask_name = "mfg-to-emi-slv-parity-mask",
+	},
+	[MCU2SUB_EMI_M1_PARITY] = {
+		.mask_name = "mcu2sub-emi-m1-parity-mask",
+	},
+	[MCU2SUB_EMI_M0_PARITY] = {
+		.mask_name = "mcu2sub-emi-m0-parity-mask",
+	},
+	[MCU2EMI_M1_PARITY] = {
+		.mask_name = "mcu2emi-m1-parity-mask",
+	},
+	[MCU2EMI_M0_PARITY] = {
+		.mask_name = "mcu2emi-m0-parity-mask",
+	},
+	[MCU2INFRA_REG_PARITY] = {
+		.mask_name = "mcu2infra-reg-parity-mask",
+	},
+	[INFRA_L3_CACHE2MCU_PARITY] = {
+		.mask_name = "infra-l3-cache2mcu-parity-mask",
+	},
+	[GPUEB_PARITY_FAIL] = {
+	.mask_name = "gpueb-parity-fail-mask",
+	},
+	[EMI_PARITY_CEN] = {
+		.mask_name = "emi-parity-cen-mask",
+	},
+	[EMI_PARITY_SUB_CEN] = {
+		.mask_name = "emi-parity-sub-cen-mask",
+	},
+	[EMI_PARITY_CHAN1] = {
+		.mask_name = "emi-parity-chan1-mask",
+	},
+	[EMI_PARITY_CHAN2] = {
+		.mask_name = "emi-parity-chan2-mask",
+	},
+	[EMI_PARITY_CHAN3] = {
+		.mask_name = "emi-parity-chan3-mask",
+	},
+	[EMI_PARITY_CHAN4] = {
+		.mask_name = "emi-parity-chan4-mask",
+	},
+	[DRAMC_ERROR_FLAG_CH_A] = {
+		.mask_name = "dramc-error-flag-ch-a-mask",
+	},
+	[DRAMC_ERROR_FLAG_CH_B] = {
+		.mask_name = "dramc-error-flag-ch-b-mask",
+	},
+	[DRAMC_ERROR_FLAG_CH_C] = {
+		.mask_name = "dramc-error-flag-ch-c-mask",
+	},
+	[DRAMC_ERROR_FLAG_CH_D] = {
+		.mask_name = "dramc-error-flag-ch-d-mask",
+	},
+	[SOC_DBG_ERR_FLAG_WDT_IRQ] = {
+		.mask_name = "soc-dbg-err-flag-wdt-irq-mask",
+	},
+	[MMU_TO_SOC_DFD_TRIGGER_EVENT] = {
+		.mask_name = "mmu-to-soc-dfd-trigger-event-mask",
+	},
+	[MFG2SOC_DFD_SMMU_TRIGGER_EVENT] = {
+		.mask_name = "mfg2soc-dfd-smmu-trigger-event-mask",
+	},
+};
+
 extern void dbg_error_flag_register_notify(struct notifier_block *nb);
 extern unsigned int get_dbg_error_flag_mask(unsigned int err_flag_enum);
 #endif
