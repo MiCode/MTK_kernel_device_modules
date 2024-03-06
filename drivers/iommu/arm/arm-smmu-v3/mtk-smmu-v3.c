70b0e8c33d85d4ea0ae611eb3a47e2761a856694
mtk28254 <shenghao.lin@mediatek.com>
2024-03-06 10:13:16 +0800

    [ALPS08657587] smmu: mt6991: enable gpu smmu acp1
    Remove the flag SMMU_DIS_TCU_CH for enabling gpu smmu acp.
    
    MTK-Commit-Id: 9ace6b8b5dad633e4d304e2c42bbcea29a3d39dc
    
    CR-Id: ALPS08657587
    Feature: ARM GPU DDK driver
    Change-Id: I9e5572d91ba7a78c6aa75bebffea54c80415ca9c
    Signed-off-by: mtk28254 <shenghao.lin@mediatek.com>

--- drivers/iommu/arm/arm-smmu-v3/mtk-smmu-v3.c
+++ drivers/iommu/arm/arm-smmu-v3/mtk-smmu-v3.c
@@ -2678,8 +2678,7 @@ static const struct mtk_smmu_plat_data mt6991_data_gpu = {
 	.smmu_plat		= SMMU_MT6991,
 	.smmu_type		= GPU_SMMU,
 	.flags			= SMMU_DELAY_HW_INIT | SMMU_EXTRA_DCM_EN | SMMU_HYP_EN |
-				  SMMU_DIS_CPU_PARTID | SMMU_DIS_CPU_TBU_PARTID |
-				  SMMU_DIS_TCU_CH,
+				  SMMU_DIS_CPU_PARTID | SMMU_DIS_CPU_TBU_PARTID,
 };
 
 static const struct mtk_smmu_plat_data *of_device_get_plat_data(struct device *dev)
