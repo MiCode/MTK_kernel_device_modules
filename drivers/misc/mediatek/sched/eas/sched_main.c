3f863fa3a4f3251939186a70f2a8ee9ace82dd27
Jing-Ting Wu <Jing-Ting.Wu@mediatek.com>
2024-03-06 10:12:07 +0800

    [ALPS08534549] sched: fix hook build error
    [Detail]
    fix hook build error
    
    MTK-Commit-Id: f4748228854ab6a291a34a7d8e12dd9727e0f158
    
    CR-Id: ALPS08534549
    Feature: Energy Aware Scheduling Plus (EAS+)
    Signed-off-by: Jing-Ting Wu <Jing-Ting.Wu@mediatek.com>
    Change-Id: I337faac889067dfe71ff7d35af80493436895d1e

--- drivers/misc/mediatek/sched/eas/sched_main.c
+++ drivers/misc/mediatek/sched/eas/sched_main.c
@@ -1035,7 +1035,7 @@ static int __init mtk_scheduler_init(void)
 		pr_info("register android_rvh_after_enqueue_task failed, returned %d\n", ret);
 
 #if IS_ENABLED(CONFIG_MTK_SCHED_BIG_TASK_ROTATE)
-	//ret = register_trace_android_rvh_new_task_stats(rotat_task_stats, NULL);
+	ret = register_trace_android_rvh_new_task_stats(rotat_task_stats, NULL);
 	if (ret)
 		pr_info("register android_rvh_new_task_stats failed, returned %d\n", ret);
 
