c98c455c64f13d0a4589ddb56da2853c776435d5
yu-chen.liu <yu-chen.liu@mediatek.com>
2024-03-06 10:18:06 +0800

    [ALPS08659114] xhci: sync upstream patch 2024_02_18-20
    [Description]
    xhci: mem: Carefully calculate size for memory allocations
    
    Carefully calculate size for memory allocations, i.e. with help
    of size_mul() macro from overflow.h.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Mathias Nyman <mathias.nyman@linux.intel.com>
    Link: https://lore.kernel.org/r/20230317154715.535523-2-mathias.nyman@linux.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    [Test]
    USB Host
    
    MTK-Commit-Id: 9186e82d911cb2ab4af57db497764372b2a32f71
    
    Signed-off-by: yu-chen.liu <yu-chen.liu@mediatek.com>
    CR-Id: ALPS08659114
    Feature: usb_ssusb
    Change-Id: I47b6013d5503ced86418f18e0ff1854531ec84f4

--- drivers/misc/mediatek/usb/usb_xhci/xhci-mem.c
+++ drivers/misc/mediatek/usb/usb_xhci/xhci-mem.c
@@ -10,6 +10,7 @@
  */
 
 #include <linux/usb.h>
+#include <linux/overflow.h>
 #include <linux/pci.h>
 #include <linux/slab.h>
 #include <linux/dmapool.h>
@@ -613,7 +614,7 @@ static void xhci_free_stream_ctx(struct xhci_hcd *xhci,
 		struct xhci_stream_ctx *stream_ctx, dma_addr_t dma)
 {
 	struct device *dev = xhci_to_hcd(xhci)->self.sysdev;
-	size_t size = sizeof(struct xhci_stream_ctx) * num_stream_ctxs;
+	size_t size = size_mul(sizeof(struct xhci_stream_ctx), num_stream_ctxs);
 
 	if (size > MEDIUM_STREAM_ARRAY_SIZE)
 		dma_free_coherent(dev, size,
@@ -1745,7 +1746,7 @@ static int scratchpad_alloc(struct xhci_hcd *xhci, gfp_t flags)
 		goto fail_sp;
 
 	xhci->scratchpad->sp_array = dma_alloc_coherent(dev,
-				     num_sp * sizeof(u64),
+				     size_mul(sizeof(u64), num_sp),
 				     &xhci->scratchpad->sp_dma, flags);
 	if (!xhci->scratchpad->sp_array)
 		goto fail_sp2;
@@ -1886,7 +1887,7 @@ int xhci_alloc_erst_(struct xhci_hcd *xhci,
 	struct xhci_segment *seg;
 	struct xhci_erst_entry *entry;
 
-	size = sizeof(struct xhci_erst_entry) * evt_ring->num_segs;
+	size = size_mul(sizeof(struct xhci_erst_entry), evt_ring->num_segs);
 	erst->entries = dma_alloc_coherent(xhci_to_hcd(xhci)->self.sysdev,
 					   size, &erst->erst_dma_addr, flags);
 	if (!erst->entries)
@@ -1918,7 +1919,7 @@ xhci_free_interrupter_(struct xhci_hcd *xhci, struct xhci_interrupter *ir)
 	if (!ir)
 		return;
 
-	erst_size = sizeof(struct xhci_erst_entry) * (ir->erst.num_entries);
+	erst_size = sizeof(struct xhci_erst_entry) * ir->erst.num_entries;
 	if (ir->erst.entries)
 		dma_free_coherent(dev, erst_size,
 				  ir->erst.entries,
