1731eb8d4aa6c8eb3ed0f3f131c679699a5151a5
Wade Wu <wei-ya.wu@mediatek.com>
2024-03-06 10:11:46 +0800

    [ALPS08607110] sugov_ext: Unmark hook function cpufreq_fast_switch
    hook about cpufreq
    
    MTK-Commit-Id: 51d52d86945d353316c715829e4b56c5f9173113
    
    Signed-off-by: Wade Wu <wei-ya.wu@mediatek.com>
    CR-Id: ALPS08607110
    Feature: Energy Aware Scheduling Plus (EAS+)
    Change-Id: I8c8909faaa7fd5be15eca8ce44433d65ab776128

--- drivers/misc/mediatek/sched/eas/eas_plus.c
+++ drivers/misc/mediatek/sched/eas/eas_plus.c
@@ -795,11 +795,8 @@ void mtk_hook_after_enqueue_task(void *data, struct rq *rq,
 	sugov_data_ptr2 = &per_cpu(rq_data, this_cpu)->sugov_data;
 	if (READ_ONCE(sugov_data_ptr->enq_update_dsu_freq) == true
 			|| READ_ONCE(sugov_data_ptr2->enq_dvfs) == true) {
-		/* need upstream, EXPORT symbols
-		 * 2542493:ANDROID: android: Export symbols for invoking cpufreq_update_util()
-		 * | https://android-review.googlesource.com/c/kernel/common/+/2542493
-		 * cpufreq_update_util(rq, 0);
-		 */
+		cpufreq_update_util(rq, 0);
+
 		WRITE_ONCE(sugov_data_ptr2->enq_dvfs, false);
 	}
 	WRITE_ONCE(sugov_data_ptr->enq_ing, false);
@@ -850,7 +847,7 @@ void mtk_pelt_rt_tp(void *data, struct rq *rq)
 	if (!get_eas_hook())
 		return;
 
-	//cpufreq_update_util(rq, 0);
+	cpufreq_update_util(rq, 0);
 }
 
 void mtk_sched_switch(void *data, struct task_struct *prev,
