91c36e312efaff74bb0c804cc48b8c34b6213108
mtk28629 <chengting.tsai@mediatek.com>
2024-03-06 10:15:41 +0800

    [ALPS08558086] report error info, kernel part
    report error info, kernel part
    
    MTK-Commit-Id: fab8f3318d0b29a2266489872fb2c4344126d9e1
    
    Signed-off-by: mtk28629 <chengting.tsai@mediatek.com>
    CR-Id: ALPS08558086
    Feature: Video Driver
    Change-Id: Ib692c177652330280d9fdcd83e4ddda7b63e7ca9

--- drivers/media/platform/mtk-vcodec/mtk_vcodec_dec.h
+++ drivers/media/platform/mtk-vcodec/mtk_vcodec_dec.h
@@ -87,6 +87,7 @@ extern const struct v4l2_m2m_ops mtk_vdec_m2m_ops;
  * to ctx instance that get lock
  */
 void mtk_vdec_queue_error_event(struct mtk_vcodec_ctx *ctx);
+void mtk_vdec_queue_error_code_event(struct mtk_vcodec_ctx *ctx, unsigned int info);
 void mtk_vdec_error_handle(struct mtk_vcodec_ctx *ctx, char *debug_str);
 void mtk_vdec_unlock(struct mtk_vcodec_ctx *ctx, u32 hw_id);
 int mtk_vdec_lock(struct mtk_vcodec_ctx *ctx, u32 hw_id);
