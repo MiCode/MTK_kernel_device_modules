56402967de7c52aa955b55700aa315a45fa665d7
Peter-TY Tsai <peter-ty.tsai@mediatek.com>
2024-03-06 10:19:57 +0800

    [ALPS08664084] sched: delete redundant arch-related codes
    [Description]
    1. remove arch-related APIs AP-side backup
    2. export val_m to scheduler.ko
    
    [Test]
    k6991_k66 PDAS and stability (bitflip Analysis, monkey,
    suspend/resume, reboot)
    
    MTK-Commit-Id: 7c6e1d641c7520c59f55cb45e20bc5d9f0876c58
    
    Signed-off-by: Peter-TY Tsai <peter-ty.tsai@mediatek.com>
    CR-Id: ALPS08664084
    Feature: Energy Aware Scheduling Plus (EAS+)
    Change-Id: Iead35a8ca5ef28ed21a5be01ffb534ca7fe488db

--- drivers/misc/mediatek/sched/eas/arch.h
+++ drivers/misc/mediatek/sched/eas/arch.h
@@ -2,268 +2,13 @@
 /*
  * Copyright (c) 2021 MediaTek Inc.
  */
-#include "eas_plus.h"
 #include "sugov/cpufreq.h"
 #include "sugov/dsu_interface.h"
-#include "eas/dsu_pwr.h"
+#include "sugov/sched_version_ctrl.h"
 #include "eas_trace.h"
 #if IS_ENABLED(CONFIG_MTK_THERMAL_INTERFACE)
 #include <thermal_interface.h>
 #endif
-
-int val_m = 1;
-
-inline
-int pd_get_opp_wFloor_Freq(int cpu, unsigned long freq,
-	int quant, int wl, unsigned long floor_freq)
-{
-	int opp = -1;
-
-	freq = max(freq, floor_freq);
-	opp = pd_freq2opp(cpu, freq, quant, wl);
-
-	return opp;
-}
-
-inline
-unsigned long pd_get_volt_wFloor_Freq_(int cpu, unsigned long freq,
-	int quant, int wl, unsigned long floor_freq)
-{
-	int opp = -1;
-
-	opp = pd_get_opp_wFloor_Freq(cpu, freq, quant, wl, floor_freq);
-
-	return (unsigned long) pd_opp2volt(cpu, opp, quant, wl);
-}
-
-inline
-unsigned long pd_get_dsu_freq_wFloor_Freq(int cpu, unsigned long freq,
-	int quant, int wl, unsigned long floor_freq)
-{
-	int opp = -1;
-
-	opp = pd_get_opp_wFloor_Freq(cpu, freq, quant, wl, floor_freq);
-
-	return (unsigned long) pd_cpu_opp2dsu_freq(cpu, opp, quant, wl);
-}
-
-unsigned long pd_get_cpufreq_wFloor_Freq(int cpu, unsigned long freq,
-	int wl, unsigned long floor_freq)
-{
-	return max(freq, floor_freq);
-}
-
-unsigned long shared_buck_lkg_pwr(int wl, int cpu, int opp, int temperature,
-	unsigned long extern_volt)
-{
-	int i;
-	unsigned long cpu_volt_orig = 0, static_pwr = 0, static_pwr_orig = 0;
-
-	cpu_volt_orig = pd_opp2volt(cpu, opp, false, wl);
-	static_pwr_orig = (unsigned long) pd_get_opp_leakage(cpu, opp, temperature);
-	if (!extern_volt || extern_volt <= cpu_volt_orig)
-		return static_pwr_orig;
-
-	i = pd_cpu_volt2opp(cpu, extern_volt, false, wl);
-	static_pwr = pd_get_opp_leakage(cpu, i, temperature);
-
-	return static_pwr;
-}
-
-unsigned long shared_buck_dyn_pwr(unsigned long dyn_pwr, unsigned long cpu_volt,
-	unsigned long extern_volt)
-{
-	if (!extern_volt || !cpu_volt || extern_volt <= cpu_volt)
-		return dyn_pwr;
-
-	return (unsigned long long)dyn_pwr * (unsigned long long)extern_volt *
-			(unsigned long long)extern_volt / cpu_volt / cpu_volt;
-}
-
-unsigned long mtk_update_dsu_status(void *private, int quant, unsigned int wl,
-	unsigned int gear_idx, unsigned long freq, unsigned long floor_freq,
-	int this_cpu, int dst_cpu, unsigned int *output)
-{
-	unsigned long dsu_freq, dsu_volt;
-	unsigned int dsu_opp;
-	struct dsu_state *dsu_ps;
-	struct eenv_dsu *eenv = (struct eenv_dsu *) private;
-
-	dsu_freq = pd_get_dsu_freq_wFloor_Freq(this_cpu, freq, quant, wl, floor_freq);
-	if (dst_cpu >= 0) {
-		if (eenv->dsu_freq_base < dsu_freq) {
-			eenv->dsu_freq_new = dsu_freq;
-			dsu_opp = dsu_get_freq_opp(eenv->dsu_freq_new);
-			dsu_ps = dsu_get_opp_ps(wl, dsu_opp);
-			eenv->dsu_volt_new = dsu_ps->volt;
-		} else {
-			eenv->dsu_freq_new = eenv->dsu_freq_base;
-			eenv->dsu_volt_new = eenv->dsu_volt_base;
-		}
-		dsu_volt = (unsigned long) eenv->dsu_volt_new;
-	} else if (dst_cpu == -2) {
-		dsu_volt = (unsigned long) eenv->dsu_volt_new;
-	} else {
-		dsu_volt = (unsigned long) eenv->dsu_volt_base;
-	}
-
-	return dsu_volt;
-}
-
-unsigned long update_dsu_status_(struct energy_env *eenv, int quant,
-	unsigned long freq, unsigned long floor_freq, int this_cpu, int dst_cpu)
-{
-	unsigned long dsu_volt;
-	unsigned int output[4];
-
-	dsu_volt = mtk_update_dsu_status(eenv->android_vendor_data1, quant,
-			eenv->wl, eenv->gear_idx, freq, floor_freq, this_cpu, dst_cpu, output);
-
-	if (trace_sched_dsu_freq_enabled())
-		trace_sched_dsu_freq(eenv->gear_idx, dst_cpu, output[0], output[1],
-				(unsigned long)output[2], (unsigned long)output[3], dsu_volt);
-
-	return dsu_volt;
-}
-
-int dsu_freq_changed_(void *private)
-{
-	struct eenv_dsu *eenv = (struct eenv_dsu *) private;
-
-	return (eenv->dsu_freq_new  > eenv->dsu_freq_base);
-}
-
-void eenv_dsu_init_(void *private, unsigned int wl,
-	int PERCORE_L3_BW, unsigned int cpumask_val,
-	unsigned int *val, unsigned int *output)
-{
-#if IS_ENABLED(CONFIG_MTK_GEARLESS_SUPPORT)
-	static struct cpu_dsu_freq_state *freq_state;
-#endif
-	struct dsu_info *dsu;
-	unsigned int dsu_opp;
-	struct dsu_state *dsu_ps;
-	unsigned int dsu_bw, sum_dsu_bw;
-	struct eenv_dsu *eenv = (struct eenv_dsu *) private;
-	unsigned int cpu = 0;
-
-	dsu = &(eenv->dsu);
-	if (PERCORE_L3_BW) {
-		sum_dsu_bw = 0;
-		while (cpumask_val) {
-			if (cpumask_val & 1) {
-				dsu_bw = get_pelt_per_core_dsu_bw(cpu);
-				val[cpu] = dsu->per_core_dsu_bw[cpu] = dsu_bw;
-				sum_dsu_bw += dsu_bw;
-			}
-			cpumask_val >>= 1;
-			cpu ++;
-		}
-		dsu->dsu_bw = sum_dsu_bw;
-	} else
-		dsu->dsu_bw = get_pelt_dsu_bw();
-
-	dsu->emi_bw = get_pelt_emi_bw();
-#if IS_ENABLED(CONFIG_MTK_THERMAL_INTERFACE)
-	dsu->temp = get_dsu_temp()/1000;
-#else
-	dsu->temp = 0;
-#endif
-
-#if IS_ENABLED(CONFIG_MTK_GEARLESS_SUPPORT)
-	eenv->dsu_freq_thermal = get_dsu_ceiling_freq();
-	freq_state = get_dsu_freq_state();
-	eenv->dsu_freq_base = freq_state->dsu_target_freq;
-	dsu_opp = dsu_get_freq_opp(eenv->dsu_freq_base);
-	dsu_ps = dsu_get_opp_ps(wl, dsu_opp);
-	eenv->dsu_volt_base = dsu_ps->volt;
-#endif
-
-	output[0] = dsu->temp;
-	output[1] = eenv->dsu_freq_base;
-	output[2] = eenv->dsu_volt_base;
-	output[3] = eenv->dsu_freq_thermal;
-	output[4] = dsu->dsu_bw;
-	output[5] = dsu->emi_bw;
-
-}
-
-inline
-unsigned long get_cpu_power_(unsigned int mtk_em, unsigned int get_lkg,
-	int quant, int wl, int *val_s, int r_o, int caller,
-	int this_cpu, int *cpu_temp, int opp, unsigned int cpumask_val,
-	unsigned long *data, unsigned long *output)
-{
-	unsigned int pd_volt = 0;
-	int cpu = 0;
-	unsigned long dyn_pwr, static_pwr = 0, pwr_eff, sum_cap = 0;
-	unsigned long sum_util = data[0];
-	unsigned long extern_volt = data[1];
-	unsigned long cap = data[2];
-	unsigned long scale_cpu = data[3];
-
-	if (get_lkg) {
-		while (cpumask_val) {
-			if (cpumask_val & 1) {
-				unsigned int cpu_static_pwr;
-
-				cpu_static_pwr = shared_buck_lkg_pwr(wl, cpu, opp,
-						cpu_temp[cpu], extern_volt);
-				static_pwr += cpu_static_pwr;
-				sum_cap += cap;
-				output[cpu + 4] = cpu_static_pwr;
-			}
-			cpumask_val >>= 1;
-			cpu ++;
-		}
-		static_pwr = (likely(sum_cap) ? (static_pwr * sum_util) / sum_cap : 0);
-	}
-
-	if (mtk_em) {
-		pwr_eff = pd_opp2pwr_eff(this_cpu, opp, false, wl, val_s,
-				false, DPT_CALL_MTK_EM_CPU_ENERGY);
-		dyn_pwr = pwr_eff * sum_util;
-
-		/* shared-buck dynamic power*/
-		pd_volt = pd_opp2volt(this_cpu, opp, quant, wl);
-		dyn_pwr = shared_buck_dyn_pwr(dyn_pwr, pd_volt, extern_volt);
-	} else {
-		pwr_eff = output[2];
-		dyn_pwr = (pwr_eff * sum_util / scale_cpu);
-	}
-
-	output[0] = dyn_pwr;
-	output[1] = static_pwr;
-	output[2] = pwr_eff;
-	output[3] = sum_cap;
-
-	return dyn_pwr + static_pwr;
-}
-
-inline
-unsigned long get_cpu_pwr_eff_(int cpu, unsigned long pd_freq, int quant, int wl,
-	int *val_s, int r_o, int caller, unsigned long floor_freq, int temperature,
-	unsigned long extern_volt, unsigned long *output)
-{
-	int opp;
-	unsigned long static_pwr_eff, pwr_eff;
-	int cap;
-	int pd_pwr_eff;
-	unsigned long pd_volt;
-
-	output[0] = opp = pd_get_opp_wFloor_Freq(cpu, pd_freq, quant, wl, floor_freq);
-
-	pd_pwr_eff = pd_opp2pwr_eff(cpu, opp, quant, wl, val_s, false, caller);
-	pd_volt = pd_opp2volt(cpu, opp, quant, wl);
-	output[1] = pd_pwr_eff = shared_buck_dyn_pwr(pd_pwr_eff, pd_volt, extern_volt);
-
-	output[2] = cap = pd_opp2cap(cpu, opp, quant, wl, val_s, false, caller);
-	output[3] = static_pwr_eff = shared_buck_lkg_pwr(wl, cpu, opp,
-				temperature, extern_volt) / cap;
-	pwr_eff = pd_pwr_eff + static_pwr_eff;
-
-	return pwr_eff;
-}
 
 unsigned long (*get_volt_wFloor_Freq_hook)(int cpu, unsigned long freq,
 	int quant, int wl, unsigned long floor_freq);
@@ -396,5 +141,39 @@ unsigned long update_dsu_status(struct energy_env *eenv, int quant,
 		return dsu_volt;
 	}
 
+	return 0;
+}
+
+bool PERCORE_L3_BW;
+
+void init_percore_l3_bw(void)
+{
+	PERCORE_L3_BW = sched_percore_l3_bw_get();
+}
+
+unsigned long (*mtk_get_dsu_pwr_hook)(int wl, int dst_cpu, unsigned long task_util,
+		unsigned long total_util, void *private, unsigned int extern_volt,
+		int dsu_pwr_enable, int PERCORE_L3_BW, void __iomem *base, int *data);
+EXPORT_SYMBOL(mtk_get_dsu_pwr_hook);
+unsigned long get_dsu_pwr(int wl, int dst_cpu, unsigned long task_util,
+		unsigned long total_util, void *private, unsigned int extern_volt,
+		int dsu_pwr_enable)
+{
+	if (mtk_get_dsu_pwr_hook) {
+		unsigned long ret;
+		int data[8];
+
+		ret = mtk_get_dsu_pwr_hook(wl, dst_cpu, task_util,
+			total_util, private, extern_volt, dsu_pwr_enable,
+			(int) PERCORE_L3_BW, get_clkg_sram_base_addr(), &data[0]);
+
+		if (trace_dsu_pwr_cal_enabled()) {
+			trace_dsu_pwr_cal(dst_cpu, task_util, total_util, data[0],
+					data[1], data[2], data[3], data[4], extern_volt,
+					data[5], data[6], data[7]);
+		}
+
+	return ret;
+	}
 	return 0;
 }
