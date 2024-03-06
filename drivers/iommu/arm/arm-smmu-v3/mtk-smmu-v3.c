87a253736a1b2052ae5df445564bb36e6f0e4f0b
Yunfei Wang <yf.wang@mediatek.com>
2024-03-06 10:14:57 +0800

    [ALPS08620520] smmu: mt6991: enable mm power control
    Enable MM smmu power control.
    
    MTK-Commit-Id: e0c1bed12939a0fb8311264d23c3884978b126d2
    
    Signed-off-by: Yunfei Wang <yf.wang@mediatek.com>
    CR-Id: ALPS08620520
    Feature: iommu_kernel
    Change-Id: I9145b7d44563db3e7ba7bad0363f38119f403cc8

--- drivers/iommu/arm/arm-smmu-v3/mtk-smmu-v3.c
+++ drivers/iommu/arm/arm-smmu-v3/mtk-smmu-v3.c
@@ -2658,8 +2658,7 @@ static const struct mtk_smmu_plat_data mt6991_data_mm = {
 	.smmu_plat		= SMMU_MT6991,
 	.smmu_type		= MM_SMMU,
 	.flags			= SMMU_DELAY_HW_INIT | SMMU_SEC_EN | SMMU_HYP_EN |
-				  SMMU_EXTRA_DCM_EN | SMMU_HANG_DETECT | SMMU_CLK_AO_EN |
-				  SMMU_DIS_TCU_CH,
+				  SMMU_EXTRA_DCM_EN | SMMU_HANG_DETECT | SMMU_DIS_TCU_CH,
 };
 
 static const struct mtk_smmu_plat_data mt6991_data_apu = {
