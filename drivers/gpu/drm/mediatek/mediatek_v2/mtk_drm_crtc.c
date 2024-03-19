d74b584c4f0acc9c0907e45fd176b90634068582
mtk23899 <xiuhai.deng@mediatek.com>
2024-03-19 10:03:26 +0800

    [ALPS08707082] mt6877: Disp: Cannot be displayed after suspend/resume
    [Description]
    Disable DSI software control.
    
    [Test]
    Local boot successful.
    
    MTK-Commit-Id: 3b495cc6bdd7aa48058abfbed87f85b235f7037a
    
    Signed-off-by: mtk23899 <xiuhai.deng@mediatek.com>
    CR-Id: ALPS08707082
    Feature: display_mobile
    Change-Id: Ic9c34f4679ead4adb6017665ca0ac119a5685707

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_crtc.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_crtc.c
@@ -10170,6 +10170,9 @@ static void mtk_crtc_addon_connector_disconnect(struct drm_crtc *crtc,
 		case MMSYS_MT6853:
 			mtk_ddp_remove_dsc_prim_MT6853(mtk_crtc, handle);
 			break;
+		case MMSYS_MT6877:
+			mtk_ddp_remove_dsc_prim_MT6877(mtk_crtc, handle);
+			break;
 		case MMSYS_MT6879:
 			mtk_ddp_remove_dsc_prim_MT6879(mtk_crtc, handle);
 			break;
@@ -10327,6 +10330,9 @@ void mtk_crtc_addon_connector_connect(struct drm_crtc *crtc,
 			break;
 		case MMSYS_MT6853:
 			mtk_ddp_insert_dsc_prim_MT6853(mtk_crtc, handle);
+			break;
+		case MMSYS_MT6877:
+			mtk_ddp_insert_dsc_prim_MT6877(mtk_crtc, handle);
 			break;
 		case MMSYS_MT6879:
 			mtk_ddp_insert_dsc_prim_MT6879(mtk_crtc, handle);
