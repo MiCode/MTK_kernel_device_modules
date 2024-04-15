04a0f23316991c0723cc4d1126c0a40def2ee28d
Allan Chia <allan.chia@mediatek.com>
2024-04-15 13:28:09 +0800

    [ALPS08848886] reviser, mnoc: kernel driver migration for k66
    [Description]
    Reviser, mnoc: kernel driver migration for k66
    Reviser device driver should be modify for early platform
    
    [Test]
    Build, bootup pass
    
    MTK-Commit-Id: 074abedb311565f93079f2e2c023932a4a58dfca
    
    Signed-off-by: Allan Chia <allan.chia@mediatek.com>
    CR-Id: ALPS08848886
    Feature: apu_sys_kernel
    Change-Id: Id2174900266b610709fd768f8f4cbafde6d63b3d

--- drivers/misc/mediatek/apusys/apu_mem/reviser/2.0/platform/reviser_device.c
+++ drivers/misc/mediatek/apusys/apu_mem/reviser/2.0/platform/reviser_device.c
@@ -57,6 +57,18 @@ static struct reviser_plat mt6853_drv = {
 	.slb_wait_time				= 0,
 };
 
+static struct reviser_plat mt6877_drv = {
+	.init					= reviser_v1_0_init,
+	.uninit					= reviser_v1_0_uninit,
+
+	.bank_size				= 0x40000,
+	.mdla_max				= 1,
+	.vpu_max				= 2,
+	.edma_max				= 1,
+	.up_max					= 1,
+	.slb_wait_time				= 0,
+};
+
 static struct reviser_plat mt6886_drv = {
 	.init					= reviser_vrv_init,
 	.uninit					= reviser_vrv_uninit,
@@ -86,6 +98,7 @@ static const struct of_device_id reviser_of_match[] = {
 	{ .compatible = "mediatek, mt6885-reviser",    .data = &mt6885_drv},
 	{ .compatible = "mediatek, mt6873-reviser",    .data = &mt6873_drv},
 	{ .compatible = "mediatek, mt6853-reviser",    .data = &mt6853_drv},
+	{ .compatible = "mediatek, mt6877-reviser",    .data = &mt6877_drv},
 	{ .compatible = "mediatek, rv-reviser-mt6886", .data = &mt6886_drv},
 	{ .compatible = "mediatek, rv-reviser",        .data = &rv_drv},
 	{/* end of list */},
