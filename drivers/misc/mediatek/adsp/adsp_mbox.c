01d29cdc7402b55d36f9c09e6a7a5497b4ccf506
20221122022012 activated <wei-yi.yang@mediatek.com>
2024-04-26 21:15:56 +0800

    [ALPS08914684] adsp: revert "check pre wakelock return type"
    [Description]
    Not send suspend ipi message after pre wakelock fail.
    
    [Test]
    Pass UT/IT.
    
    MTK-Commit-Id: 226a88960f3d0b88af30f1a033167c4603dfc070
    
    CR-Id: ALPS08914684
    Feature: ADSP AP Driver
    Change-Id: I6178b6c4da1e0228272d50bcd23a6709eda8217e
    Signed-off-by: 20221122022012 activated <wei-yi.yang@mediatek.com>

--- drivers/misc/mediatek/adsp/adsp_mbox.c
+++ drivers/misc/mediatek/adsp/adsp_mbox.c
@@ -151,11 +151,7 @@ int adsp_mbox_send(struct mtk_mbox_pin_send *pin_send, void *msg,
 		result = MBOX_CONFIG_ERR;
 		goto EXIT_MUTEX;
 	}
-
-	if (adsp_pre_wake_lock((u32)cid) != 0) {
-		result = MBOX_PIN_BUSY;
-		goto EXIT;
-	}
+	adsp_pre_wake_lock((u32)cid);
 
 	if (mtk_mbox_check_send_irq(mbdev, pin_send->mbox,
 				    pin_send->pin_index)) {
