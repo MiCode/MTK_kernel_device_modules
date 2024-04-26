6678a837a460d88a1af37f41f62edfa2f391f9c1
hank.li <hank.li@mediatek.com>
2024-04-26 21:13:39 +0800

    [ALPS07859874] sched: change condition of skip overutilization
    [Description]
    (p64) change the condition of skipping overutilization DPT.
    
    [Test]
    Monkey+suspend/resume+reboot+cycle test
    
    MTK-Commit-Id: 5ce8e8ddf84e683fba4bf3f1658f2a0c931ea5a2
    
    Signed-off-by: hank.li <hank.li@mediatek.com>
    CR-Id: ALPS07859874
    Feature: Energy Aware Scheduling Plus (EAS+)
    Change-Id: I051f3cda0c4130e5189ef36334e027795a0d3d52

--- drivers/misc/mediatek/sched/sugov/cpufreq.h
+++ drivers/misc/mediatek/sched/sugov/cpufreq.h
@@ -257,6 +257,7 @@ extern int (*is_dpt_support_driver_hook) (void);
 extern void (*set_v_driver_hook)(int v);
 #define for_each_collab_type(collab_type) for (collab_type = 0; collab_type < nr_collab_type; collab_type++)
 int collab_type_0_ret_function(void);
+extern int get_sys_max_cap_cluster(void);
 
 #define DPT_CALL_UPDATE_WL_TBL 0
 #define DPT_CALL_UPDATE_CURR_COLLAB_STATE 1
