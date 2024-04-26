58419c0bd913991d8b45dd3487daa1c568801b15
yonggang.yu <yonggang.yu@mediatek.com>
2024-04-26 21:13:09 +0800

    [ALPS08859142] DISP: Fixed mess panel when power on
    [Description]
    Fixed mess panel when power on, as bw level
    not enough.
    
    [Test]
    boot,suspend and resume, mestress
    
    MTK-Commit-Id: 3d5b6aba3a0c07eb7ddd24e18cdff136c13eb00b
    
    Signed-off-by: yonggang.yu <yonggang.yu@mediatek.com>
    CR-Id: ALPS08859142
    Feature: display_mobile
    Change-Id: I25ea76b534b7d4ddfad4c6f8222fe17a3506ad54

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_pmqos.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_pmqos.c
@@ -925,6 +925,19 @@ int mtk_disp_set_per_larb_hrt_bw(struct mtk_drm_crtc *mtk_crtc, unsigned int bw)
 			mtk_disp_clear_channel_hrt_bw_MT6991(mtk_crtc);
 		}
 
+		/* Just for MT6989 SWRG mess panel as channel bw not enough */
+		/* TODO: Need to remove when official version */
+		if (priv->data->mmsys_id == MMSYS_MT6989) {
+			struct mtk_dsi *dsi = container_of(comp, struct mtk_dsi, ddp_comp);
+
+			dsi->ext = find_panel_ext(dsi->panel);
+			if (dsi->ext && dsi->ext->params->is_cphy && tmp1)
+				tmp1 += bw_base;
+
+			if (mtk_crtc_is_frame_trigger_mode(crtc) && tmp1)
+				tmp1 += bw_base;
+		}
+
 		mtk_icc_set_bw(priv->hrt_by_larb, 0, MBps_to_icc(tmp1));
 
 		mtk_vidle_dvfs_bw_set(tmp1);
