cef62d312ebca115367e4f5db331e2dda60dd6cb
Peter-TY Tsai <peter-ty.tsai@mediatek.com>
2024-05-28 10:26:22 +0800

    [ALPS08878748] sched: solve runnable_boost trace not printing
    [Description]
    as-is: sched_runnable_boost not printing in eas+
    to-be: both sugov and eas+ show sched_runnable_boost
    
    [Test]
    build load
    
    MTK-Commit-Id: d1e6290e148fbac4a662d03e2a8eb9eb9e288d8f
    
    Signed-off-by: Peter-TY Tsai <peter-ty.tsai@mediatek.com>
    CR-Id: ALPS08878748
    Feature: Energy Aware Scheduling Plus (EAS+)
    Change-Id: Ieb7be4f8810be632f8db7d5aa7d712879ef5ed60

--- drivers/misc/mediatek/sched/common.c
+++ drivers/misc/mediatek/sched/common.c
@@ -8,12 +8,6 @@
 #include <sched/sched.h>
 #include "common.h"
 #define CREATE_TRACE_POINTS
-#include "common_trace.h"
-
-#define lsub_positive(_ptr, _val) do {				\
-	typeof(_ptr) ptr = (_ptr);				\
-	*ptr -= min_t(typeof(*ptr), *ptr, _val);		\
-} while (0)
 
 MODULE_LICENSE("GPL");
 
@@ -109,67 +103,6 @@ unsigned long mtk_uclamp_rq_util_with(struct rq *rq, unsigned long util,
 }
 #endif /* CONFIG_UCLAMP_TASK */
 
-static inline unsigned long task_util(struct task_struct *p)
-{
-	return READ_ONCE(p->se.avg.util_avg);
-}
-
-static inline unsigned long _task_util_est(struct task_struct *p)
-{
-	struct util_est ue = READ_ONCE(p->se.avg.util_est);
-
-	return max(ue.ewma, (ue.enqueued & ~UTIL_AVG_UNCHANGED));
-}
-
-/* modified from k66 cpu_util() */
-unsigned long mtk_cpu_util_next(int cpu, struct task_struct *p, int dst_cpu, int boost)
-{
-	struct cfs_rq *cfs_rq = &cpu_rq(cpu)->cfs;
-	unsigned long util = READ_ONCE(cfs_rq->avg.util_avg);
-	unsigned long runnable;
-
-	if (is_runnable_boost_enable() && boost) {
-		runnable = READ_ONCE(cfs_rq->avg.runnable_avg);
-		util = max(util, runnable);
-	}
-
-	if (p && task_cpu(p) == cpu && dst_cpu != cpu)
-		lsub_positive(&util, task_util(p));
-	else if (p && task_cpu(p) != cpu && dst_cpu == cpu)
-		util += task_util(p);
-
-	if (sched_feat(UTIL_EST) && is_util_est_enable()) {
-		unsigned long util_est;
-
-		util_est = READ_ONCE(cfs_rq->avg.util_est.enqueued);
-
-		if (dst_cpu == cpu)
-			util_est += _task_util_est(p);
-		else if (p && unlikely(task_on_rq_queued(p) || current == p))
-			lsub_positive(&util_est, _task_util_est(p));
-
-		util = max(util, util_est);
-	}
-
-	if (trace_sched_runnable_boost_enabled())
-		trace_sched_runnable_boost(is_runnable_boost_enable(), boost, cfs_rq->avg.util_avg,
-				cfs_rq->avg.util_est.enqueued, runnable, util);
-
-	return min(util, capacity_orig_of(cpu) + 1);
-}
-
-/* cloned from k66 cpu_util_cfs() */
-unsigned long mtk_cpu_util_cfs(int cpu)
-{
-	return mtk_cpu_util_next(cpu, NULL, -1, 0);
-}
-
-/* cloned from k66 cpu_util_cfs_boost() */
-unsigned long mtk_cpu_util_cfs_boost(int cpu)
-{
-	return mtk_cpu_util_next(cpu, NULL, -1, 1);
-}
-
 void parse_eas_data(struct eas_info *info)
 {
 	struct device_node *dn = NULL;
@@ -189,9 +122,4 @@ void parse_eas_data(struct eas_info *info)
 		pr_info("No EAS node!\n");
 		info->available = false;
 	}
-}
-
-void mtk_cpu_util_cfs_boost_hook(void *data, int cpu, unsigned long *util)
-{
-	*util = mtk_cpu_util_cfs_boost(cpu);
 }
