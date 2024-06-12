4c72823a4eb8bb71b377c3c8c235d21a37e3c85f
Hungwen Chien <hungwen.chien@mediatek.com>
2024-06-12 11:47:15 +0800

    [ALPS08686299] pkvm_tmem: Revise protocol of dyn_hcall
    [Description]
    Due to latest ACK update, need to reivse dyn_hcall protocol
    from kvm_cpu_context to user_pt_regs
    
    [Test]
    Boot and build pass
    
    MTK-Commit-Id: 4fa0737c3728e982ae39352e49f479526ce767b8
    
    Signed-off-by: Hungwen Chien <hungwen.chien@mediatek.com>
    CR-Id: ALPS08686299
    Feature: trusted_mem
    Change-Id: I0749ec81ac9b24baa4f6f621a1a5bdd2f6714aac

--- drivers/misc/mediatek/pkvm_tmem/pkvm_tmem_host.h
+++ drivers/misc/mediatek/pkvm_tmem/pkvm_tmem_host.h
@@ -6,10 +6,10 @@
 #ifndef __PKVM_TMEM_HOST_H__
 #define __PKVM_TMEM_HOST_H__
 
-int __kvm_nvhe_hyp_tmem_init(const struct pkvm_module_ops *ops);
-void __kvm_nvhe_hyp_region_protect(struct kvm_cpu_context *ctx);
-void __kvm_nvhe_hyp_region_unprotect(struct kvm_cpu_context *ctx);
-void __kvm_nvhe_hyp_page_protect(struct kvm_cpu_context *ctx);
-void __kvm_nvhe_hyp_page_unprotect(struct kvm_cpu_context *ctx);
+int kvm_nvhe_sym(hyp_tmem_init)(const struct pkvm_module_ops *ops);
+void kvm_nvhe_sym(hyp_region_protect)(struct user_pt_regs *regs);
+void kvm_nvhe_sym(hyp_region_unprotect)(struct user_pt_regs *regs);
+void kvm_nvhe_sym(hyp_page_protect)(struct user_pt_regs *regs);
+void kvm_nvhe_sym(hyp_page_unprotect)(struct user_pt_regs *regs);
 
 #endif
