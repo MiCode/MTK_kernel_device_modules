b0d4b75de190beea0ab2f98db75c2f9888c75701
aparna.kumari <aparna.kumari@mediatek.com>
2024-06-12 11:53:43 +0800

    [ALPS08963427] upower : change upower_get_core_tbl logic
    [Description]
    fix the logic to get the bank
    
    [Test]
    Correct values in upower table
    
    MTK-Commit-Id: cf9416f13f2bb9af4069df6b49f99f7ee8299f4b
    
    Change-Id: I39ba6cdbe211f976b96c94494e5efbfc193efd25
    Signed-off-by: aparna.kumari <aparna.kumari@mediatek.com>
    CR-Id: ALPS08963427
    Feature: cpufreq

--- drivers/misc/mediatek/upower/mtk_unified_power_api.c
+++ drivers/misc/mediatek/upower/mtk_unified_power_api.c
@@ -127,24 +127,10 @@ struct upower_tbl *upower_get_core_tbl(unsigned int cpu)
 {
 	struct upower_tbl *ptr_tbl;
 	struct upower_tbl_info *ptr_tbl_info;
-#ifdef FIRST_CLUSTER_IS_L
-	enum upower_bank bank = UPOWER_BANK_L;
-#else
+
 	enum upower_bank bank = UPOWER_BANK_LL;
-#endif
-#ifdef FIRST_CLUSTER_IS_L
-	if (cpu < 4) /* cpu 0-3 */
-		bank = UPOWER_BANK_0;
-	else if (cpu < 8) /* cpu 4-7 */
-		bank = UPOWER_BANK_1;
-#else
-	if (cpu < 4) /* cpu 0-3 */
-		bank = UPOWER_BANK_LL;
-	else if (cpu < 8) /* cpu 4-7 */
-		bank = UPOWER_BANK_LL + 1;
-	else if (cpu < 10) /* cpu 8-9 */
-		bank = UPOWER_BANK_LL + 2;
-#endif
+
+	bank = cpu_cluster_mapping(cpu);
 
 #ifdef UPOWER_L_PLUS
 	if (cpu == UPOWER_L_PLUS_CORE)
