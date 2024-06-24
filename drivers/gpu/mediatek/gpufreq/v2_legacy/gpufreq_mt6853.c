15cfe3a3626766bc50ac6c40fb8da0c5f8bd7ed5
sourav.sardana <sourav.sardana@mediatek.com>
2024-06-24 11:30:38 +0800

    [ALPS08963462] gpu: fix multi-thread issue
    [Description]
    There is a timing multi-thread issue PTPOD update volt when power on
    ,but when do real volt switch power is off, scale vgpu fail and
    gpufreq abort happens.
    Fix to add lock before update volt.
    
    [Test]
    Boot Pass, opp table verified
    
    MTK-Commit-Id: d59abcaf155671d69b38d5a3ba0cf161d54cae80
    
    Feature: MFGSYS driver
    CR-Id: ALPS08963462
    Change-Id: I57d58fb370c4782e6010adf33a8fc2732ad6c95b
    Signed-off-by: sourav.sardana <sourav.sardana@mediatek.com>

--- drivers/gpu/mediatek/gpufreq/v2_legacy/gpufreq_mt6853.c
+++ drivers/gpu/mediatek/gpufreq/v2_legacy/gpufreq_mt6853.c
@@ -606,28 +606,33 @@ static void __mt_gpufreq_vgpu_set_mode(unsigned int mode)
 		GPUFREQ_LOGE("failed to configure mode, ret = %d, mode = %d\n", ret, mode);
 	}
 }
-static void __mt_gpufreq_volt_switch_without_vsram_volt(unsigned int volt_old, unsigned int volt_new)
+static void __gpufreq_volt_switch_by_ptpod(unsigned int volt_old, unsigned int volt_new)
 {
 	unsigned int vsram_volt_new, vsram_volt_old;
 	int ret = GPUFREQ_SUCCESS;
-
+	bool g_fixed_freq_volt_state = g_dvfs_state & DVFS_FIX_FREQ_VOLT;
 	volt_new = VOLT_NORMALIZATION(volt_new);
 
 	GPUFREQ_LOGI("volt_new = %d, volt_old = %d\n", volt_new, volt_old);
+	mutex_lock(&gpufreq_lock);
 
-	vsram_volt_new = __gpufreq_get_vsram_by_vgpu(volt_new);
-	vsram_volt_old = __gpufreq_get_vsram_by_vgpu(volt_old);
+	if(__gpufreq_get_power_state() && !g_fixed_freq_volt_state) {
+		vsram_volt_new = __gpufreq_get_vsram_by_vgpu(volt_new);
+		vsram_volt_old = __gpufreq_get_vsram_by_vgpu(volt_old);
 
-		/* voltage scaling */
-	ret = __gpufreq_volt_scale_gpu(
-		volt_old, volt_new, vsram_volt_old, vsram_volt_new);
-	if (unlikely(ret)) {
-		GPUFREQ_LOGE("fail to scale Vgpu: (%d->%d), Vsram_gpu: (%d->%d)",
+			/* voltage scaling */
+		ret = __gpufreq_volt_scale_gpu(
 			volt_old, volt_new, vsram_volt_old, vsram_volt_new);
-	} else {
-		g_gpu.cur_volt = volt_new;
-		g_gpu.cur_vsram = vsram_volt_new;
+		if (unlikely(ret)) {
+			GPUFREQ_LOGE("fail to scale Vgpu: (%d->%d), Vsram_gpu: (%d->%d)",
+				volt_old, volt_new, vsram_volt_old, vsram_volt_new);
+		} else {
+			g_gpu.cur_volt = volt_new;
+			g_gpu.cur_vsram = vsram_volt_new;
+		}
 	}
+
+	mutex_unlock(&gpufreq_lock);
 }
 /*
  * interpolation none PTPOP.
@@ -674,26 +679,6 @@ void mt_gpufreq_update_volt_interpolation(void)
 }
 
 /*
- * switch voltage and vsram via PMIC
- */
-static void _gpufreq_volt_switch_without_vsram_gpu(
-		unsigned int vgpu_old, unsigned int vgpu_new)
-{
-	unsigned int vsram_gpu_new, vsram_gpu_old;
-
-	vgpu_new = VOLT_NORMALIZATION(vgpu_new);
-
-	vsram_gpu_new = __gpufreq_get_vsram_by_vgpu(vgpu_new);
-	vsram_gpu_old = __gpufreq_get_vsram_by_vgpu(vgpu_old);
-
-	__gpufreq_volt_scale_gpu(
-			vgpu_old,
-			vgpu_new,
-			vsram_gpu_old,
-			vsram_gpu_new);
-}
-
-/*
  * API : update OPP and switch back to default voltage setting
  */
 void mt_gpufreq_restore_default_volt(void)
@@ -722,7 +707,7 @@ void mt_gpufreq_restore_default_volt(void)
 
 	__gpufreq_set_springboard();
 
-	__mt_gpufreq_volt_switch_without_vsram_volt(g_gpu.cur_volt,
+	__gpufreq_volt_switch_by_ptpod(g_gpu.cur_volt,
 		g_gpu.working_table[g_gpu.cur_oppidx].volt);
 
 	mutex_unlock(&ptpod_lock);
@@ -790,7 +775,6 @@ unsigned int mt_gpufreq_update_volt(unsigned int pmic_volt[], unsigned int array
 	int i;
 	int target_idx;
 	struct gpufreq_opp_info *signed_table = g_gpu.signed_table;
-	bool g_fixed_freq_volt_state = g_dvfs_state & DVFS_FIX_FREQ_VOLT;
 
 	//__mt_gpufreq_update_table_by_asensor();
 
@@ -815,15 +799,9 @@ unsigned int mt_gpufreq_update_volt(unsigned int pmic_volt[], unsigned int array
 
 	__gpufreq_update_gpu_working_table();
 
-	/* update volt if powered */
-	if (__gpufreq_get_power_state() && !g_fixed_freq_volt_state) {
-		_gpufreq_volt_switch_without_vsram_gpu(
+	__gpufreq_volt_switch_by_ptpod(
 			g_gpu.cur_volt,
 			signed_table[g_gpu.cur_oppidx].volt);
-
-		g_gpu.cur_volt = signed_table[g_gpu.cur_oppidx].volt;
-		g_gpu.cur_vsram = signed_table[g_gpu.cur_oppidx].vsram;
-	}
 
 	mutex_unlock(&ptpod_lock);
 
