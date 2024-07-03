9ebb4b2b6c4543645f41c0cbad9cf9c30338736b
Qiming Sun <qiming.sun@mediatek.com>
2024-07-03 14:49:51 +0800

    [ALPS09070820] iommu: mt6899: enable secure feature for mm iommu
    [Description]
    Enable iommu secure feature flag IOMMU_SEC_EN for mm iommu.
    
    [Test]
    Check whether fpga can boot to adb shell and check whether
    alloc/map iova can be performed normally.
    
    MTK-Commit-Id: a1ea28bdcae2e8600367421056a0a6216f19e118
    
    Signed-off-by: Qiming Sun <qiming.sun@mediatek.com>
    CR-Id: ALPS09070820
    Feature: iommu_kernel
    Change-Id: I3a5da33850149120f9dcffcc4434b2b572813606
    (cherry-pick from b0c67a7cb514cf8dbd81484d763040d3e84af925)

--- drivers/iommu/mtk_iommu.c
+++ drivers/iommu/mtk_iommu.c
@@ -3968,7 +3968,7 @@ static const struct mtk_iommu_plat_data mt6897_data_apu1 = {
 static const struct mtk_iommu_plat_data mt6899_data_disp = {
 	.m4u_plat	= M4U_MT6899,
 	.flags          = OUT_ORDER_WR_EN | GET_DOM_ID_LEGACY |
-			  NOT_STD_AXI_MODE | TLB_SYNC_EN | /*IOMMU_SEC_EN |*/
+			  NOT_STD_AXI_MODE | TLB_SYNC_EN | IOMMU_SEC_EN |
 			  SKIP_CFG_PORT | IOVA_34_EN | IOMMU_CLK_AO_EN | IOMMU_EN_PRE |
 			  /*HAS_BCLK |*/ HAS_SMI_SUB_COMM | SAME_SUBSYS/* | PGTABLE_PA_35_EN*/,
 	.hw_list        = &mm_iommu_list,
@@ -3985,7 +3985,7 @@ static const struct mtk_iommu_plat_data mt6899_data_disp = {
 static const struct mtk_iommu_plat_data mt6899_data_mdp = {
 	.m4u_plat	= M4U_MT6899,
 	.flags          = OUT_ORDER_WR_EN | GET_DOM_ID_LEGACY |
-			  NOT_STD_AXI_MODE | TLB_SYNC_EN | /*IOMMU_SEC_EN |*/
+			  NOT_STD_AXI_MODE | TLB_SYNC_EN | IOMMU_SEC_EN |
 			  SKIP_CFG_PORT | IOVA_34_EN | IOMMU_CLK_AO_EN | IOMMU_EN_PRE |
 			  /*HAS_BCLK | */HAS_SMI_SUB_COMM | SAME_SUBSYS/* | PGTABLE_PA_35_EN*/,
 	.hw_list        = &mm_iommu_list,
