356b502f45bcd0f0538ede62ff12ef774e6d4aea
Joe.Zhou <Joe.Zhou@mediatek.com>
2024-07-03 14:50:10 +0800

    [ALPS08477036] mt6899: mmc driver porting
    [Description]
    For mt6899 msdc bring up driver porting.
    
    [Test]
    Build pass
    
    MTK-Commit-Id: 49dede86b5aee718b44e717f422a65207bf016ae
    
    Signed-off-by: Joe.Zhou <Joe.Zhou@mediatek.com>
    CR-Id: ALPS08477036
    Feature: MSDC
    Change-Id: Ieb27d427d144a74383922f8d50df716c5fcb47a0

--- drivers/mmc/host/mtk-mmc.c
+++ drivers/mmc/host/mtk-mmc.c
@@ -674,6 +674,31 @@ static const struct mtk_mmc_compatible mt6991_compat = {
 	},
 };
 
+static const struct mtk_mmc_compatible mt6899_compat = {
+	.clk_div_bits = 12,
+	.recheck_sdio_irq = false,
+	.hs400_tune = false,
+	.pad_tune_reg = MSDC_PAD_TUNE0,
+	.async_fifo = true,
+	.data_tune = true,
+	.busy_check = true,
+	.stop_clk_set = {
+		.enable = 1,
+		.stop_cnt = 1,
+		.pop_cnt = 2,
+	},
+	.enhance_rx = true,
+	.support_64g = true,
+	.clock_set = {
+		.need_gate_cg = false,
+	},
+	.new_tx_ver = MSDC_NEW_TX_V2,
+	.new_rx_ver = MSDC_NEW_RX_V1,
+	.infra_check = {
+		.enable = false,
+	},
+};
+
 static const struct of_device_id msdc_of_ids[] = {
 	{ .compatible = "mediatek,mt8135-mmc", .data = &mt8135_compat},
 	{ .compatible = "mediatek,mt8173-mmc", .data = &mt8173_compat},
@@ -698,6 +723,7 @@ static const struct of_device_id msdc_of_ids[] = {
 	{ .compatible = "mediatek,mt6853-mmc", .data = &mt6853_compat},
 	{ .compatible = "mediatek,mt6781-mmc", .data = &mt6781_compat},
 	{ .compatible = "mediatek,mt6765-mmc", .data = &mt6765_compat},
+	{ .compatible = "mediatek,mt6899-mmc", .data = &mt6899_compat},
 	{}
 };
 MODULE_DEVICE_TABLE(of, msdc_of_ids);
