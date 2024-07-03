2d9e6da47779b69598305f6891ec745b0a3973e6
Sabrina Kuo <Yi.Kuo@mediatek.com>
2024-07-03 14:49:06 +0800

    [ALPS09027445] MMQoS: mt6899: mmqos porting
    [Description]
    mmqos porting for mt6899
    
    [Test]
    boot to HS
    
    MTK-Commit-Id: 088c006dc6addcd25148b05fd8efb502861f3714
    
    Signed-off-by: Sabrina Kuo <Yi.Kuo@mediatek.com>
    CR-Id: ALPS09027445
    Feature: MMQoS
    Change-Id: If1d916bff911c7dbc24ee4bf5ed3414e90a3c13e
    (cherry picked from commit f29ce256f86db45c8a1f8d15d5142e181c69a6b6)

--- drivers/misc/mediatek/mmqos/mmqos_wrapper.c
+++ drivers/misc/mediatek/mmqos/mmqos_wrapper.c
@@ -80,6 +80,10 @@ static struct wrapper_data wrapper_data_mt6833 = {
 	.max_ostd = 40,
 	.icc_dst_id = SLAVE_COMMON(0),
 };
+static struct wrapper_data wrapper_data_mt6899 = {
+	.max_ostd = 40,
+	.icc_dst_id = SLAVE_COMMON(0),
+};
 static struct wrapper_data *mmqos_wrapper;
 static struct device *dev;
 static BLOCKING_NOTIFIER_HEAD(hrt_bw_throttle_notifier);
@@ -311,6 +315,10 @@ static const struct of_device_id of_mmqos_wrapper_match_tbl[] = {
 	{
 		.compatible = "mediatek,mt6833-mmqos-wrapper",
 		.data = &wrapper_data_mt6833,
+	},
+	{
+		.compatible = "mediatek,mt6899-mmqos-wrapper",
+		.data = &wrapper_data_mt6899,
 	},
 	{}
 };
