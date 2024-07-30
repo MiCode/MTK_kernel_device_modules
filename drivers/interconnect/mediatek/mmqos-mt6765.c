05a293e7855a9f2b772e499ce093eeeb36cb7125
ot_devendra.balihar <ot_devendra.balihar@mediatek.com>
2024-07-30 10:39:19 +0800

    [ALPS08963405] codebase: mt6765: disable mminfra
    [Description]
    apply check to disable mminfra
    
    [Test]
    mmqos-basic-ut
    
    MTK-Commit-Id: 1e1c55ca858a564e0b203ba670f653eb929226ee
    
    Change-Id: I6a20bae70088e6605aa4e6480ddc21c46f796283
    Feature: MMQoS
    Signed-off-by: ot_devendra.balihar <ot_devendra.balihar@mediatek.com>
    CR-Id: ALPS08963405

--- drivers/interconnect/mediatek/mmqos-mt6765.c
+++ drivers/interconnect/mediatek/mmqos-mt6765.c
@@ -165,6 +165,7 @@ static const struct mtk_mmqos_desc mmqos_desc_mt6765 = {
 	.comm_port_hrt_types = {
 		{ HRT_MAX_BWL, HRT_NONE, HRT_NONE, HRT_NONE, HRT_NONE, HRT_DISP },
 	},
+	.freq_mode = NO_MMINFRA,
 };
 static const struct of_device_id mtk_mmqos_mt6765_of_ids[] = {
 	{
