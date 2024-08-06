f4262ac1890afa00cdb1cb9671e43fb22699bc51
Chris-YC Chen <chris-yc.chen@mediatek.com>
2024-08-06 15:55:44 +0800

    [ALPS09099952] mml: change hrt mode
    [Description]
    change hrt mode
    
    [Test]
    VP
    
    MTK-Commit-Id: 9c484c24718f18de4be51f73da94a504a336f77d
    
    Signed-off-by: Chris-YC Chen <chris-yc.chen@mediatek.com>
    CR-Id: ALPS09099952
    Feature: mml
    Change-Id: I8892b6a33632465ae59b4fed97fcc41bacc8fe82

--- drivers/gpu/drm/mediatek/mml/mtk-mml-tp-mt6899.c
+++ drivers/gpu/drm/mediatek/mml/mtk-mml-tp-mt6899.c
@@ -1178,7 +1178,7 @@ static const struct mml_topology_ops tp_ops_mt6899 = {
 static __init int mml_topology_ip_init(void)
 {
 	/* init hrt mode as max ostd */
-	mtk_mml_hrt_mode = MML_HRT_OSTD_MAX;
+	mtk_mml_hrt_mode = MML_HRT_ENABLE;
 
 	return mml_topology_register_ip(TOPOLOGY_PLATFORM, &tp_ops_mt6899);
 }
