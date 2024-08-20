7cb3d9117449aab78ada5856b6c05790717464a3
Peter Wang <peter.wang@mediatek.com>
2024-08-20 09:06:22 +0800

    [ALPS09222825] ufs: mediatek: release vcore when suspend
    [Description]
    If clock scaling is disable and fix G5, relsese vocre when suspend.
    
    [Test]
    pdas/ufs
    
    MTK-Commit-Id: 00ad362826256f3f1e4a0b5d2f943fafbd46d35d
    
    Signed-off-by: Peter Wang <peter.wang@mediatek.com>
    CR-Id: ALPS09222825
    Feature: ufs
    Change-Id: If5fa0f5361700b347e4cb7f18ce8a100aae88770

--- drivers/ufs/ufs-mediatek.c
+++ drivers/ufs/ufs-mediatek.c
@@ -2510,6 +2510,9 @@ static int ufs_mtk_suspend(struct ufs_hba *hba, enum ufs_pm_op pm_op,
 	if (ufshcd_is_clkscaling_supported(hba) && (host->clk_scale_up)) {
 		ufs_mtk_pm_qos(hba, false);
 		_ufs_mtk_clk_scale(hba, false);
+	} else if ((!ufshcd_is_clkscaling_supported(hba) &&
+		    hba->pwr_info.gear_rx == UFS_HS_G5)) {
+		_ufs_mtk_clk_scale(hba, false);
 	}
 
 	return 0;
@@ -2541,6 +2544,9 @@ static int ufs_mtk_resume(struct ufs_hba *hba, enum ufs_pm_op pm_op)
 	/* Request pm_qos/clk if enter suspend is scale up mode */
 	if (ufshcd_is_clkscaling_supported(hba) && (host->clk_scale_up)) {
 		ufs_mtk_pm_qos(hba, true);
+		_ufs_mtk_clk_scale(hba, true);
+	} else if ((!ufshcd_is_clkscaling_supported(hba) &&
+		    hba->pwr_info.gear_rx == UFS_HS_G5)) {
 		_ufs_mtk_clk_scale(hba, true);
 	}
 
