313d19a790cc814110d548ede3f0c7268a563b10
Victor Zheng <zi-xuan.zheng@mediatek.com>
2024-08-20 09:03:33 +0800

    [ALPS08852876] cmdq: revert burst irq WA
    [Description]
    revert burst irq WA
    
    [Test]
    ATE, Tuota
    
    MTK-Commit-Id: b74184e5d735998ec6a1c44faf5e0e16428c3487
    
    Signed-off-by: Victor Zheng <zi-xuan.zheng@mediatek.com>
    CR-Id: ALPS08852876
    Feature: cmdq_gce
    Change-Id: Ic7762ff16c51723633736f65141d3cbfe91e7512

--- drivers/misc/mediatek/cmdq/mailbox/mtk-cmdq-mailbox-ext.c
+++ drivers/misc/mediatek/cmdq/mailbox/mtk-cmdq-mailbox-ext.c
@@ -134,10 +134,6 @@ struct cmdq_util_controller_fp *cmdq_util_controller;
 #define CMDQ_MIN_AGE_VALUE              (5)	/* currently disable age */
 #define CMDQ_INIT_BUF_SIZE		8484
 
-#define CMDQ_MMINFRA_VOTER_OFS	0x120
-#define CMDQ_MMINFRA_VOTER_GCED_MASK	BIT(7)
-#define CMDQ_MMINFRA_VOTER_GCEM_MASK	BIT(8)
-
 #define GCED_HWID				(0)
 #define GCEM_HWID				(1)
 
@@ -326,7 +322,6 @@ struct cmdq {
 	bool		err_irq;
 	void __iomem	*dram_pwr_base;
 	void __iomem	*mminfra_ao_base;
-	void __iomem	*mminfra_voter_base;
 	bool		error_irq_sw_req;
 	bool		gce_vm;
 	bool		spr3_timer;
@@ -334,7 +329,6 @@ struct cmdq {
 	struct device	*pd_mminfra_1;
 	struct device	*pd_mminfra_ao;
 	bool		gce_ddr_sel_wla;
-	bool		gce_mask_voter;
 	bool		gce_req_wa;
 	unsigned int	dbg3;
 	bool		gce_res_sw_mode;
@@ -3204,7 +3198,7 @@ static int cmdq_probe(struct platform_device *pdev)
 #if !IS_ENABLED(CONFIG_VIRTIO_CMDQ)
 	int port;
 #endif
-	u32 dram_pwr_pa, mminfra_ao_pa, mminfra_voter_pa;
+	u32 dram_pwr_pa, mminfra_ao_pa;
 
 	plat_data = (struct gce_plat *)of_device_get_match_data(dev);
 	if (!plat_data) {
@@ -3508,14 +3502,6 @@ static int cmdq_probe(struct platform_device *pdev)
 		}
 	}
 
-	if (of_property_read_bool(dev->of_node, "gce-mask-voter")) {
-		cmdq->gce_mask_voter = true;
-		if (!of_property_read_u32(dev->of_node, "mminfra-voter-base", &mminfra_voter_pa)) {
-			cmdq_msg("mminfra-voter-base:%#x", mminfra_voter_pa);
-			cmdq->mminfra_voter_base = ioremap(mminfra_voter_pa, 0x1000);
-		}
-	}
-
 	cmdq->gce_req_wa = of_property_read_bool(dev->of_node, "gce-req-wa");
 
 	if (cmdq->hwid == 0 && cmdq_print_debug)
@@ -3756,13 +3742,6 @@ void cmdq_mbox_enable(void *chan)
 				cmdq_util_get_bit_feature() &
 				CMDQ_LOG_FEAT_PERF);
 
-		if (cmdq->gce_mask_voter) {
-			writel(readl(cmdq->mminfra_voter_base + CMDQ_MMINFRA_VOTER_OFS) &
-				~(CMDQ_MMINFRA_VOTER_GCED_MASK),
-				cmdq->mminfra_voter_base + CMDQ_MMINFRA_VOTER_OFS);
-			cmdq_log("%s hwid:%d voter:%#x", __func__, cmdq->hwid,
-				readl(cmdq->mminfra_voter_base + CMDQ_MMINFRA_VOTER_OFS));
-		}
 		if (cmdq->gce_req_wa) {
 			cmdq_mbox_set_resource_req(GCED_HWID, true, true, GCE_DDREN_BIT);
 			cmdq_mbox_set_resource_req(GCEM_HWID, true, true, GCE_DDREN_BIT);
@@ -3891,13 +3870,6 @@ void cmdq_mbox_disable(void *chan)
 				cmdq_err("pm_runtime_put_sync err:%d", ret);
 		}
 
-		if (cmdq->gce_mask_voter) {
-			writel(readl(cmdq->mminfra_voter_base + CMDQ_MMINFRA_VOTER_OFS) |
-				CMDQ_MMINFRA_VOTER_GCED_MASK | CMDQ_MMINFRA_VOTER_GCEM_MASK ,
-				cmdq->mminfra_voter_base + CMDQ_MMINFRA_VOTER_OFS);
-			cmdq_log("%s hwid:%d voter:%#x", __func__, cmdq->hwid,
-				readl(cmdq->mminfra_voter_base + CMDQ_MMINFRA_VOTER_OFS));
-		}
 
 		if (cmdq->gce_req_wa) {
 			cmdq_mbox_set_resource_req(GCEM_HWID, true, false, GCE_DDREN_BIT);
