cfcc87e3bc4bff4f23dd1bb1bea0b3c44d019bfe
ot_vikram.b <ot_vikram.b@mediatek.com>
2024-08-27 18:02:11 +0800

    [ALPS09294092] iommu: Add PGTABLE_35 flag
    [Description]
    ADD PGTABLE_PA_35_EN for mt6765
    
    [Test]
    iommu basic test pass
    
    MTK-Commit-Id: 01a3789d5e2c21f90fe8be349cc220cc7ce978fe
    
    Change-Id: Ib72206571b1e80d8584f7f646efc1895cd41e75b
    CR-Id: ALPS09294092
    Signed-off-by: "ot_vikram.b" <ot_vikram.b@mediatek.com>
    Feature: iommu_kernel

--- drivers/iommu/mtk_iommu.c
+++ drivers/iommu/mtk_iommu.c
@@ -3576,7 +3576,8 @@ static const struct mtk_iommu_plat_data mt6761_data = {
 static const struct mtk_iommu_plat_data mt6765_data = {
 	.m4u_plat      = M4U_MT6765,
 	.flags         = HAS_SUB_COMM | OUT_ORDER_WR_EN | WR_THROT_EN |
-			 NOT_STD_AXI_MODE | SHARE_PGTABLE | HAS_EMI_PM,
+			 NOT_STD_AXI_MODE | SHARE_PGTABLE |
+			 HAS_EMI_PM | PGTABLE_PA_35_EN,
 	.inv_sel_reg   = REG_MMU_INV_SEL_GEN1,
 	.iova_region   = single_domain,
 	.iova_region_nr = ARRAY_SIZE(single_domain),
