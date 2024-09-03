545b13ec658129107c2bd3ad46527fd75edd6e95
Chris-YC Chen <chris-yc.chen@mediatek.com>
2024-09-03 15:05:44 +0800

    [ALPS09309087] mml: alpha resize aal width check
    [Description]
    alpha resize aal width check
    
    [Test]
    VP
    
    MTK-Commit-Id: 97fb312cae03e6234ffb990d167a251123a2254e
    
    Signed-off-by: Chris-YC Chen <chris-yc.chen@mediatek.com>
    CR-Id: ALPS09309087
    Feature: mml
    Change-Id: I08f3cdadc4b2f87ccef40b70336ab062f5c9f1e1

--- drivers/gpu/drm/mediatek/mml/mtk-mml-tp-mt6991.c
+++ drivers/gpu/drm/mediatek/mml/mtk-mml-tp-mt6991.c
@@ -1581,7 +1581,7 @@ static enum mml_mode tp_query_mode(struct mml_dev *mml, struct mml_frame_info *i
 		if (!MML_FMT_ALPHA(info->src.format) ||
 		    info->src.width <= 32 ||
 		    info->dest_cnt != 1 ||
-		    info->dest[0].crop.r.width <= 32 ||
+		    info->dest[0].crop.r.width < 50 ||
 		    info->dest[0].compose.width <= 9)
 			goto not_support;
 		if (mml_isdc(info->mode))
