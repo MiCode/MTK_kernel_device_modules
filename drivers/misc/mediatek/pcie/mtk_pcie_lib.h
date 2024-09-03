e0d3d822718dd270863580dff46a631f448b584e
Jian Yang <jian.yang@mediatek.com>
2024-09-03 15:06:10 +0800

    [ALPS09321316] revert "PCI: misc: Compliance test code integration"
    [Description]
    This reverts commit ea12818e8966355e679926183aa39516a137a92b
    
    [Test]
    OK
    
    MTK-Commit-Id: 59133fbecf2ed555b78656c317f1b463116a038f
    
    Signed-off-by: Jian Yang <jian.yang@mediatek.com>
    CR-Id: ALPS09321316
    Feature: pcie
    Change-Id: I93fee22bda96e52edcbe9169f18d3df554b2ee6d

--- drivers/misc/mediatek/pcie/mtk_pcie_lib.h
+++ drivers/misc/mediatek/pcie/mtk_pcie_lib.h
@@ -15,33 +15,12 @@
 #include <linux/of_platform.h>
 #include <linux/pci.h>
 #include <linux/phy/phy.h>
-#include <linux/platform_device.h>
 #include <linux/reset.h>
 #include <linux/slab.h>
 
 #define PCIE_PORT_MAX		4
 #define BUF_SIZE		50
 #define MAX_BUF_SZ		2000
-
-enum PCIE_PIPE_RATE {
-	PIPE_RATE_GEN1 = 0,
-	PIPE_RATE_GEN2,
-	PIPE_RATE_GEN3,
-	PIPE_RATE_GEN4,
-};
-
-struct mtk_pcie_info;
-
-struct pcie_test_lib {
-	int (*loopback)(struct mtk_pcie_info *smt_info, int port);
-	int (*compliance)(struct mtk_pcie_info *smt_info, int port, char *cmd,
-			  char *preset);
-};
-
-struct preset_cb {
-	char *name;
-	void (*cb)(void __iomem *phy_base, int lane_num);
-};
 
 struct mtk_pcie_info {
 	char *name;
@@ -56,10 +35,8 @@ struct mtk_pcie_info {
 	unsigned int *max_lane;
 	unsigned int *max_speed;
 	unsigned int max_port;
-	unsigned int eye[4];
+	unsigned int eye[PCIE_LM_EYE_AXIS];
 	char response[MAX_BUF_SZ];
-	struct pcie_test_lib *test_lib[PCIE_PORT_MAX];
-	struct platform_device *pdev[PCIE_PORT_MAX];
 
 	/* char device */
 	struct cdev smt_cdev;
@@ -69,9 +46,5 @@ struct mtk_pcie_info {
 
 unsigned int mtk_get_value(char *str);
 int mtk_pcie_lane_margin_entry(struct mtk_pcie_info *pcie_smt, int port, int mode);
-
-extern struct pcie_test_lib pcie_sphy2_test_lib;
-extern struct pcie_test_lib pcie_sphy3_test_lib;
-extern struct pcie_test_lib pcie_sphy4_test_lib;
 
 #endif /*__MTK_PCIE_LIB_H__*/
