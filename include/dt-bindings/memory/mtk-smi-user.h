8d574e74b8d113a2320c8bd94a5f7a707200dee3
Sabrina Kuo <Yi.Kuo@mediatek.com>
2025-01-19 07:03:53 +0800

    [ALPS08459700] smi: add dt-binding include for smi-user-id
    add dt-binding include for smi-user-id
    
    MTK-Commit-Id: 77459ba2f76c7aa1a8136f39226479deb42f1c6d
    
    Signed-off-by: Sabrina Kuo <Yi.Kuo@mediatek.com>
    CR-Id: ALPS08459700
    Feature: smi driver
    Change-Id: I68a6aa364e62069e6db26995019c653518600714

---
+++ include/dt-bindings/memory/mtk-smi-user.h
@@ -0,0 +1,59 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2023 MediaTek Inc.
+ * Author: Yc Li <yc.li@mediatek.com>
+ */
+#ifndef MTK_SMI_USER_H
+#define MTK_SMI_USER_H
+
+
+#define MTK_SMI_DRIVER          (0)
+/* disp */
+#define MTK_SMI_DISP            (1)
+/* MML */
+#define MTK_SMI_MML0            (2)
+#define MTK_SMI_MML1            (3)
+/* VDEC */
+#define MTK_SMI_VDEC_CORE       (4)
+#define MTK_SMI_VDEC_LAT        (5)
+/* VECN */
+#define MTK_SMI_VENC0           (6)
+#define MTK_SMI_VENC1           (7)
+#define MTK_SMI_VENC2           (8)
+#define MTK_SMI_JPEGDEC0        (9)
+#define MTK_SMI_JPEGDEC1        (10)
+#define MTK_SMI_JPEGENC0        (11)
+#define MTK_SMI_JEPGENC1        (12)
+/* CAM */
+#define MTK_SMI_CCU             (13)
+#define MTK_SMI_CAMSV1          (14)
+#define MTK_SMI_CAMSV2          (15)
+#define MTK_SMI_CAMSV3          (16)
+#define MTK_SMI_PDA1            (17)
+#define MTK_SMI_PDA2            (18)
+#define MTK_SMI_MRAW1           (19)
+#define MTK_SMI_MRAW2           (20)
+#define MTK_SMI_CAM_RAW         (21)
+#define MTK_SMI_CAM_RAWA        (22)
+#define MTK_SMI_CAM_RAWB        (23)
+#define MTK_SMI_CAM_RAWC        (24)
+#define MTK_SMI_CAM_YUVA        (25)
+#define MTK_SMI_CAM_YUVB        (26)
+#define MTK_SMI_CAM_YUVC        (27)
+#define MTK_SMI_CAM_AOV         (28)
+/* IMG */
+#define MTK_SMI_IMG_DPE         (29)
+#define MTK_SMI_IMG_FW          (30)
+#define MTK_SMI_IMG_DIP         (31)
+#define MTK_SMI_IMG_TRAW        (32)
+#define MTK_SMI_IMG_WPE_EIS     (33)
+#define MTK_SMI_IMG_WPE_TNR     (34)
+#define MTK_SMI_IMG_WPE_LITE    (35)
+#define MTK_SMI_IMG_AOV         (36)
+
+/* MMINFRA */
+#define MTK_SMI_MMINFRA         (37)
+
+#define MTK_SMI_USER_ID_NR      (38)
+
+#endif
