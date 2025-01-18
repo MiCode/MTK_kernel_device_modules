1f761977bf7d55ed31ecf2b95c10646c26417e21
mtk12211 <shang.zhou@mediatek.com>
2025-01-19 07:27:49 +0800

    [ALPS08798046] DISP: Fix bypass PQ in AOD issue
    [Description]
    PQ need be bypassed in AOD mode.
    Correct the find CRTC access interate connectors method.
    
    [Test]
    AOD, Display PDAS, ME stress test
    
    MTK-Commit-Id: d02eacddf778ae2e56d6ec8ff1cbae329ed4536a
    
    Signed-off-by: mtk12211 <shang.zhou@mediatek.com>
    CR-Id: ALPS08798046
    Feature: Always On Display
    Change-Id: Id5865216f0291963a15da04de7d322a4123a29c9

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_drv.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_drv.c
@@ -1090,6 +1090,8 @@ static void mtk_atomic_doze_preparation(struct drm_device *dev,
 	struct drm_connector *connector;
 	struct drm_connector_state *old_conn_state;
 	int i;
+	struct mtk_drm_crtc *mtk_crtc = NULL;
+	struct mtk_ddp_comp *comp = NULL;
 
 	for_each_new_connector_in_state(old_state, connector,
 		old_conn_state, i) {
@@ -1100,8 +1102,16 @@ static void mtk_atomic_doze_preparation(struct drm_device *dev,
 			continue;
 		}
 
-		if (old_state && old_state->crtcs[i].old_state)
-			mtk_atomit_doze_update_pq(crtc, 0, old_state->crtcs[i].old_state->active);
+		mtk_crtc = to_mtk_crtc(crtc);
+		comp = mtk_ddp_comp_request_output(mtk_crtc);
+
+		if (comp && comp->id == DDP_COMPONENT_DSI0 && old_state->crtcs[0].old_state)
+			mtk_atomit_doze_update_pq(crtc, 0,
+				old_state->crtcs[0].old_state->active);
+
+		if (comp && comp->id == DDP_COMPONENT_DSI1 && old_state->crtcs[3].old_state)
+			mtk_atomit_doze_update_pq(crtc, 0,
+				old_state->crtcs[3].old_state->active);
 
 		mtk_atomic_doze_update_dsi_state(dev, crtc, 1);
 
@@ -1117,6 +1127,8 @@ static void mtk_atomic_doze_finish(struct drm_device *dev,
 	struct drm_connector *connector;
 	struct drm_connector_state *old_conn_state;
 	int i;
+	struct mtk_drm_crtc *mtk_crtc = NULL;
+	struct mtk_ddp_comp *comp = NULL;
 
 	for_each_new_connector_in_state(old_state, connector,
 		old_conn_state, i) {
@@ -1129,8 +1141,16 @@ static void mtk_atomic_doze_finish(struct drm_device *dev,
 
 		mtk_atomic_doze_update_dsi_state(dev, crtc, 0);
 
-		if (old_state && old_state->crtcs[i].old_state)
-			mtk_atomit_doze_update_pq(crtc, 1, old_state->crtcs[i].old_state->active);
+		mtk_crtc = to_mtk_crtc(crtc);
+		comp = mtk_ddp_comp_request_output(mtk_crtc);
+
+		if (comp && comp->id == DDP_COMPONENT_DSI0 && old_state->crtcs[0].old_state)
+			mtk_atomit_doze_update_pq(crtc, 1,
+				old_state->crtcs[0].old_state->active);
+
+		if (comp && comp->id == DDP_COMPONENT_DSI1 && old_state->crtcs[3].old_state)
+			mtk_atomit_doze_update_pq(crtc, 1,
+				old_state->crtcs[3].old_state->active);
 	}
 }
 
