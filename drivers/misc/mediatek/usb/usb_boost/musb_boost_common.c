92067eb14791eed10b347127a0a0e286105eaa96
ot904167 <ot_liang.hu@mediatek.com>
2025-01-19 07:00:14 +0800

    [ALPS08617997] usb_boost: add vcore type name in musb boost
       add vcore type name in musb boost to avoid null pointer
    
    MTK-Commit-Id: b6187728f10ade0184c1ab289234314b4b719814
    
    Signed-off-by: ot904167 <ot_liang.hu@mediatek.com>
    CR-Id: ALPS08617997
    Feature: Configuration
    Change-Id: Ie3dd64e63f7c32d12a6c92fddeac96e6336aecff

--- drivers/misc/mediatek/usb/usb_boost/musb_boost_common.c
+++ drivers/misc/mediatek/usb/usb_boost/musb_boost_common.c
@@ -82,6 +82,7 @@ static char *type_name[_TYPE_MAXID] = {
 	"cpu_freq",
 	"cpu_core",
 	"dram_vcore",
+	"vcore",
 };
 
 enum {
@@ -107,6 +108,7 @@ static struct wakeup_source *usb_boost_ws;
 static int cpu_freq_dft_para[_ATTR_PARA_RW_MAXID] = {1, 3, 300, 0};
 static int cpu_core_dft_para[_ATTR_PARA_RW_MAXID] = {1, 3, 300, 0};
 static int dram_vcore_dft_para[_ATTR_PARA_RW_MAXID] = {1, 3, 300, 0};
+static int vcore_dft_para[_ATTR_PARA_RW_MAXID] = {1, 60, 300, 0};
 static void __usb_boost_empty(void) { return; }
 static void __usb_boost_cnt(void) { trigger_cnt_disabled++; return; }
 static void __usb_boost_id_empty(int id) { return; }
@@ -121,6 +123,7 @@ struct boost_ops {
 struct boost_ops __the_boost_ops = {
 	__usb_boost_empty,
 	{__usb_boost_id_empty,
+	 __usb_boost_id_empty,
 	 __usb_boost_id_empty,
 	 __usb_boost_id_empty} };
 
@@ -410,6 +413,9 @@ static void default_setting(void)
 
 	usb_boost_set_para_and_arg(TYPE_DRAM_VCORE, dram_vcore_dft_para,
 			ARRAY_SIZE(dram_vcore_dft_para), &dram_vcore_dft_arg);
+
+	usb_boost_set_para_and_arg(TYPE_VCORE, vcore_dft_para,
+			ARRAY_SIZE(vcore_dft_para), &dram_vcore_dft_arg);
 }
 
 static int which_attr(struct mtk_usb_boost *inst, struct device_attribute
