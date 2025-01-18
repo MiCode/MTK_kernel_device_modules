44df0d70676d5673d352e4dacce1d1572bb86be3
Kh Hsieh <Kh.Hsieh@mediatek.com>
2025-01-19 07:28:36 +0800

    [ALPS08809715] lpm: mt6991: fix common sodi
    [Description]
    1. Remove common sodi w/ hrtimer.
    2. Remove common sodi timer.
    3. Remove code which disable common sodi on suspend flow.
    [Test]
    Suspend/SODI/D7X
    
    MTK-Commit-Id: 114d7f8f45f156a8a83c44f0ac733dcbd1d57560
    
    Signed-off-by: Kh Hsieh <Kh.Hsieh@mediatek.com>
    CR-Id: ALPS08809715
    Feature: Sodi
    Change-Id: I9e411095eca11e057ae85cd19896eaabfe0ff9a8

--- drivers/misc/mediatek/lpm/modules/debug/v2/lpm_dbg_pm.c
+++ drivers/misc/mediatek/lpm/modules/debug/v2/lpm_dbg_pm.c
@@ -15,9 +15,6 @@
 #include <lpm.h>
 #include <lpm_module.h>
 #include <lpm_dbg_common_v2.h>
-#include <lpm_timer.h>
-
-static struct hrtimer common_sodi_enable_timer;
 
 static int lpm_dbg_probe(struct platform_device *pdev)
 {
@@ -26,7 +23,6 @@ static int lpm_dbg_probe(struct platform_device *pdev)
 
 #define LPM_KERNEL_SUSPEND "lpm-kernel-suspend"
 #define COMMON_SODI_DTS_NAME "common-sodi"
-#define COMMON_SODI_ENABLE_MS 120000
 
 int kernel_suspend_only;
 
@@ -64,17 +60,6 @@ static struct platform_driver lpm_dbg_driver = {
 	}
 };
 
-static enum hrtimer_restart common_sodi_enable_func(struct hrtimer *timer)
-{
-	lpm_smc_spm_dbg(MT_SPM_DBG_SMC_COMMON_SODI5_CTRL,
-		MT_LPM_SMC_ACT_SET,
-		1, 0);
-	pr_info("[name:mtk_lpm][P] - common sodi5 = %d (%s:%d)\n",
-					1, __func__, __LINE__);
-
-	return HRTIMER_NORESTART;
-};
-
 
 int lpm_dbg_pm_init(void)
 {
@@ -102,15 +87,11 @@ int lpm_dbg_pm_init(void)
 
 		ret = of_property_read_u32(lpm_node, COMMON_SODI_DTS_NAME, &common_sodi_enable);
 
-		if (ret == 0) {
-			if (common_sodi_enable) {
-				hrtimer_init(&common_sodi_enable_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
-				common_sodi_enable_timer.function = common_sodi_enable_func;
-				hrtimer_start(&common_sodi_enable_timer,
-					ms_to_ktime(COMMON_SODI_ENABLE_MS),
-					HRTIMER_MODE_REL);
-			}
-		} else
+		if (ret == 0)
+			lpm_smc_spm_dbg(MT_SPM_DBG_SMC_COMMON_SODI5_CTRL,
+					MT_LPM_SMC_ACT_SET,
+					common_sodi_enable, 0);
+		else
 			common_sodi_enable = 0;
 
 		of_node_put(lpm_node);
@@ -118,10 +99,8 @@ int lpm_dbg_pm_init(void)
 		common_sodi_enable = 0;
 	}
 
-	if (common_sodi_enable == 0)
-		pr_info("[name:mtk_lpm][P] - common sodi5 = %d (%s:%d)\n",
-						0, __func__, __LINE__);
-
+	pr_info("[name:mtk_lpm][P] - common sodi5 = %d (%s:%d)\n",
+					common_sodi_enable, __func__, __LINE__);
 	pr_info("[name:mtk_lpm][P] - kernel suspend only = %d (%s:%d)\n",
 					kernel_suspend_only, __func__, __LINE__);
 
