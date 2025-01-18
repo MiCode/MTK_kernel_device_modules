3358703e86677b51ef19e43d63bdebcf6ab2257c
chih-hong.yang <chih-hong.yang@mediatek.com>
2025-01-19 07:44:25 +0800

    [ALPS08795459] Audio: avoid pop
    [Description]
    1. when retry calibration, power off -> on sequence step
    by step instead of off on adc directly
    
    [Test]
    1. Wechat record
    2. basic playback/record UT
    
    MTK-Commit-Id: cfba19981a84a3402905cc6bfbc13643d661ecf5
    
    Signed-off-by: chih-hong.yang <chih-hong.yang@mediatek.com>
    CR-Id: ALPS08795459
    Feature: Record Preprocessing Enhancement (BesRecord)
    Change-Id: I955753bf1264821a7f2f94622d4ef8da58108aa3

--- sound/soc/codecs/mt6681.c
+++ sound/soc/codecs/mt6681.c
@@ -7971,13 +7971,14 @@ static int mt_adc_l_event(struct snd_soc_dapm_widget *w,
 					"%s(), adc_l calibration fail, resetting...\n",
 					__func__);
 				/* Disable audio L ADC */
-				regmap_update_bits(priv->regmap, MT6681_AUDENC_PMU_CON1,
-						   RG_AUDADCLPWRUP_MASK_SFT,
-						   0x0 << RG_AUDADCLPWRUP_SFT);
+				regmap_write(priv->regmap, MT6681_AUDENC_PMU_CON0, 0x0);
+				regmap_write(priv->regmap, MT6681_AUDENC_PMU_CON1, 0x0);
 				/* Enable audio L ADC */
-				regmap_update_bits(priv->regmap, MT6681_AUDENC_PMU_CON1,
-						   RG_AUDADCLPWRUP_MASK_SFT,
-						   0x1 << RG_AUDADCLPWRUP_SFT);
+				regmap_write(priv->regmap, MT6681_AUDENC_PMU_CON0, 0x4);
+				regmap_write(priv->regmap, MT6681_AUDENC_PMU_CON0, 0x6);
+				regmap_write(priv->regmap, MT6681_AUDENC_PMU_CON0, 0x7);
+				regmap_write(priv->regmap, MT6681_AUDENC_PMU_CON1, 0x40);
+				regmap_write(priv->regmap, MT6681_AUDENC_PMU_CON1, 0x50);
 				usleep_range(500, 520);
 				regmap_read(priv->regmap, MT6681_AUDENC_PMU_CON32,
 					    &rc_tune);
@@ -8418,13 +8419,14 @@ static int mt_adc_r_event(struct snd_soc_dapm_widget *w,
 					 "%s(), adc_r calibration fail, resetting...\n",
 					 __func__);
 				/* Disable audio R ADC */
-				regmap_update_bits(priv->regmap, MT6681_AUDENC_PMU_CON3,
-						   RG_AUDADCRPWRUP_MASK_SFT,
-						   0x0 << RG_AUDADCRPWRUP_SFT);
+				regmap_write(priv->regmap, MT6681_AUDENC_PMU_CON2, 0x0);
+				regmap_write(priv->regmap, MT6681_AUDENC_PMU_CON3, 0x0);
 				/* Enable audio R ADC */
-				regmap_update_bits(priv->regmap, MT6681_AUDENC_PMU_CON3,
-						   RG_AUDADCRPWRUP_MASK_SFT,
-						   0x1 << RG_AUDADCRPWRUP_SFT);
+				regmap_write(priv->regmap, MT6681_AUDENC_PMU_CON2, 0x84);
+				regmap_write(priv->regmap, MT6681_AUDENC_PMU_CON2, 0x86);
+				regmap_write(priv->regmap, MT6681_AUDENC_PMU_CON2, 0x87);
+				regmap_write(priv->regmap, MT6681_AUDENC_PMU_CON3, 0x40);
+				regmap_write(priv->regmap, MT6681_AUDENC_PMU_CON3, 0x50);
 				usleep_range(500, 520);
 				regmap_read(priv->regmap, MT6681_AUDENC_PMU_CON33,
 					    &rc_tune);
