629a0d2013b7fabbc1f67b8a68f7d3e05b7d3bbc
Rocco Yue <rocco.yue@mediatek.com>
2025-01-19 07:34:51 +0800

    [ALPS08829097] ccci: add legacy chip MD EE reg dump
    [Description]
    Legacy chips using lk+ATF do not support dumping MD debug
    registers in the security word.
    Now, in conjunction with the legacy chip kernel upgrade,
    we have re-implemented the function of dumping MD registers
    for these legacy chips in the kernel. Moreover, since
    different platforms require dumping different registers,
    we have divided them according to the AP platform.
    
    gen 93: mt6768 + mt6761/mt6765;
    gen 95: mt6781;
    gen 97: mt6885/mt6893/mt6853 + mt6877/mt6833;
    
    [Test]
    mt6991, mt6768, mt6877, mt6897, mt6893 build pass, MD EE dump
    test, PDAS test pass.
    
    MTK-Commit-Id: a23f5c6f4538189eb144938b1e710ec9a4e8ff5d
    
    Signed-off-by: Rocco Yue <rocco.yue@mediatek.com>
    CR-Id: ALPS08829097
    Feature: CCCI_HIF_CONTROL
    Change-Id: I0f3e294034cdecc31fb23ab240c8c5d1517323f1

--- drivers/misc/mediatek/eccci/fsm/ap_md_reg_dump.h
+++ drivers/misc/mediatek/eccci/fsm/ap_md_reg_dump.h
@@ -6,6 +6,11 @@
 #ifndef __AP_MD_REG_DUMP_H__
 #define __AP_MD_REG_DUMP_H__
 
+#include "md_sys1_platform.h"
+#include "modem_reg_base.h"
+#include "modem_secure_base.h"
+#include "modem_sys.h"
+
 enum MD_REG_ID {
 	MD_REG_SET_DBGSYS_TIME_OUT_ADDR = 0,
 	MD_REG_PC_MONITOR_ADDR,
@@ -35,9 +40,21 @@ enum md_dump_flag {
 #define MD_REG_DUMP_MAGIC   (0x44554D50) /* DUMP */
 extern struct ccci_plat_val md_cd_plat_val_ptr;
 
-void md_dump_register_6873(void);
+void legacy_md_dump_regmd_dump_register(struct ccci_modem *md);
 void md_dump_reg(struct ccci_modem *md);
 extern u32 get_expected_boot_status_val(void);
 extern void md_cd_lock_modem_clock_src(int locked);
+size_t mdreg_write32(size_t reg_id, size_t value);
+
+// md gen93 that dump md register in kernel
+extern void md_dump_register_for_mt6765(struct ccci_modem *md);
+extern void md_dump_register_for_mt6768(struct ccci_modem *md);
+
+// md gen95 that dump md register in kernel
+extern void md_dump_register_for_mt6781(struct ccci_modem *md);
+
+// md gen97 that dump md register in kernel
+extern void md_dump_register_for_mt6877(struct ccci_modem *md);
+extern void md_dump_register_for_mt6885(struct ccci_modem *md);
 
 #endif
---
+++ drivers/misc/mediatek/eccci/fsm/ap_md_reg_dump_gen95.c
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2024 MediaTek Inc.
+ */
+
+#include "ap_md_reg_dump.h"
+
+#define TAG "mcd"
+
+void md_dump_register_for_mt6781(struct ccci_modem *md)
+{
+    /* implement later */
+}
