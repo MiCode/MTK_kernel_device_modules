e4d3d6ccf18307841db2e07036b568bb6d5e52bd
mtk26156 <Yuri.chen@mediatek.com>
2025-01-19 07:07:30 +0800

    [ALPS08545858] r48 GPU: align to r46 APO
    align r48 to r46 APO
    
    MTK-Commit-Id: 4d501d4fb8056b44998769a6b3f89ae02f03973a
    
    CR-Id: ALPS08545858
    Feature: GPU DVFS Policy
    Change-Id: I978b64d3978aa269d8f3c18f36745fe438bad479
    Signed-off-by: mtk26156 <Yuri.chen@mediatek.com>

--- drivers/gpu/mediatek/ged/include/ged_notify_sw_vsync.h
+++ drivers/gpu/mediatek/ged/include/ged_notify_sw_vsync.h
@@ -41,9 +41,9 @@ enum gpu_fallback_mode {
 	ALIGN_FAST_DVFS,
 };
 
-#if IS_ENABLED(CONFIG_MTK_GPU_APO_SUPPORT)
 enum gpu_apo_support {
 	APO_NORMAL_SUPPORT = 1,
+	APO_2_0_NORMAL_SUPPORT,
 	APO_NORMAL_AND_LP_SUPPORT,
 	APO_INVALID_SUPPORT,
 };
@@ -53,7 +53,6 @@ enum gpu_apo_hint {
 	APO_LP_HINT,
 	APO_INVALID_HINT,
 };
-#endif /* CONFIG_MTK_GPU_APO_SUPPORT */
 
 enum gpu_dvfs_policy_state ged_get_policy_state(void);
 enum gpu_dvfs_policy_state ged_get_prev_policy_state(void);
