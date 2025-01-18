515196edfa377dc58fe97b00035d2103ff217d27
ot_hui.liao <ot_hui.liao@mediatek.com>
2025-01-19 07:52:18 +0800

    [ALPS08678993] cmdq: add mms AIDL service and manifest
    [Description]
    Revert "[ALPS08678993] cmdq:porting for mt6877"
    
    This reverts commit 2fa50af14075d5b29eac0ae9cada48a5cfcf43bc.
    
    Reason for revert: <Workaround revert>
    
    [Test]
    Boot pass and pass MDP basic test.
    
    MTK-Commit-Id: 12866c6a48a69bbf4a94f98eba64ce5f9de3200d
    
    Change-Id: Icd17300a2c87787942935f594c25cdd1bc75024d
    Signed-off-by: ot_hui.liao <ot_hui.liao@mediatek.com>
    CR-Id: ALPS08678993
    Feature: cmdq_gce

--- drivers/misc/mediatek/cmdq/mailbox/cmdq-util.c
+++ drivers/misc/mediatek/cmdq/mailbox/cmdq-util.c
@@ -1295,8 +1295,7 @@ void cmdq_util_devapc_dump(void)
 }
 EXPORT_SYMBOL(cmdq_util_devapc_dump);
 
-#if IS_ENABLED(CONFIG_DEVICE_MODULES_MTK_DEVAPC) && !IS_ENABLED(CONFIG_MTK_GPU_MT6877_SUPPORT) &&\
-!IS_ENABLED(CONFIG_MTK_SMI_BWC_MT6761)
+#if IS_ENABLED(CONFIG_DEVICE_MODULES_MTK_DEVAPC)
 static struct devapc_vio_callbacks devapc_vio_handle = {
 	.id = INFRA_SUBSYS_GCE,
 	.debug_dump = cmdq_util_devapc_dump,
@@ -1483,8 +1482,7 @@ int cmdq_util_init(void)
 	if (exists)
 		dput(dir);
 
-#if IS_ENABLED(CONFIG_DEVICE_MODULES_MTK_DEVAPC) && !IS_ENABLED(CONFIG_MTK_GPU_MT6877_SUPPORT) &&\
-!IS_ENABLED(CONFIG_MTK_SMI_BWC_MT6761)
+#if IS_ENABLED(CONFIG_DEVICE_MODULES_MTK_DEVAPC)
 	register_devapc_vio_callback(&devapc_vio_handle);
 #endif
 
