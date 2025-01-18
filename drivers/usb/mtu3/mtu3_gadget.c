6649e199fbc5da0feefa581b0cace016b292ef6d
yu-chen.liu <yu-chen.liu@mediatek.com>
2025-01-19 07:19:25 +0800

    [ALPS08659114] mtu3: sync upstream patch 2024_02_18-6
    [Description]
    usb: mtu3: unlock @mtu->lock just before giving back request
    
    No need unlock @mtu->lock when unmap request, unlock it just before
    giving back request, due to we do not lock this spinlock when map
    the request.
    
    Reviewed-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
    Signed-off-by: Chunfeng Yun <chunfeng.yun@mediatek.com>
    Link: https://lore.kernel.org/r/20230417025203.18097-4-chunfeng.yun@mediatek.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    [Test]
    USB Device
    
    MTK-Commit-Id: aa748b425a230a38dbd057216c9ff64b206f7237
    
    Signed-off-by: yu-chen.liu <yu-chen.liu@mediatek.com>
    CR-Id: ALPS08659114
    Feature: usb_ssusb
    Change-Id: Ib1303a7cc6589b384e24169aadd1f19e90f00a4a

--- drivers/usb/mtu3/mtu3_gadget.c
+++ drivers/usb/mtu3/mtu3_gadget.c
@@ -94,7 +94,6 @@ __acquires(mep->mtu->lock)
 		req->status = status;
 
 	trace_mtu3_req_complete(mreq);
-	spin_unlock(&mtu->lock);
 
 	/* ep0 makes use of PIO, needn't unmap it */
 	if (mep->epnum)
@@ -103,6 +102,7 @@ __acquires(mep->mtu->lock)
 	dev_dbg(mtu->dev, "%s complete req: %p, sts %d, %d/%d\n",
 		mep->name, req, req->status, req->actual, req->length);
 
+	spin_unlock(&mtu->lock);
 	usb_gadget_giveback_request(&mep->ep, req);
 	spin_lock(&mtu->lock);
 }
