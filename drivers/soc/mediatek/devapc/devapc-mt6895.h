5023e85e470355b36ea940575ff3c55fefb76977
Jackson Chang <jackson-kt.chang@mediatek.com>
2025-01-19 00:55:26 +0800

    [ALPS06637896] devapc: add common ko support
    Add devapc common ko support
    
    MTK-Commit-Id: d68d54b94e4392d73e1c9fa49cacacbbaf02d1a1
    
    Signed-off-by: Jackson Chang <jackson-kt.chang@mediatek.com>
    CR-Id: ALPS06637896
    Feature: devapc
    Change-Id: I0aa469d2391564b28d2f9673162dec0568691eb8

--- drivers/soc/mediatek/devapc/devapc-mt6895.h
+++ drivers/soc/mediatek/devapc/devapc-mt6895.h
@@ -1800,14 +1800,4 @@ enum DEVAPC_VIO_SLAVE_NUM {
 #endif
 };
 
-int __weak mmup_enable_count(void)
-{
-	return 0;
-}
-
-bool __weak is_adsp_feature_in_active(void)
-{
-	return 0;
-}
-
 #endif /* __DEVAPC_MT6895_H__ */
