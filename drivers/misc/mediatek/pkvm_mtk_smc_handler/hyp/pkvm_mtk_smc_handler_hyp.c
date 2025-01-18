75b1a923aff96c733262582ba4fee0af13e3e7cf
Rock.Yeh <rock.yeh@mediatek.com>
2025-01-19 07:46:09 +0800

    [ALPS08686299] add a method to decouple el1 and el2
    [Description]
    We encountered a symbol coupled problem in GZ project.
    pKVM provides vendor module for vendor to develop their
    own feature for el2, but MTK has other hypervisor project.
    In non-pKVM project, some el1 common modules may refer
    to pKVMs module symbols, then it will get Unknown
    symbol error. For this problem, we create a table in
    el2 and use smc call to replace a symbol calling.
    
    [Test]
    - boot
    - smc test
    
    MTK-Commit-Id: 85d99cc120c5c7d05ebe49f6c2cb6faff1075f52
    
    Signed-off-by: Rock.Yeh <rock.yeh@mediatek.com>
    CR-Id: ALPS08686299
    Feature: trusted_mem
    Change-Id: Iefe306e4a6fc023ad020fc3ba065f5f5624c82dd

--- drivers/misc/mediatek/pkvm_mtk_smc_handler/hyp/pkvm_mtk_smc_handler_hyp.c
+++ drivers/misc/mediatek/pkvm_mtk_smc_handler/hyp/pkvm_mtk_smc_handler_hyp.c
@@ -7,25 +7,81 @@
 #include <asm/kvm_pkvm_module.h>
 
 #include "../../include/pkvm_mtk_smc_handler/pkvm_mtk_smc_handler.h"
+#include "../../../../arch/arm64/kvm/hyp/include/nvhe/spinlock.h"
 
-// Here saves all el2 smc call ids for blocking any invlid el1 smc
+// Here saves all el2 smc call ids for blocking any invalid el1 smc
 // call to use el2 smc call ids
 static const uint64_t el2_smc_id_list[] = {
 };
 
-bool mtk_smc_handler(struct user_pt_regs *ctxt)
+struct nlist {
+	u64 key;
+	int val;
+};
+
+#define HVC_TABLE_SIZE 64
+static struct nlist hvc_table[HVC_TABLE_SIZE];
+static u32 hvc_cur_index;
+static hyp_spinlock_t handler_glist_lock;
+
+void add_hvc(u64 smc_id, int hvc_id)
+{
+	hyp_spin_lock(&handler_glist_lock);
+	hvc_table[hvc_cur_index].key = smc_id;
+	hvc_table[hvc_cur_index].val = hvc_id;
+	hvc_cur_index++;
+	hyp_spin_unlock(&handler_glist_lock);
+}
+
+int lookup_hvc(u64 smc_id)
 {
 	int i = 0;
-	uint64_t func_id = ctxt->regs[0] & ~ARM_SMCCC_CALL_HINTS;
+	int max_size = hvc_cur_index < HVC_TABLE_SIZE ? hvc_cur_index : HVC_TABLE_SIZE;
+
+	for (i = 0; i < max_size; i++) {
+		if (hvc_table[i].key == smc_id)
+			return hvc_table[i].val;
+	}
+	return -1;
+}
+
+bool mtk_smc_handler(struct user_pt_regs *ctxt)
+{
+	u64 smc_id = ctxt->regs[0] & ~ARM_SMCCC_CALL_HINTS;
+	u64 smc_key_id;
+	int hvc_id;
+	int i = 0;
 
 	for (i = 0; i < ARRAY_SIZE(el2_smc_id_list); i++) {
-		if (func_id == el2_smc_id_list[i])
+		if (smc_id == el2_smc_id_list[i])
 			return true;
+	}
+
+	hvc_id = lookup_hvc(smc_id);
+	if (hvc_id != -1) {
+		ctxt->regs[1] = hvc_id;
+		return true;
+	}
+
+	switch (smc_id){
+	case SMC_ID_MTK_PKVM_ADD_HVC:
+		hvc_id = lookup_hvc(smc_id);
+		if (hvc_id != -1) {
+			ctxt->regs[0] = SMC_RET_MTK_PKVM_SMC_HANDLER_DUPLICATED_ID;
+			ctxt->regs[1] = hvc_id;
+			return true;
+		}
+
+		smc_key_id = ctxt->regs[1];
+		hvc_id = ctxt->regs[2];
+		add_hvc(smc_key_id, hvc_id);
+		return true;
 	}
 	return false;
 }
 
 int mtk_smc_handler_hyp_init(const struct pkvm_module_ops *ops)
 {
+	hyp_spin_lock_init(&handler_glist_lock);
 	return ops->register_host_smc_handler(mtk_smc_handler);
 }
