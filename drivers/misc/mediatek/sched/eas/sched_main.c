da50f819a1d4695fe5baa791956235c831147d91
hank.li <hank.li@mediatek.com>
2025-01-19 07:15:11 +0800

    [ALPS07859874] sched: open tick hook
    open mtk tick hook
    
    MTK-Commit-Id: 4d80a42dae70e8fb11279e93b0e798aee7380eb1
    
    Signed-off-by: hank.li <hank.li@mediatek.com>
    CR-Id: ALPS07859874
    Feature: Energy Aware Scheduling Plus (EAS+)
    Change-Id: I26ed85affc0aa1eac1945e26517f8595c8d1ad01

--- drivers/misc/mediatek/sched/eas/sched_main.c
+++ drivers/misc/mediatek/sched/eas/sched_main.c
@@ -981,8 +981,8 @@ static int __init mtk_scheduler_init(void)
 		pr_info("register trace_android_rvh_cpu_overutilized failed\n");
 
 
-	//ret = register_trace_android_rvh_tick_entry(
-	//		mtk_tick_entry, NULL);
+	ret = register_trace_android_rvh_tick_entry(
+			mtk_tick_entry, NULL);
 	if (ret)
 		pr_info("register android_rvh_tick_entry failed\n");
 
