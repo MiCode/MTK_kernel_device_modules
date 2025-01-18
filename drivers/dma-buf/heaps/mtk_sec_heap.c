83cb018720c40869a97db2354ec3169601b6d0eb
Walter-zh Wu <walter-zh.wu@mediatek.com>
2025-01-19 07:28:35 +0800

    [ALPS06702793] pkvm: refactor is_pkvm_enable checking
    [Description]
    refactor is_pkvm_enable()
    
    [Test]
    build pass
    
    MTK-Commit-Id: 1738a30e54fa059f838551af9ad68db8f583046d
    
    Signed-off-by: Walter-zh Wu <walter-zh.wu@mediatek.com>
    CR-Id: ALPS06702793
    Feature: trusted_mem
    Change-Id: I1293e17b286a1502661a8516b57db714dd76f09e

--- drivers/dma-buf/heaps/mtk_sec_heap.c
+++ drivers/dma-buf/heaps/mtk_sec_heap.c
@@ -1285,11 +1285,9 @@ static int mtee_common_buffer_v2(struct ssheap_buf_info *ssheap, u8 pmm_attr,
 	uint32_t tmp_count = 0;
 	int count = 0;
 
-#if IS_ENABLED(CONFIG_64BIT)
 	/* pkvm smmu vendro moudle is not ready, so do nothing */
-	if (is_protected_kvm_enabled())
+	if (is_pkvm_enabled())
 		return 0;
-#endif
 
 	if (!ssheap || !ssheap->pmm_page) {
 		pr_err("ssheap info not ready!\n");
