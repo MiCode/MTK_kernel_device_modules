5c2d11b8a18c2dcf165ba3646297ea3f106c2dc1
Dennis YC Hsieh <dennis-yc.hsieh@mediatek.com>
2025-01-19 07:51:28 +0800

    [ALPS08910979] mml: reduce mips
    [Description]
    1. Do not reset pq hist completion instance if not enable pq.
    2. Default disable mml log in mml-record.
    
    [Test]
    VP
    
    MTK-Commit-Id: 976ed179bdcf3199e98bfe717b0431c90bb53fc3
    
    Signed-off-by: Dennis YC Hsieh <dennis-yc.hsieh@mediatek.com>
    CR-Id: ALPS08910979
    Feature: mml
    Change-Id: If41507b4d5072554b989695c0622fe05bd6cca01

--- drivers/gpu/drm/mediatek/mml/mtk-mml-core.c
+++ drivers/gpu/drm/mediatek/mml/mtk-mml-core.c
@@ -149,7 +149,11 @@ int mml_err_cnt;
 module_param(mml_err_cnt, int, 0644);
 
 /* control bits see mtk-mml-core.h enum mml_log_buf_setting */
+#if IS_ENABLED(CONFIG_MTK_MML_DEBUG)
 int mml_log_rec = mml_logbuf_msg | mml_logbuf_log | mml_logbuf_krn;
+#else
+int mml_log_rec = mml_logbuf_krn;
+#endif
 EXPORT_SYMBOL(mml_log_rec);
 module_param(mml_log_rec, int, 0644);
 
