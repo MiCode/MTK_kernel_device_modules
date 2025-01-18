c1e88c5aa9642bb29f8fb4c03fa9357de2e6dcc4
Willy GhostKing <Willy-CF.Huang@mediatek.com>
2025-01-19 07:05:11 +0800

    [ALPS08485024] sda: lastbus: add reduce timeout threshold
    [Description]
    Fix sysfs read information
    Fix sysfs write restrictions
    Improve calculate timeout setting function
    Improve dynamic dump debugging log
    add function: provide get and set threshold function
    
    [Test]
    Local test UT/IT
    
    MTK-Commit-Id: 4dc196ce51a83fdfa45a638cbd42c435491d369a
    
    CR-Id: ALPS08485024
    Feature: LastBUS
    Signed-off-by: Willy GhostKing <Willy-CF.Huang@mediatek.com>
    Change-Id: I4c70def1c0dbe26242951c26b3df3ca3f977b6fa

--- drivers/misc/mediatek/sda/last_bus.c
+++ drivers/misc/mediatek/sda/last_bus.c
@@ -29,11 +29,9 @@
 #include "dbg_error_flag.h"
 #include "last_bus.h"
 
-#define DUMP_BUFF_SIZE 0x5000
 static struct cfg_lastbus my_cfg_lastbus;
 static char dump_buf[DUMP_BUFF_SIZE];
-static int dump_buf_size;
-static int aee_dump;
+static int dump_buf_size, aee_dump;
 #if IS_ENABLED(CONFIG_MTK_LASTBUS_DEBUG)
 static struct proc_dir_entry *entry;
 #endif
@@ -57,21 +55,36 @@ static unsigned long long gray_code_to_binary_convert(unsigned long long gray_co
 		gray_code >>= 1;
 		value ^= gray_code;
 	}
-
 	return value;
 }
 
-static unsigned int calculate_timeout_setting(unsigned int bus_freq_mhz, unsigned int timeout_ms)
+static unsigned int revert_timeout(unsigned int bus_freq_mhz, unsigned int timeout_hex)
 {
-	unsigned int value;
+	return ((timeout_hex + 0x1) << 10) / bus_freq_mhz;
+}
 
-	value = ((timeout_ms * 1000 * bus_freq_mhz) >> 10) - 1;
-	pr_info("bus_freq_mhz = %d, timeout_ms = %d, timeout_threshold = 0x%x\n",
-		bus_freq_mhz, timeout_ms, value);
+static unsigned int calculate_timeout_setting(unsigned int bus_freq_mhz, unsigned int timeout_us)
+{
+	unsigned int value = 0;
+
+	/* As Rsh 10, scale for each experiment may increase by multiple times of freq */
+	value = ((timeout_us * bus_freq_mhz) >> 10) - 1;
+	if (value > TIMEOUT_THRES_LIMIT) {
+		value = TIMEOUT_THRES_LIMIT;
+		pr_debug("The original timeout setting value is > max timeout_threshold(%d us) as freq (%d), timeout_threshold: 0x%x\n",
+				revert_timeout(bus_freq_mhz, value), bus_freq_mhz, value);
+	} else if (timeout_us < revert_timeout(bus_freq_mhz, TIMEOUT_THRES_LOWEST)) {
+		value = TIMEOUT_THRES_LOWEST;
+		pr_debug("The original timeout setting value is < min timeout_threshold(%d us) as freq (%d), timeout_threshold: 0x%x\n",
+				revert_timeout(bus_freq_mhz, value), bus_freq_mhz, value);
+	} else {
+		pr_debug("The result you set: bus_freq_mhz = %d, timeout_us = %d, timeout_threshold = 0x%x\n",
+			bus_freq_mhz, revert_timeout(bus_freq_mhz, value), value);
+	}
 	return value;
 }
 
-static void lastbus_init_monitor_v1(struct lastbus_monitor *m, unsigned int timeout_ms,
+static void lastbus_init_monitor_v1(struct lastbus_monitor *m, unsigned int timeout_us,
 		unsigned int timeout_type, void __iomem *base)
 {
 	unsigned int bus_freq_mhz, timeout_setting;
@@ -82,33 +95,21 @@ static void lastbus_init_monitor_v1(struct lastbus_monitor *m, unsigned int time
 			reg_offset = m->idle_masks[i].reg_offset;
 			reg_value = m->idle_masks[i].reg_value;
 			writel(reg_value, base + reg_offset);
-			pr_info("set idle_mask 0x%x = 0x%x\n", m->base + reg_offset, reg_value);
+			pr_debug("set idle_mask 0x%x = 0x%x\n", m->base + reg_offset, reg_value);
 		}
 	}
-
 	/* clear timeout status with DBG_CKEN */
 	writel((LASTBUS_TIMEOUT_CLR | LASTBUS_DEBUG_CKEN), base);
-
 	/* de-assert clear bit with DBG_CKEN */
 	writel(LASTBUS_DEBUG_CKEN, base);
 
-	if (timeout_ms == 0xFFFFFFFF) {
-		/* set maximum timeout for 1st edition*/
-		writel(((0xFFFF << TIMEOUT_THRES_SHIFT) | LASTBUS_DEBUG_CKEN |
-			(timeout_type << TIMEOUT_TYPE_SHIFT)), base);
-		writel(((0xFFFF << TIMEOUT_THRES_SHIFT) | LASTBUS_DEBUG_CKEN | LASTBUS_DEBUG_EN |
-			(timeout_type << TIMEOUT_TYPE_SHIFT)), base);
-	} else {
-		bus_freq_mhz = m->bus_freq_mhz;
-		timeout_setting = calculate_timeout_setting(bus_freq_mhz, timeout_ms);
-		if (timeout_setting > 0xFFFF)
-			timeout_setting = 0xFFFF;
-		writel(((timeout_setting << TIMEOUT_THRES_SHIFT) | LASTBUS_DEBUG_CKEN |
-			(timeout_type << TIMEOUT_TYPE_SHIFT)), base);
-		writel(((timeout_setting << TIMEOUT_THRES_SHIFT) | LASTBUS_DEBUG_CKEN |
-			LASTBUS_DEBUG_EN | (timeout_type << TIMEOUT_TYPE_SHIFT)), base);
-	}
-	pr_info("base setting = 0x%x\n", readl(base));
+	bus_freq_mhz = m->bus_freq_mhz;
+	timeout_setting = calculate_timeout_setting(bus_freq_mhz, timeout_us);
+	writel(((timeout_setting << TIMEOUT_THRES_SHIFT) | LASTBUS_DEBUG_CKEN |
+		(timeout_type << TIMEOUT_TYPE_SHIFT)), base);
+	writel(((timeout_setting << TIMEOUT_THRES_SHIFT) | LASTBUS_DEBUG_CKEN |
+		LASTBUS_DEBUG_EN | (timeout_type << TIMEOUT_TYPE_SHIFT)), base);
+	pr_debug("%s control setting = 0x%x\n", m->name, readl(base));
 }
 
 static void lastbus_dump_monitor(const struct lastbus_monitor *m, void __iomem *base)
@@ -120,19 +121,19 @@ static void lastbus_dump_monitor(const struct lastbus_monitor *m, void __iomem *
 	buf_point = check_buf_size(50);
 	dump_buf_size += snprintf(dump_buf + buf_point, DUMP_BUFF_SIZE - buf_point,
 		"--- %s 0x%08x %d ---\n", m->name, m->base, m->num_ports);
-	pr_info("--- %s 0x%08x %d ---\n", m->name, m->base, m->num_ports);
+	pr_debug("--- %s 0x%08x %d ---\n", m->name, m->base, m->num_ports);
 
 	for (i = 0; i < m->num_ports; i++) {
 		buf_point = check_buf_size(10);
 		dump_buf_size += snprintf(dump_buf + buf_point, DUMP_BUFF_SIZE - buf_point,
 			"%08x\n", readl(base + 0x408 + i * 4));
-		pr_info("%08x\n", readl(base + 0x408 + i * 4));
+		pr_debug("%08x\n", readl(base + 0x408 + i * 4));
 	}
 
 	if (m->num_bus_status != 0) {
-		pr_info("bus busy/idle status:");
+		pr_debug("bus busy/idle status:");
 		for (i = 0; i < m->num_bus_status; i++) {
-			pr_info("0x%x", readl(base + m->offset_bus_status + i * 4));
+			pr_debug("0x%x", readl(base + m->offset_bus_status + i * 4));
 			buf_point = check_buf_size(10);
 			dump_buf_size += snprintf(dump_buf + buf_point, DUMP_BUFF_SIZE - buf_point,
 				"%08x\n", readl(base + m->offset_bus_status + i * 4));
@@ -142,7 +143,7 @@ static void lastbus_dump_monitor(const struct lastbus_monitor *m, void __iomem *
 	grad_code = readl(base + 0x404);
 	grad_code = (grad_code << 32) | readl(base + 0x400);
 	bin_code = gray_code_to_binary_convert(grad_code);
-	pr_info("%s: gray_code = 0x%llx, binary = 0x%llx\n", __func__, grad_code, bin_code);
+	pr_debug("%s: gray_code = 0x%llx, binary = 0x%llx\n", __func__, grad_code, bin_code);
 	buf_point = check_buf_size(50);
 	dump_buf_size += snprintf(dump_buf + buf_point, DUMP_BUFF_SIZE - buf_point,
 		"timestamp: 0x%llx\n", bin_code);
@@ -189,9 +190,9 @@ int lastbus_dump(int force_dump)
 			lastbus_dump_monitor(m, base);
 		}
 		if (is_timeout || force_clean) {
-			lastbus_init_monitor_v1(m, my_cfg_lastbus.timeout_ms,
+			lastbus_init_monitor_v1(m, my_cfg_lastbus.timeout_ms * MS_US_TRANSLATE,
 				my_cfg_lastbus.timeout_type, base);
-			pr_info("%s: status after clean...is_timeout: %d, force_clean: %d.\n",
+			pr_debug("%s: status after clean...is_timeout: %d, force_clean: %d.\n",
 				__func__, (readl(base) & LASTBUS_TIMEOUT), force_clean);
 		}
 		iounmap(base);
@@ -209,7 +210,6 @@ static int last_bus_dump_event(struct notifier_block *this,
 	last_bus_err_status = get_dbg_error_flag_mask(INFRA_LASTBUS_TIMEOUT) |
 				get_dbg_error_flag_mask(PERI_LASTBUS_TIMEOUT);
 
-
 	if (!(err_flag_status & last_bus_err_status)) {
 		pr_err("err_flag_status %lx, last_bus_err_status %lx\n",
 			err_flag_status, last_bus_err_status);
@@ -221,14 +221,12 @@ static int last_bus_dump_event(struct notifier_block *this,
 	if (aee_dump != 0)
 		aee_kernel_exception("last_bus_timeout", "detect last bus timeout");
 #endif
-
 	return 0;
 }
 
 static struct notifier_block dbg_error_flag_notifier_last_bus = {
 	.notifier_call = last_bus_dump_event,
 };
-
 
 static int last_bus_probe(struct platform_device *pdev)
 {
@@ -261,7 +259,7 @@ static int last_bus_probe(struct platform_device *pdev)
 	ret = of_property_read_u32(np, "timeout-ms", &my_cfg_lastbus.timeout_ms);
 	if (ret < 0) {
 		dev_err(dev, "couldn't find property timeout-ms(%d)\n", ret);
-		my_cfg_lastbus.timeout_ms = 0xFFFFFFFF;
+		my_cfg_lastbus.timeout_ms = 1000;
 	}
 
 	/* get timeout_type */
@@ -349,7 +347,6 @@ static int last_bus_probe(struct platform_device *pdev)
 
 	if (my_cfg_lastbus.enabled == 1 && num != 0) {
 		dbg_error_flag_register_notify(&dbg_error_flag_notifier_last_bus);
-
 		my_cfg_lastbus.num_used_monitors = num;
 		if (num > NR_MAX_LASTBUS_MONITOR) {
 			dev_err(dev, "%s: Error: number of monitors(%d) is great than %d!\n",
@@ -386,16 +383,53 @@ static struct platform_driver last_bus_drv = {
 };
 
 #if IS_ENABLED(CONFIG_MTK_LASTBUS_DEBUG)
+static int lastbus_set_by_num(unsigned int bus_num, unsigned int time_scale)
+{
+	struct lastbus_monitor *m = NULL;
+	void __iomem *base;
+
+	if (bus_num > my_cfg_lastbus.num_used_monitors) {
+		pr_info("LastBus input index (%d) is out of bound!\n", bus_num + 1);
+		return 1;
+	}
+
+	m = &my_cfg_lastbus.monitors[bus_num];
+	base = ioremap(m->base, ((0x408 + m->num_ports * 4) / 0x100 + 1) * 0x100);
+	lastbus_init_monitor_v1(m, time_scale, my_cfg_lastbus.timeout_type, base);
+	pr_info("Set new timeout threshold of %s to %d us.\n", m->name,
+		revert_timeout(m->bus_freq_mhz, readl(base) >> TIMEOUT_THRES_SHIFT));
+
+	iounmap(base);
+	return 0;
+}
+
+static int lastbus_get_by_num(unsigned int bus_num)
+{
+	struct lastbus_monitor *m = NULL;
+	void __iomem *base;
+
+	if (bus_num > my_cfg_lastbus.num_used_monitors) {
+		pr_info("LastBus input index (%d) is out of bound!\n", bus_num + 1);
+		return 1;
+	}
+
+	m = &my_cfg_lastbus.monitors[bus_num];
+	base = ioremap(m->base, ((0x408 + m->num_ports * 4) / 0x100 + 1) * 0x100);
+	pr_info("Get %d-%s timeout threshold: %d us.", bus_num + 1, m->name,
+		revert_timeout(m->bus_freq_mhz, readl(base) >> TIMEOUT_THRES_SHIFT));
+
+	iounmap(base);
+	return 0;
+}
+
 static int last_bus_show(struct seq_file *m, void *v)
 {
 	int i, point;
 
-
 	if (my_cfg_lastbus.enabled == 1 && my_cfg_lastbus.num_used_monitors != 0) {
-		seq_puts(m, "=== Last bus monitor: ===\n");
-
+		seq_puts(m, "=== Last bus monitor ===\n");
 		for (i = 0; i < my_cfg_lastbus.num_used_monitors; i++) {
-			seq_printf(m, "monitor-name: %s, ports %d.\n",
+			seq_printf(m, "monitor %d named %s has %d ports to dump.\n", i + 1,
 				my_cfg_lastbus.monitors[i].name,
 				my_cfg_lastbus.monitors[i].num_ports);
 		}
@@ -408,51 +442,96 @@ static int last_bus_show(struct seq_file *m, void *v)
 			point = dump_buf_size % DUMP_BUFF_SIZE;
 			seq_write(m, (char *)dump_buf, point);
 		}
+		seq_puts(m, "\nYou can do some debug action...\n");
+		seq_puts(m, "0: TIMEOUT_DUMP: dump as errflag result.\n");
+		seq_puts(m, "1: FORCE_DUMP: force dump.\n");
+		seq_puts(m, "2: FORCE_CLEAR: force reset all lastbus.\n");
+		seq_puts(m, "3: GET_PER_TMO: get a specific lastbus timeout setting.\n");
+		seq_puts(m, "4: REDUCE_TMO: change timeout threshold(us) of a specific lastbus.\n");
 	} else
 		seq_puts(m, "Last bus monitor is not enabled.\n");
 	return 0;
 }
 
-
 static ssize_t last_bus_write(struct file *filp,
 	const char *ubuf, size_t cnt, loff_t *data)
 {
-	char buf[64];
-	long val;
-	int ret;
+	static uint32_t lastbus_opt[3];
+	char buf[64], *name, *cur = buf;
+	unsigned int val, bus_num, timeout_us, ret, i = 0, tmo_opt = 1;
 
 	if (cnt >= sizeof(buf))
 		return -EINVAL;
 
-	if (copy_from_user(&buf, ubuf, cnt))
+	if (!ubuf) {
+		pr_notice("%s: ubuf = NULL\n", __func__);
+		return -EINVAL;
+	}
+
+	if (copy_from_user(&buf, ubuf, cnt)) {
+		pr_notice("%s: copy error\n", __func__);
 		return -EFAULT;
+	}
 
-	buf[cnt] = 0;
+	while ((name = strsep(&cur, " ")) != NULL) {
+		if (i > ARRAY_SIZE(lastbus_opt)) {
+			pr_notice("Too many input elements.\n");
+			return -EINVAL;
+		}
+		ret = kstrtoint(name, 10, (unsigned int *)(&lastbus_opt[i]));
+		if (ret) {
+			pr_notice("kstrtoint %s error.\n", name);
+			return -EINVAL;
+		}
+		if (lastbus_opt[0] <= FORCE_CLEAR) {
+			tmo_opt = 0;
+			break;
+		}
+		++i;
+	}
 
-	ret = kstrtoul(buf, 10, (unsigned long *)&val);
-
-	if (ret < 0)
-		return ret;
+	val = lastbus_opt[0];
+	if (val >= TOTAL_MODE) {
+		pr_notice("Input mode[opt 0] %d must be < %d.\n", val, TOTAL_MODE);
+		return -EINVAL;
+	}
+	if (tmo_opt) {
+		bus_num = lastbus_opt[1] - 1;
+		timeout_us = lastbus_opt[2];
+		if (bus_num < 0 || timeout_us <= 0) {
+			pr_notice("Input bus_num or timeout_us[%d %d] must be > 0.\n",
+				bus_num + 1, timeout_us);
+			return -EINVAL;
+		}
+	}
 
 	switch (val) {
-	case 0:
+	case TIMEOUT_DUMP:
 		/* dump last bus timeout information */
 		lastbus_dump(TIMEOUT_DUMP);
 		break;
-	case 1:
+	case FORCE_DUMP:
 		/* force dump last bus timeout information */
 		lastbus_dump(FORCE_DUMP);
 		break;
-	case 2:
+	case FORCE_CLEAR:
 		/* force init lastbus to clean status */
 		lastbus_dump(FORCE_CLEAR);
+		break;
+	case GET_PER_TMO:
+		if (lastbus_get_by_num(bus_num))
+			pr_info("Get timeout fail.\n");
+		break;
+	case REDUCE_TMO:
+		/* reduce lastbus timeout */
+		if (lastbus_set_by_num(bus_num, timeout_us))
+			pr_info("Set new timeout fail.\n");
 		break;
 	default:
 		break;
 	}
 	return cnt;
 }
-
 
 /*** Seq operation of last_bus ****/
 static int last_bus_open(struct inode *inode, struct file *file)
