8e873979d7165c8720249b3ded124f262e15783f
yan-hong.zhang <yan-hong.zhang@mediatek.com>
2025-01-19 06:59:56 +0800

    [ALPS08606670] gpueb: mt6989: Add api for clear gpueb to apmcu irq
    Add api for clear gpueb to apmcu IRQ[1].
    
    MTK-Commit-Id: f6bdf0c5265833bddc6c9f008ffc20c4add786f7
    
    CR-Id: ALPS08606670
    Feature: GPUEB firmware
    Change-Id: I3f669c0ba50e553b6f9fb3cb708f4cbc731fef8a
    Signed-off-by: yan-hong.zhang <yan-hong.zhang@mediatek.com>

--- drivers/gpu/mediatek/gpueb/gpueb_ipi.c
+++ drivers/gpu/mediatek/gpueb/gpueb_ipi.c
@@ -39,6 +39,7 @@ const char *gpueb_mbox_pin_recv_name[20];
 unsigned int g_mbox_size = 0;
 unsigned int g_slot_size = 0;
 unsigned int g_ts_mbox;
+static void __iomem *g_gpueb_mbox_ipi;
 
 #if IPI_TEST
 struct test_msg {
@@ -311,6 +312,9 @@ int gpueb_ipi_init(struct platform_device *pdev)
 	int i = 0;
 	int ret;
 
+	struct device *gpueb_dev = &pdev->dev;
+	struct resource *res = NULL;
+
 	ret = gpueb_ipi_table_init(pdev);
 	if (ret == 0)
 		return -ENODEV;
@@ -379,6 +383,17 @@ int gpueb_ipi_init(struct platform_device *pdev)
 		return ret;
 	}
 #endif
+	res = platform_get_resource_byname(pdev, IORESOURCE_MEM, "mbox0_send");
+	if (unlikely(!res)) {
+		gpueb_pr_info(GPUEB_TAG, "fail to get resource MBOX0_SEND");
+		return -1;
+	}
+	g_gpueb_mbox_ipi = devm_ioremap(gpueb_dev, res->start, resource_size(res));
+	if (unlikely(!g_gpueb_mbox_ipi)) {
+		gpueb_pr_info(GPUEB_TAG, "fail to ioremap MBOX0_SEND: 0x%llx", res->start);
+		return -1;
+	}
+
 	return 0;
 }
 
@@ -507,3 +522,14 @@ int get_gpueb_slot_size(void)
 	return g_slot_size;
 }
 EXPORT_SYMBOL_GPL(get_gpueb_slot_size);
+
+void gpueb_clr_mbox1_irq(void)
+{
+	if (g_gpueb_mbox_ipi) {
+
+		/* GPUEB to APMCU IRQ[1] SW INT Clear */
+		writel(1, (g_gpueb_mbox_ipi + 0x84));
+	} else
+		gpueb_pr_info(GPUEB_TAG, "null g_gpueb_mbox_ipi");
+}
+EXPORT_SYMBOL_GPL(gpueb_clr_mbox1_irq);
