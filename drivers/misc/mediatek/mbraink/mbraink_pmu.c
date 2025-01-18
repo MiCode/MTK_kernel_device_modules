39123d0294b068a51eb0a8088e6dc060b7606d03
Raymond Lin <yu-jui.lin@mediatek.com>
2025-01-19 07:35:53 +0800

    [ALPS08628391] spec inst from swpm
    [Description]
    fix mips from swpm
    
    [Test]
    MBTF Test
    
    MTK-Commit-Id: 389c16623421023191ce23ae956622777959c4fe
    
    Signed-off-by: Raymond Lin <yu-jui.lin@mediatek.com>
    CR-Id: ALPS08628391
    Feature: MBrain
    Change-Id: Icbe6fba10c3082538b51a7f84590826cccfa42cb

--- drivers/misc/mediatek/mbraink/mbraink_pmu.c
+++ drivers/misc/mediatek/mbraink/mbraink_pmu.c
@@ -6,34 +6,27 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/perf_event.h>
+#include <swpm_perf_arm_pmu.h>
 #include "mbraink_pmu.h"
 
-#if (MBRAINK_LANDING_FEATURE_CHECK == 0)
-extern DEFINE_PER_CPU(struct perf_event *, inst_spec_events);
 static DEFINE_PER_CPU(unsigned long long,  inst_spec_count);
-#endif
-
 static u64 spec_instructions[8];
 
-#if (MBRAINK_LANDING_FEATURE_CHECK == 0)
 static int mbraink_pmu_get_inst_count(int cpu)
 {
-	struct perf_event *event = per_cpu(inst_spec_events, cpu);
 	unsigned long long new = 0;
 	unsigned long long old = per_cpu(inst_spec_count, cpu);
 	unsigned long long diff = 0;
 
-	if (event && event->state == PERF_EVENT_STATE_ACTIVE && cpu_online(cpu)) {
-		perf_event_read_local(event, &new, NULL, NULL);
-		if (new > old && old > 0)
-			diff = (unsigned long long)(new - old);
+	new = swpm_pmu_get_count(INST_SPEC_EVT, cpu);
+	if (new > old && old > 0)
+		diff = (unsigned long long)(new - old);
 
-		per_cpu(inst_spec_count, cpu) = new;
-		spec_instructions[cpu] += diff;
+	per_cpu(inst_spec_count, cpu) = new;
+	spec_instructions[cpu] += diff;
 
-		pr_info("perf event read on CPU %d, new: %llu, old: %llu, diff: %llu\n",
-				cpu, new, old, diff);
-	}
+	pr_info("perf event read on CPU %d, new: %llu, old: %llu, diff: %llu\n",
+			cpu, new, old, diff);
 
 	return diff;
 }
@@ -45,10 +38,7 @@ static void _mbraink_gen_pmu_count(void *val)
 	cpu = smp_processor_id();
 	mbraink_pmu_get_inst_count(cpu);
 }
-#endif
 
-#if IS_ENABLED(CONFIG_MTK_LPM_MT6989) && \
-	(MBRAINK_LANDING_FEATURE_CHECK == 0)
 int mbraink_enable_pmu_inst_spec(bool enable)
 {
 	pr_info("mbrain enable pmu inst spec, enable: %d", enable);
@@ -93,24 +83,3 @@ int mbraink_get_pmu_inst_spec(struct mbraink_pmu_info *pmuInfo)
 	return 0;
 }
 
-#else
-int mbraink_enable_pmu_inst_spec(bool enable)
-{
-	pr_info("%s: not enable do nothing\n", __func__);
-	return 0;
-}
-
-int mbraink_get_pmu_inst_spec(struct mbraink_pmu_info *pmuInfo)
-{
-	pmuInfo->cpu0_pmu_data_inst_spec = spec_instructions[0];
-	pmuInfo->cpu1_pmu_data_inst_spec = spec_instructions[1];
-	pmuInfo->cpu2_pmu_data_inst_spec = spec_instructions[2];
-	pmuInfo->cpu3_pmu_data_inst_spec = spec_instructions[3];
-	pmuInfo->cpu4_pmu_data_inst_spec = spec_instructions[4];
-	pmuInfo->cpu5_pmu_data_inst_spec = spec_instructions[5];
-	pmuInfo->cpu6_pmu_data_inst_spec = spec_instructions[6];
-	pmuInfo->cpu7_pmu_data_inst_spec = spec_instructions[7];
-	pr_info("%s: not enable do nothing\n", __func__);
-	return 0;
-}
-#endif
