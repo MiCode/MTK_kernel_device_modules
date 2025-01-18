a6351a01d40b2fd9e84e4d505e724436920e956c
henry.tu <henry.tu@mediatek.com>
2025-01-19 07:16:51 +0800

    [ALPS08704738] panel: add serdes panel driver support
    add serdes panel drvier support
    
    MTK-Commit-Id: 49f8c6de027474c60f7f10c1c1854d97f9a78428
    
    Signed-off-by: henry.tu <henry.tu@mediatek.com>
    CR-Id: ALPS08704738
    Change-Id: I238e773b3310be774d5d9c1b8f495a92acd6eac7
    Feature: display_mobile

---
+++ drivers/gpu/drm/panel/bridge-serdes-max96789.h
@@ -0,0 +1,35 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+#include <drm/drm_panel.h>
+#include <drm/drm_modes.h>
+#include <drm/drm_bridge.h>
+
+#include <linux/module.h>
+#include <linux/of_platform.h>
+#include <linux/of_graph.h>
+#include <linux/platform_device.h>
+
+struct vdo_timing {
+	u32 width;
+	u32 height;
+	u32 hfp;
+	u32 hsa;
+	u32 hbp;
+	u32 vfp;
+	u32 vsa;
+	u32 vbp;
+	u32 fps;
+	u32 pll;
+	u32 lppf;
+	u32 physcial_w;
+	u32 physcial_h;
+};
+
+void serdes_enable(struct drm_bridge *bridge);
+void serdes_pre_enable(struct drm_bridge *bridge);
+void serdes_disable(struct drm_bridge *bridge);
+void serdes_get_modes(struct drm_bridge *bridge, struct vdo_timing *disp_mode);
+
