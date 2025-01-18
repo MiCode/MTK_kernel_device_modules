36891407b6007f0e44477c712331533d0602c9fc
huifen wang <huifen.wang@mediatek.com>
2025-01-19 07:21:45 +0800

    [ALPS08679316]  fix snprintf build errors
    [Description]
    Fix build error according to 76deb683b6.
    kernel_device_modules-mainline/drivers/tee/teei/510/tz_driver/sysfs.c:182:8: error: snprintf will always be truncated; specified size is 32, but format string expands to at least 33 [-Werror,-Wfortify-source]
      182 |         ret = snprintf(buf, UUID_STRING_LENGTH,
          |               ^
    1 error generated.
    [Test]
    build pass
    
    MTK-Commit-Id: 2cc277c0c3b49a628f9d3b194a726433e306afdd
    
    Signed-off-by: huifen wang <huifen.wang@mediatek.com>
    CR-Id: ALPS08679316
    Feature: teei
    Change-Id: Ib331d3c1934e36005c561998ce598713fc59fd20

--- drivers/tee/teei/510/tz_driver/sysfs.c
+++ drivers/tee/teei/510/tz_driver/sysfs.c
@@ -179,7 +179,7 @@ static inline void uuid_to_str(struct TEEC_UUID *uuid, char *buf)
 {
 	int ret = 0;
 
-	ret = snprintf(buf, UUID_STRING_LENGTH,
+	ret = snprintf(buf, UUID_STRING_LENGTH+1,
 			"%08x%04x%04x%02x%02x%02x%02x%02x%02x%02x%02x",
 			uuid->timeLow, uuid->timeMid,
 			uuid->timeHiAndVersion,
@@ -486,7 +486,7 @@ static ssize_t list_ut_drv_show(struct device *cd,
 
 	list_for_each_entry(entry, &ut_drv_list, list) {
 		uuid_to_str(&entry->uuid, uuid_str);
-		s += snprintf(s, UUID_STRING_LENGTH, "%s\n", uuid_str);
+		s += snprintf(s, UUID_STRING_LENGTH+1, "%s\n", uuid_str);
 	}
 
 	return (ssize_t)(s - buf);
