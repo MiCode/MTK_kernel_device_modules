019571e8ce52918629c89fbc5ab74b0eec3cd7e6
Dong-Hung Lin <dong-hung.lin@mediatek.com>
2025-01-19 08:49:22 +0800

    [ALPS09158276] disp: remove mode switch re-start trigger loop
    [Description]
    remove mode switch re-start trigger loop
    [Test]
    mestress
    
    MTK-Commit-Id: 56a42e17d8070de4460a8d2b429d9ca5be62a32b
    
    Signed-off-by: Dong-Hung Lin <dong-hung.lin@mediatek.com>
    CR-Id: ALPS09158276
    Feature: display_mobile
    Change-Id: Ia0f9e191b0571059eb4b71fe1b7d3bfffb20b8ca

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_crtc.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_crtc.c
@@ -6479,14 +6479,13 @@ void mtk_crtc_mode_switch_on_ap_config(struct mtk_drm_crtc *mtk_crtc,
 	CRTC_MMP_MARK((int) drm_crtc_index(crtc), mode_switch, 0, 3);
 
 	/* adjust trigger loop in different display mode */
-	if (mtk_crtc_with_trigger_loop(crtc) &&
-			mtk_crtc_with_event_loop(crtc) &&
+	if (mtk_crtc_with_event_loop(crtc) &&
 			mtk_crtc_is_frame_trigger_mode(crtc)) {
-		mtk_crtc_stop_trig_loop(crtc);
 		mtk_crtc_stop_event_loop(crtc);
 
+		mtk_crtc_skip_merge_trigger(mtk_crtc);
+
 		mtk_crtc_start_event_loop(crtc);
-		mtk_crtc_start_trig_loop(crtc);
 	}
 	if (drm_crtc_index(crtc) == 0)
 		mtk_vidle_update_dt_by_type(crtc,
@@ -6674,14 +6673,13 @@ void mtk_crtc_mode_switch_config(struct mtk_drm_crtc *mtk_crtc,
 	CRTC_MMP_MARK((int) drm_crtc_index(crtc), mode_switch, 0, 3);
 
 	/* adjust trigger loop in different display mode */
-	if (mtk_crtc_with_trigger_loop(crtc) &&
-			mtk_crtc_with_event_loop(crtc) &&
+	if (mtk_crtc_with_event_loop(crtc) &&
 			mtk_crtc_is_frame_trigger_mode(crtc)) {
-		mtk_crtc_stop_trig_loop(crtc);
 		mtk_crtc_stop_event_loop(crtc);
 
+		mtk_crtc_skip_merge_trigger(mtk_crtc);
+
 		mtk_crtc_start_event_loop(crtc);
-		mtk_crtc_start_trig_loop(crtc);
 	}
 	if (drm_crtc_index(crtc) == 0)
 		mtk_vidle_update_dt_by_type(crtc,
@@ -6895,14 +6893,13 @@ static void mtk_crtc_msync2_switch_begin(struct drm_crtc *crtc)
 	mtk_ddp_comp_io_cmd(output_comp, NULL, DSI_PLL_SWITCH_REFERENCE_CNT_CTL,
 		&mtk_crtc->msync2.msync_frame_status);
 
-	if (mtk_crtc_with_trigger_loop(crtc) && mtk_crtc_with_event_loop(crtc)) {
-		mtk_crtc_stop_trig_loop(crtc);
+	if (mtk_crtc_with_event_loop(crtc) &&
+			mtk_crtc_is_frame_trigger_mode(crtc)) {
 		mtk_crtc_stop_event_loop(crtc);
 
 		mtk_crtc_skip_merge_trigger(mtk_crtc);
 
 		mtk_crtc_start_event_loop(crtc);
-		mtk_crtc_start_trig_loop(crtc);
 	}
 
 	/* set frame done */
