720e0e13b5c436c71e98aeb52aad59b554ff622d
William Chu <william.chu@mediatek.com>
2025-01-19 09:49:38 +0800

    [ALPS09474990] add mbraink driver
    [Description]
    - add mbrain driver for 6993.
    
    [Test]
    - UT/IT
    - MTBF-Monkey
    
    MTK-Commit-Id: 5b2899ac3da5cd647f463c060ef86f8a11bc03c3
    
    Signed-off-by: William Chu <william.chu@mediatek.com>
    CR-Id: ALPS09474990
    Feature: MBrain
    Change-Id: Ic4dc267fadfd2019ba19d94d5ed8f2bf130b04e0

---
+++ drivers/misc/mediatek/mbraink/modules/v6993/mbraink_v6993.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2022 MediaTek Inc.
+ */
+
+#ifndef MBRAINK_V6993_H
+#define MBRAINK_V6993_H
+
+#include "mbraink_v6993_memory.h"
+#include "mbraink_v6993_audio.h"
+#include "mbraink_v6993_battery.h"
+#include "mbraink_v6993_power.h"
+#include "mbraink_v6993_gpu.h"
+#include "mbraink_v6993_gps.h"
+#include "mbraink_v6993_wifi.h"
+#include "mbraink_v6993_camera.h"
+
+#endif /*end of MBRAINK_V6993_H*/
