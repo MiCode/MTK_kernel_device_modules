8c5d54e2bf19c579b4e24c11e8b976448bd2b089
chien-wei hsu <chien-wei.hsu@mediatek.com>
2025-01-19 09:21:43 +0800

    [ALPS08864080] adsp: Update clock porting
    [Description]
    Renamed CLK_TOP_ADSP_SEL to CLK_PD_ADSP_TOP in the ADSP clock enum and
    updated all references to ensure correct clock management.
    
    [Test]
    Build FPGA Pass
    
    MTK-Commit-Id: fed0861370479d9e5e52e72486041005976198b6
    
    Signed-off-by: chien-wei hsu <chien-wei.hsu@mediatek.com>
    CR-Id: ALPS08864080
    Feature: ADSP AP Driver
    Change-Id: Ic96ce930e853a6677a35f21df0d042e20fdc4ff4

--- drivers/misc/mediatek/adsp/v3/adsp_clk_v3.c
+++ drivers/misc/mediatek/adsp/v3/adsp_clk_v3.c
@@ -8,7 +8,7 @@
 #include "adsp_core.h"
 
 enum adsp_clk {
-	CLK_TOP_ADSP_SEL,
+	CLK_PD_ADSP_TOP,
 	ADSP_CLK_NUM
 };
 
@@ -18,32 +18,32 @@ struct adsp_clock_attr {
 };
 
 static struct adsp_clock_attr adsp_clks[ADSP_CLK_NUM] = {
-	[CLK_TOP_ADSP_SEL] = {"clk_top_adsp_sel", NULL},
+	[CLK_PD_ADSP_TOP] = {"clk_pd_adsp_top", NULL},
 };
-
 
 int adsp_mt_enable_clock(void)
 {
 	int ret = 0;
 
-	ret = clk_prepare_enable(adsp_clks[CLK_TOP_ADSP_SEL].clock);
+	ret = clk_prepare_enable(adsp_clks[CLK_PD_ADSP_TOP].clock);
 	if (IS_ERR(&ret)) {
 		pr_err("%s(), clk_prepare_enable %s fail, ret %d\n",
-			__func__, adsp_clks[CLK_TOP_ADSP_SEL].name, ret);
+			__func__, adsp_clks[CLK_PD_ADSP_TOP].name, ret);
 		return -EINVAL;
 	}
+	pr_debug("%s() done()\n", __func__);
+
 	return ret;
 }
 
 void adsp_mt_disable_clock(void)
 {
-	pr_debug("%s()\n", __func__);
-	clk_disable_unprepare(adsp_clks[CLK_TOP_ADSP_SEL].clock);
+	clk_disable_unprepare(adsp_clks[CLK_PD_ADSP_TOP].clock);
+	pr_debug("%s(), done\n", __func__);
 }
 
 /* clock init */
-int adsp_clk_probe(struct platform_device *pdev,
-			  struct adsp_clk_operations *ops)
+int adsp_clk_probe(struct platform_device *pdev, struct adsp_clk_operations *ops)
 {
 	int ret = 0;
 
@@ -55,14 +55,15 @@ int adsp_clk_probe(struct platform_device *pdev,
 		if (IS_ERR(adsp_clks[i].clock)) {
 			ret = PTR_ERR(adsp_clks[i].clock);
 			pr_err("%s devm_clk_get %s fail %d\n", __func__,
-				   adsp_clks[i].name, ret);
+			       adsp_clks[i].name, ret);
+			return ret;
 		}
 	}
-#ifndef BRINGUP_ADSP
+
 	ops->enable = adsp_mt_enable_clock;
 	ops->disable = adsp_mt_disable_clock;
 	/* ops->select not supported, PLL mux in adsp */
-#endif
+
 	return 0;
 }
 
