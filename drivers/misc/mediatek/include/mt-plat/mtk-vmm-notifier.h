5845ec208538f2cafaf034fef2f911fddc6dc179
mtk32733 <eric.chien@mediatek.com>
2025-01-19 09:36:13 +0800

    [ALPS09589946] vmm: add mt6993 ap support
    [Description]
    Patchest 1, copy from mt6991.
    
    1. HWCCF voter api
    2. Backupvoter 0 (sw view; HW view: 1)
    3. ap voter bit 11
    4. cvfs_en/cvfs_dump api
    From 9874625.
    
    [Test]
    Build pass.
    
    MTK-Commit-Id: 935cd8801b9b4c5910c5d35d57d320446c9188ab
    
    CR-Id: ALPS09589946
    Feature: VMM
    Signed-off-by: mtk32733 <eric.chien@mediatek.com>
    Change-Id: Ic08d986a2e216cf893c37bf6516382bc43a4bebd

--- drivers/misc/mediatek/include/mt-plat/mtk-vmm-notifier.h
+++ drivers/misc/mediatek/include/mt-plat/mtk-vmm-notifier.h
@@ -12,6 +12,19 @@
  * @param openIsp 1: isp on 0: isp off
  * @return int
  */
+
+enum VMM_CVFS_SEL_ID {
+    VMM_CVFS_SEL_ID_START,
+    VMM_CVFS_CAM_SEL = VMM_CVFS_SEL_ID_START,
+    VMM_CVFS_IMG_SEL,
+    VMM_CVFS_IPE_SEL,
+    VMM_CVFS_VDE_SEL,
+    VMM_CVFS_SEL_ID_END,
+};
+
 int vmm_isp_ctrl_notify(int openIsp);
+int vmm_enable_cvfs(int user_id, enum VMM_CVFS_SEL_ID vmm_cvfs_sel_id);
+int vmm_disable_cvfs(int user_id, enum VMM_CVFS_SEL_ID vmm_cvfs_sel_id);
+int vmm_cvfs_dump(void);
 
 #endif
