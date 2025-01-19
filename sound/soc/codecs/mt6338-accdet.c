517ae790f29772cc58fa9d3a3e887c996dd9a42f
Roy-fp Wu <roy-fp.wu@mediatek.com>
2025-01-19 08:28:35 +0800

    [ALPS09021315] ACCDET: Fix K6.6 build warning
    [Description]
    Fix unused but set variable build warning in k6.6
    
    [Test]
    Build pass
    
    MTK-Commit-Id: c9dbc23d3b3d835360e9c901667411596d5d5c49
    
    Signed-off-by: Roy-fp Wu <roy-fp.wu@mediatek.com>
    CR-Id: ALPS09021315
    Feature: accdet
    Change-Id: I73d5736edf0f9bb3c75689254852d367c5ac7a30

--- sound/soc/codecs/mt6338-accdet.c
+++ sound/soc/codecs/mt6338-accdet.c
@@ -803,6 +803,9 @@ static void accdet_get_efuse(void)
 			accdet->moisture_vm);
 	}
 	pr_info("%s efuse=0x%x,auxadc_val=%dmv\n", __func__, efuseval, accdet->auxadc_offset);
+
+	// Mark variables as used to prevent warnings
+	(void)ret;
 }
 
 static void accdet_get_efuse_4key(void)
@@ -834,6 +837,9 @@ static void accdet_get_efuse_4key(void)
 	pr_info("accdet key thresh: mid=%dmv,voice=%dmv,up=%dmv,down=%dmv\n",
 		accdet_dts.four_key.mid, accdet_dts.four_key.voice,
 		accdet_dts.four_key.up, accdet_dts.four_key.down);
+
+	// Mark variables as used to prevent warnings
+	(void)ret;
 }
 
 static u32 key_check(u32 v)
@@ -2041,6 +2047,9 @@ static u32 config_moisture_detect_2_1(void)
 	pmic_write_mset(MT6338_RG_EINT0CTURBO_ADDR, MT6338_RG_EINT0CTURBO_SHIFT,
 			0x1F, cturbo);
 
+	// Mark variables as used to prevent warnings
+	(void)ret;
+
 	return 0;
 }
 
@@ -2064,6 +2073,9 @@ static u32 config_moisture_detect_2_1_1(void)
 		pmic_write_mset(MT6338_RG_ACCDETSPARE_ADDR, 0x3, 0x1F, 0x5);
 	else
 		pmic_write_mset(MT6338_RG_ACCDETSPARE_ADDR, 0x3, 0x1F, vref_1v);
+
+	// Mark variables as used to prevent warnings
+	(void)ret;
 
 	return 0;
 }
@@ -2144,6 +2156,10 @@ static irqreturn_t ex_eint_handler(int irq, void *data)
 
 	disable_irq_nosync(accdet->gpioirq);
 	ret = queue_work(accdet->eint_workqueue, &accdet->eint_work);
+
+	// Mark variables as used to prevent warnings
+	(void)ret;
+
 	return IRQ_HANDLED;
 }
 
@@ -2856,6 +2872,9 @@ static void accdet_ipi_rx_internal(unsigned int *msg_data)
 	int ret;
 
 	ret = queue_work(accdet->ipi_workqueue, &accdet->ipi_work);
+
+	// Mark variables as used to prevent warnings
+	(void)ret;
 }
 
 static int accdet_ipi_rx_handler(unsigned int id,
@@ -3035,6 +3054,9 @@ static int accdet_probe(struct platform_device *pdev)
 	atomic_set(&accdet_first, 1);
 	mod_timer(&accdet_init_timer, (jiffies + ACCDET_INIT_WAIT_TIMER));
 	dev_info(&pdev->dev, "%s done\n", __func__);
+
+	// Mark variables as used to prevent warnings
+	(void)res;
 
 	return 0;
 
--- sound/soc/codecs/mt6357-accdet.c
+++ sound/soc/codecs/mt6357-accdet.c
@@ -685,6 +685,9 @@ static void accdet_get_efuse(void)
 	}
 	pr_info("%s efuse=0x%x,auxadc_val=%dmv\n", __func__, efuseval,
 		accdet->auxadc_offset);
+
+	// Mark variables as used to prevent warnings
+	(void)ret;
 }
 
 static void accdet_get_efuse_4key(void)
@@ -716,6 +719,9 @@ static void accdet_get_efuse_4key(void)
 	pr_info("accdet key thresh: mid=%dmv,voice=%dmv,up=%dmv,down=%dmv\n",
 		accdet_dts.four_key.mid, accdet_dts.four_key.voice,
 		accdet_dts.four_key.up, accdet_dts.four_key.down);
+
+	// Mark variables as used to prevent warnings
+	(void)ret;
 }
 
 static u32 key_check(u32 v)
@@ -1544,6 +1550,10 @@ void accdet_irq_handle(void)
 	} else {
 		pr_notice("%s no interrupt detected!\n", __func__);
 	}
+
+	// Mark variables as used to prevent warnings
+	(void)acc_sts;
+	(void)eint_sts;
 }
 
 static irqreturn_t mtk_accdet_irq_handler_thread(int irq, void *data)
@@ -1589,6 +1599,10 @@ static irqreturn_t ex_eint_handler(int irq, void *data)
 
 	disable_irq_nosync(accdet->gpioirq);
 	ret = queue_work(accdet->eint_workqueue, &accdet->eint_work);
+
+	// Mark variables as used to prevent warnings
+	(void)ret;
+
 	return IRQ_HANDLED;
 }
 
@@ -2245,6 +2259,9 @@ static int accdet_probe(struct platform_device *pdev)
 	}
 	atomic_set(&accdet_first, 1);
 	mod_timer(&accdet_init_timer, (jiffies + ACCDET_INIT_WAIT_TIMER));
+
+	// Mark variables as used to prevent warnings
+	(void)res;
 
 	return 0;
 
