f80687b366c4f5fcf2b574aa854234bd4ef34419
William Yang <william-tw.yang@mediatek.com>
2025-01-19 09:39:48 +0800

    [ALPS09664542] DISP: unmask dpc func for vidle
    [Description]
    unmask dpc func to fix swrgo underrun issue
    
    [Test]
    vp, cam
    
    MTK-Commit-Id: 194cb57243383b37e9d4dc9d57f2a238cbabbdbd
    
    Signed-off-by: William Yang <william-tw.yang@mediatek.com>
    CR-Id: ALPS09664542
    Feature: display_mobile
    Change-Id: I82f04cde671b1ca7be3c0acfdd1e56c5ff98baf6

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_vidle.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_vidle.c
@@ -865,7 +865,7 @@ void mtk_vidle_register(const struct dpc_funcs *funcs, enum mtk_dpc_version vers
 		__func__, vidle_data.panel_type, version,
 		vidle_data.level, vidle_data.hrt_bw, vidle_data.srt_bw);
 	vidle_data.dpc_version = version;
-//	disp_dpc_driver = *funcs;
+	disp_dpc_driver = *funcs;
 
 	if (version == DPC_VER1)
 		mtk_vidle_register_v1(funcs);
