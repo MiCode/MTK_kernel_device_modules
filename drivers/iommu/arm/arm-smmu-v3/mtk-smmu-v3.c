f855aca91ab618d65bfab1d0a4451edbcb1641bd
Yunfei Wang <yf.wang@mediatek.com>
2025-01-19 09:29:36 +0800

    [ALPS09162427] smmu/mediatek: mt6993: Enable mm smmu sec falg
    [Description]
    Enabled mm smmu sec flag in the mt6993 platform data to ensure
    secure features initialization.
    
    [Test]
    Build pass and pass self-ktf test
    
    MTK-Commit-Id: de9b601d14a7d624bbd83128fe11da9a5afa1569
    
    Signed-off-by: Yunfei Wang <yf.wang@mediatek.com>
    CR-Id: ALPS09162427
    Feature: iommu_kernel
    Change-Id: I6e8bd76f68c2d15d93899017db6d96633f8b29f7

--- drivers/iommu/arm/arm-smmu-v3/mtk-smmu-v3.c
+++ drivers/iommu/arm/arm-smmu-v3/mtk-smmu-v3.c
@@ -3005,8 +3005,8 @@ static const struct mtk_smmu_plat_data mt6991_data_gpu = {
 static const struct mtk_smmu_plat_data mt6993_data_mm = {
 	.smmu_plat		= SMMU_MT6993,
 	.smmu_type		= MM_SMMU,
-	.flags			= SMMU_DELAY_HW_INIT | /* SMMU_SEC_EN | SMMU_HYP_EN | */
-				  SMMU_EXTRA_DCM_EN | /* SMMU_HANG_DETECT | */SMMU_CLK_AO_EN,
+	.flags			= SMMU_DELAY_HW_INIT | SMMU_SEC_EN |/* SMMU_HYP_EN | */
+				  SMMU_EXTRA_DCM_EN | SMMU_HANG_DETECT | SMMU_CLK_AO_EN,
 };
 
 static const struct mtk_smmu_plat_data mt6993_data_apu = {
