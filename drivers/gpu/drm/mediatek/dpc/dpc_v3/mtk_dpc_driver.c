f42e978ba82c9027b505b7c4a661f352ba6fed71
William Yang <william-tw.yang@mediatek.com>
2025-01-19 09:41:54 +0800

    [ALPS09676387] DISP: disable more funcs for bringup stage
    [Description]
    disable more funcs for bringup stage
    
    [Test]
    boot, vp
    
    MTK-Commit-Id: 693b4bd6de7fe3426451ddda6ce9978cc9d7e226
    
    Signed-off-by: William Yang <william-tw.yang@mediatek.com>
    CR-Id: ALPS09676387
    Feature: display_mobile
    Change-Id: I820f0a681b86555952874f5b72720dbd725e2bff

--- drivers/gpu/drm/mediatek/dpc/dpc_v3/mtk_dpc_driver.c
+++ drivers/gpu/drm/mediatek/dpc/dpc_v3/mtk_dpc_driver.c
@@ -2275,6 +2275,20 @@ static int mtk_dpc_probe_v3(struct platform_device *pdev)
 	funcs_v3.dpc_duration_update = priv->duration_update;
 	funcs_v3.dpc_enable = priv->enable;
 
+	if (priv->mmsys_id == MMSYS_MT6993) {
+		funcs_v3.dpc_config = NULL;
+		funcs_v3.dpc_group_enable = NULL;
+		funcs_v3.dpc_mtcmos_auto = NULL;
+		funcs_v3.dpc_mtcmos_vote = NULL;
+		funcs_v3.dpc_dsi_pll_set = NULL;
+		funcs_v3.dpc_vidle_power_keep = NULL;
+		funcs_v3.dpc_vidle_power_release = NULL;
+		funcs_v3.dpc_vidle_power_keep_by_gce = NULL;
+		funcs_v3.dpc_vidle_power_release_by_gce = NULL;
+		funcs_v3.dpc_check_pll = NULL;
+		funcs_v3.dpc_analysis = NULL;
+	}
+
 	mtk_vidle_register(&funcs_v3, DPC_VER2);
 	mml_dpc_register(&funcs_v3, DPC_VER2);
 	mdp_dpc_register(&funcs_v3, DPC_VER2);
