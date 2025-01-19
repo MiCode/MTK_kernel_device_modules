9dd76988bce1a91785f2df8714117d9e780bf44f
Chinwen Chang <chinwen.chang@mediatek.com>
2025-01-19 09:01:11 +0800

    [ALPS09453451] mkp: restrict the protection range
    [Description]
    No runtime protection to mitigate its impact on syscall.
    
    [Test]
    Boot-up pass.
    
    MTK-Commit-Id: 6ef6c4bbc9e06866b57bcf7eeeedbdd008360a55
    
    Signed-off-by: Chinwen Chang <chinwen.chang@mediatek.com>
    CR-Id: ALPS09453451
    Feature: mkp
    Change-Id: I91c19c6f32feba2fefc297f5a6ce2f611e7f0a29

--- drivers/misc/mediatek/mkp/mkp_demo.c
+++ drivers/misc/mediatek/mkp/mkp_demo.c
@@ -1064,7 +1064,7 @@ int __init mkp_demo_init(void)
 	int ret = 0, ret_erri_line;
 	unsigned long size = 0x100000;
 	struct device_node *node;
-	u32 mkp_policy_default = 0x0001fffb; // disable selinux_state policy as default
+	u32 mkp_policy_default = 0x0001ffe3;
 	u32 mkp_policy = 0x0001ffff;
 	const char *mkp_panic;
 
