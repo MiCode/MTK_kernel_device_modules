bd3525e86967987b9b6e4f91e77def1bcd348a3b
Kuanlin Wang <kuan-lin.wang@mediatek.com>
2025-01-19 09:31:47 +0800

    [ALPS09635345] pmic: Add Mbrain glitch cnt dump for MT6991
    [Description]
    Add Mbrain glitch cnt dump for MT6991
    
    [Test]
    Test pass
    
    MTK-Commit-Id: 871f982c4558d24f665d7972f0b7ade4f93a7050
    
    Signed-off-by: Kuanlin Wang <kuan-lin.wang@mediatek.com>
    CR-Id: ALPS09635345
    Feature: pmic_driver
    Change-Id: I97af157cc1c9bed47b164857c6ea8e7794ffc46a

---
+++ drivers/mfd/mtk-spmi-pmic-debug.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0
+ * Copyright (c) 2024 MediaTek Inc.
+ */
+
+#ifndef __MTK_SPMI_PMIC_DEBUG_H__
+#define __MTK_SPMI_PMIC_DEBUG_H__
+
+#define spmi_glitch_idx_cnt 65
+extern void mtk_spmi_pmic_get_glitch_cnt(u16 *buf);
+
+MODULE_AUTHOR("HS Chien <HS.Chien@mediatek.com>");
+MODULE_DESCRIPTION("Debug driver for MediaTek SPMI PMIC");
+MODULE_LICENSE("GPL");
+
+#endif /*__MTK_SPMI_PMIC_DEBUG_H__*/
