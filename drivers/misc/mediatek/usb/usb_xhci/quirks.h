211d8fcccfe5a0fcd0590b5bcbe6f2fed6e13d45
Denis Hsu <denis.hsu@mediatek.com>
2025-01-19 08:54:22 +0800

    [ALPS09070892] xhci: mbrain implement
    [Description]
    xhci mbrain implement
    
    [Test]
    build pass
    
    MTK-Commit-Id: c493bef224b80b44cc25776b45f0512a03588b41
    
    CR-Id: ALPS09070892
    Feature: usb_ssusb
    Change-Id: Ic86dcc9ca94e8587847b5507e9b1c0126af60495
    Signed-off-by: Denis Hsu <denis.hsu@mediatek.com>

--- drivers/misc/mediatek/usb/usb_xhci/quirks.h
+++ drivers/misc/mediatek/usb/usb_xhci/quirks.h
@@ -11,10 +11,32 @@
 
 #include <linux/usb.h>
 #include "usbaudio.h"
+#include <linux/hashtable.h>
+#include <linux/jiffies.h>
 
 void xhci_mtk_init_snd_quirk(struct snd_usb_audio *chip);
 void xhci_mtk_apply_quirk(struct usb_device *udev);
 void xhci_mtk_trace_init(struct device *dev);
 void xhci_mtk_trace_deinit(struct device *dev);
 
+struct xhci_mbrain {
+	u16 vid;
+	u16 pid;
+	u16 bcd;
+	enum usb_device_state	state;
+	enum usb_device_speed	speed;
+	u16 err_reason;
+};
+
+struct xhci_mbrain_hash_node {
+	char dev_name[32];
+	struct xhci_mbrain mbrain_data;
+	unsigned long jiffies;
+	bool updated_db;
+	struct hlist_node node;
+};
+
+typedef void (*xhci_enum_mbrain_callback)(struct xhci_mbrain data);
+int register_xhci_enum_mbrain_cb(xhci_enum_mbrain_callback cb);
+int unregister_xhci_enum_mbrain_cb(void);
 #endif
