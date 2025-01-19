b7052ea4b29c9c56a8159927f89c8f646187b88f
William Yang <william-tw.yang@mediatek.com>
2025-01-19 08:49:51 +0800

    [ALPS09181384] DISP: update dt timer when doze active changed
    [Description]
    No need to update dt timer
    doze active = 1, crtc active 0 => 1
    doze active = 1, crtc active 1 => 0
    doze active = 0, crtc active 1 => 0
    doze active = 0, crtc active 1 => 1 (non-doze normal case)
    
    Need to update dt timer
    doze active = 0, crtc active 0 => 1
    
    Need to update dt timer for doze transition
    Frame[N-1] : doze active 1 => 0               (doze_changed = 1)
    Frame[N]   : crtc active = 1, doze active = 0 (doze_changed = 0)
    
    [Test]
    mestress
    
    MTK-Commit-Id: d32016ce93fa21e6ff45d119af6f173b82411ae8
    
    Signed-off-by: William Yang <william-tw.yang@mediatek.com>
    CR-Id: ALPS09181384
    Feature: display_mobile
    Change-Id: Ib648d38a630853007d04398f908083c838e3b502

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_crtc.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_crtc.c
@@ -16160,10 +16160,16 @@ static void mtk_drm_crtc_atomic_begin(struct drm_crtc *crtc,
 
 	if (crtc_id == 0 && mtk_drm_helper_get_opt(priv->helper_opt, MTK_DRM_OPT_VIDLE_FULL_SCENARIO)) {
 		/* update DT timer to avoid mismatch between real TE and DT timer */
-		if (crtc->state->active_changed && crtc->state->active &&
-		    !mtk_crtc_state->prop_val[CRTC_PROP_DOZE_ACTIVE])
-			mtk_vidle_update_dt_by_type(crtc,
-					mtk_dsi_is_cmd_mode(output_comp) ? PANEL_TYPE_CMD : PANEL_TYPE_VDO);
+		if (crtc->state->active && !mtk_crtc_state->prop_val[CRTC_PROP_DOZE_ACTIVE]) {
+			if (crtc->state->active_changed ||
+			    (old_mtk_state->doze_changed != mtk_crtc_state->doze_changed)) {
+				DDPINFO("crtc active_changed(%u) doze_changed(%u->%u)\n",
+					(bool)crtc->state->active_changed,
+					old_mtk_state->doze_changed, mtk_crtc_state->doze_changed);
+				mtk_vidle_update_dt_by_type(crtc, mtk_dsi_is_cmd_mode(output_comp) ?
+							    PANEL_TYPE_CMD : PANEL_TYPE_VDO);
+			}
+		}
 
 		/* keep power on to avoid extra TE during mode switch process */
 		if (mode_changed)
