e490f1e01850a10d7c5da5b0f4126081fab1e948
mtk31023 <meng-hsien.lin@mediatek.com>
2025-01-19 09:03:39 +0800

    [ALPS09415707] slc: mt6991: add error flag support
    [Description]
    add error flag support
    
    [Test]
    reboot
    
    MTK-Commit-Id: f0cc68d7117ded501364dd987f5e4e067a79ec29
    
    Signed-off-by: mtk31023 <meng-hsien.lin@mediatek.com>
    CR-Id: ALPS09415707
    Feature: slc api driver
    Change-Id: I5c672d451cdeee0af077b49eac8e875b132a2301

--- drivers/edac/mediatek/mtk_edac_slc.c
+++ drivers/edac/mediatek/mtk_edac_slc.c
@@ -37,7 +37,24 @@ struct slc_drvdata {
 	unsigned int cs_num;
 	unsigned int chn_num;
 	unsigned int assert;
+	unsigned int error_flags_enable;
+	struct error_flags_data *error_flags;
 };
+
+struct error_flags_data{
+	unsigned int *int_sts;
+	int int_sts_len;
+	unsigned int *int_sts_msk;
+	int int_sts_msk_len;
+	unsigned int *int_sts_clr;
+	int int_sts_clr_len;
+	unsigned int dbg_info_mux_sel;
+	unsigned int *dbg_info_mux_vals;
+	int dbg_info_mux_vals_len;
+	unsigned int *dbg_data;
+	int dbg_data_len;
+};
+
 static enum error_type read_parity_status(struct edac_device_ctl_info *dci, unsigned int emi_id)
 {
 	struct arm_smccc_res smc_res;
@@ -112,7 +129,7 @@ static irqreturn_t slc_err_handler(int irq, void *dev_id)
 	unsigned long long parity_err_tol;
 	unsigned int parity_err_value;
 	unsigned int parity_err_ext_value;
-	unsigned int emi_id;
+	unsigned int emi_id, i, j, reg_val;
 
 	for (emi_id = 0; emi_id < drvdata->slc_parity_cnt; emi_id++) {
 		parity_err_value = readl(drvdata->base[emi_id] + drvdata->parity_err_offset);
@@ -142,6 +159,37 @@ static irqreturn_t slc_err_handler(int irq, void *dev_id)
 		aee_kernel_exception("SLC_PARITY", ecc_mesg);
 	}
 
+	total_error_type = NO_ERROR;
+	if (drvdata->error_flags_enable == 1) {
+		//dump error flag
+		for (emi_id = 0; emi_id < drvdata->slc_parity_cnt; emi_id++) {
+			pr_info("emi: %d\n", emi_id);
+			for (i=0; i<drvdata->error_flags->int_sts_len; ++i) {
+				reg_val = readl(drvdata->base[emi_id] + drvdata->error_flags->int_sts[i]);
+				if ((reg_val & drvdata->error_flags->int_sts_msk[i]) != 0 )
+					total_error_type = UNCORRECTABLE_ERROR;
+				pr_info("%x: %x\n", drvdata->error_flags->int_sts[i], reg_val);
+			}
+			for (i=0; i<drvdata->error_flags->dbg_info_mux_vals_len; ++i) {
+				pr_info("mux: %d\n", drvdata->error_flags->dbg_info_mux_vals[i]);
+				writel(drvdata->error_flags->dbg_info_mux_vals[i], drvdata->base[emi_id] + drvdata->error_flags->dbg_info_mux_sel);
+				for (j=0; j<drvdata->error_flags->dbg_data_len; ++j) {
+					pr_info("\t%x: %x\n", drvdata->error_flags->dbg_data[j], readl(drvdata->base[emi_id] + drvdata->error_flags->dbg_data[j]));
+				}
+			}
+		}
+#if 0
+		//clear error flag
+		for (emi_id = 0; emi_id < drvdata->slc_parity_cnt; emi_id++) {
+			for (i=0; i<drvdata->error_flags->int_sts_clr_len; ++i) {
+				writel(0xffffffff, drvdata->base[emi_id] + drvdata->error_flags->int_sts_clr[i]);
+			}
+		}
+#endif
+		if (total_error_type == UNCORRECTABLE_ERROR)
+			BUG_ON(1);
+	}
+
 	if (drvdata->assert)
                 BUG_ON(1);
 
@@ -163,6 +211,7 @@ static int slc_err_probe(struct platform_device *pdev)
 	int res = 0;
 	int size, ret, i;
 	unsigned int *dump_list;
+	struct device_node *error_flags_node = NULL;
 
 	dci = edac_device_alloc_ctl_info(sizeof(*drvdata), "slc", 1, "slc", 1, 0, 0);
 	if (!dci) {
@@ -189,7 +238,7 @@ static int slc_err_probe(struct platform_device *pdev)
 	}
 	drvdata->slc_parity_cnt = (unsigned int)size;
 
-	drvdata->base = devm_kmalloc_array(&pdev->dev, drvdata->slc_parity_cnt, sizeof(unsigned int), GFP_KERNEL);
+	drvdata->base = devm_kzalloc(&pdev->dev, drvdata->slc_parity_cnt * sizeof(unsigned int), GFP_KERNEL);
 	if (!drvdata->base) {
 		dev_info(&pdev->dev, "Unable to alloc mem\n");
 		res = -ENOMEM;
@@ -206,14 +255,14 @@ static int slc_err_probe(struct platform_device *pdev)
 		}
 	}
 
-	size = of_property_count_elems_of_size(pdev->dev.of_node, "dump", sizeof(unsigned int));
+	size = of_property_count_u32_elems(pdev->dev.of_node, "dump");
 	if (size <= 0) {
 		dev_info(&pdev->dev, "Unable to get dump size\n");
 		res = -ENXIO;
 		goto err2;
 	}
 
-	dump_list = devm_kmalloc(&pdev->dev, size * sizeof(unsigned int), GFP_KERNEL);
+	dump_list = devm_kzalloc(&pdev->dev, size * sizeof(unsigned int), GFP_KERNEL);
 	if (!dump_list) {
 		dev_info(&pdev->dev, "Unable to alloc mem\n");
 		res = -ENOMEM;
@@ -257,6 +306,125 @@ static int slc_err_probe(struct platform_device *pdev)
 		dev_info(&pdev->dev, "Unable to get assert\n");
 		res = -ENXIO;
 		goto err2;
+	}
+
+	error_flags_node = of_get_child_by_name(pdev->dev.of_node, "error-flags");
+	ret = 0;
+	if (error_flags_node) {
+		drvdata->error_flags_enable = 1;
+		dev_info(&pdev->dev, "error flags support\n");
+		drvdata->error_flags = devm_kzalloc(&pdev->dev, sizeof(*(drvdata->error_flags)), GFP_KERNEL);
+		if (!drvdata->error_flags) {
+			dev_info(&pdev->dev, "Failed to allocate memory for error_flags\n");
+			ret = -ENOMEM;
+			goto err2;
+		}
+
+		//int-sts
+		drvdata->error_flags->int_sts_len = of_property_count_u32_elems(error_flags_node, "int-sts");
+		if (drvdata->error_flags->int_sts_len < 0) {
+			dev_info(&pdev->dev, "Failed to count int-sts elements\n");
+			ret = drvdata->error_flags->int_sts_len;
+			goto err2;
+		}
+		drvdata->error_flags->int_sts = devm_kzalloc(&pdev->dev, drvdata->error_flags->int_sts_len * sizeof(u32), GFP_KERNEL);
+		if (!drvdata->error_flags->int_sts) {
+			dev_info(&pdev->dev, "Failed to allocate memory for int-sts\n");
+			ret = -ENOMEM;
+			goto err2;
+		}
+		ret = of_property_read_u32_array(error_flags_node, "int-sts", drvdata->error_flags->int_sts, drvdata->error_flags->int_sts_len);
+		if (ret) {
+			dev_info(&pdev->dev, "Failed to read int-sts property\n");
+			goto err2;
+		}
+
+
+		//int-sts-msk
+		drvdata->error_flags->int_sts_msk_len = of_property_count_u32_elems(error_flags_node, "int-sts-msk");
+		if (drvdata->error_flags->int_sts_msk_len < 0) {
+			dev_info(&pdev->dev, "Failed to count int-sts-msk elements\n");
+			ret = drvdata->error_flags->int_sts_msk_len;
+			goto err2;
+		}
+		drvdata->error_flags->int_sts_msk = devm_kzalloc(&pdev->dev, drvdata->error_flags->int_sts_msk_len * sizeof(u32), GFP_KERNEL);
+		if (!drvdata->error_flags->int_sts_msk) {
+			dev_info(&pdev->dev, "Failed to allocate memory for int-sts-msk\n");
+			ret = -ENOMEM;
+			goto err2;
+		}
+		ret = of_property_read_u32_array(error_flags_node, "int-sts-msk", drvdata->error_flags->int_sts_msk, drvdata->error_flags->int_sts_msk_len);
+		if (ret) {
+			dev_info(&pdev->dev, "Failed to read int-sts-msk property\n");
+			goto err2;
+		}
+
+		//int-sts-clr
+		drvdata->error_flags->int_sts_clr_len = of_property_count_u32_elems(error_flags_node, "int-sts-clr");
+		if (drvdata->error_flags->int_sts_clr_len < 0) {
+			dev_info(&pdev->dev, "Failed to count int-sts-clr elements\n");
+			ret = drvdata->error_flags->int_sts_clr_len;
+			goto err2;
+		}
+		drvdata->error_flags->int_sts_clr = devm_kzalloc(&pdev->dev, drvdata->error_flags->int_sts_clr_len * sizeof(u32), GFP_KERNEL);
+		if (!drvdata->error_flags->int_sts_clr) {
+			dev_info(&pdev->dev, "Failed to allocate memory for int-sts-clr\n");
+			ret = -ENOMEM;
+			goto err2;
+		}
+		ret = of_property_read_u32_array(error_flags_node, "int-sts-clr", drvdata->error_flags->int_sts_clr, drvdata->error_flags->int_sts_clr_len);
+		if (ret) {
+			dev_info(&pdev->dev, "Failed to read int-sts-clr property\n");
+			goto err2;
+		}
+
+		//dbg-info-mux-sel
+		ret = of_property_read_u32(error_flags_node, "dbg-info-mux-sel", &(drvdata->error_flags->dbg_info_mux_sel));
+		if (ret) {
+			dev_info(&pdev->dev, "Failed to get dbg_info_mux_sel property\n");
+			goto err2;
+		}
+
+		//dbg-info-mux-vals
+		drvdata->error_flags->dbg_info_mux_vals_len = of_property_count_u32_elems(error_flags_node, "dbg-info-mux-vals");
+                if (drvdata->error_flags->dbg_info_mux_vals_len < 0) {
+                        dev_info(&pdev->dev, "Failed to count dbg-info-mux-vals elements\n");
+                        ret = drvdata->error_flags->dbg_info_mux_vals_len;
+			goto err2;
+                }
+                drvdata->error_flags->dbg_info_mux_vals = devm_kzalloc(&pdev->dev, drvdata->error_flags->dbg_info_mux_vals_len * sizeof(u32), GFP_KERNEL);
+                if (!drvdata->error_flags->dbg_info_mux_vals) {
+                        dev_info(&pdev->dev, "Failed to allocate memory for dbg-info-mux-vals\n");
+                        ret = -ENOMEM;
+			goto err2;
+                }
+                ret = of_property_read_u32_array(error_flags_node, "dbg-info-mux-vals", drvdata->error_flags->dbg_info_mux_vals, drvdata->error_flags->dbg_info_mux_vals_len);
+                if (ret) {
+                        dev_info(&pdev->dev, "Failed to read dbg-info-mux-vals property\n");
+			goto err2;
+                }
+
+		//dbg-data
+		drvdata->error_flags->dbg_data_len = of_property_count_u32_elems(error_flags_node, "dbg-data");
+                if (drvdata->error_flags->dbg_data_len < 0) {
+                        dev_info(&pdev->dev, "Failed to count dbg-data elements\n");
+                        ret = drvdata->error_flags->dbg_data_len;
+			goto err2;
+                }
+                drvdata->error_flags->dbg_data = devm_kzalloc(&pdev->dev, drvdata->error_flags->dbg_data_len * sizeof(u32), GFP_KERNEL);
+                if (!drvdata->error_flags->dbg_data) {
+                        dev_info(&pdev->dev, "Failed to allocate memory for dbg-data\n");
+                        ret = -ENOMEM;
+			goto err2;
+                }
+                ret = of_property_read_u32_array(error_flags_node, "dbg-data", drvdata->error_flags->dbg_data, drvdata->error_flags->dbg_data_len);
+                if (ret) {
+                        dev_info(&pdev->dev, "Failed to read dbg-data property\n");
+			goto err2;
+                }
+	} else {
+		drvdata->error_flags_enable = 0;
+		dev_info(&pdev->dev, "error flags not support\n");
 	}
 
 	id = of_match_device(slc_parity_of_ids, &pdev->dev);
