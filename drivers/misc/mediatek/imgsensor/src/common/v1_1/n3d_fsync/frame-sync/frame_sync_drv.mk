245981c245eb49402e50e47ce6de9ee3b7fc1836
Jeff Teng <jeff.teng@mediatek.com>
2025-01-19 08:06:20 +0800

    [ALPS08967978] mt6781: seninf UT - kernel
    [Description]
    imgsensor/eeprom
    
    [Test]
    preflight
    
    MTK-Commit-Id: 41cc9daf7ab80e11adf13b9763f6cdf811488c64
    
    Signed-off-by: Jeff Teng <jeff.teng@mediatek.com>
    CR-Id: ALPS08967978
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: If6f2f9fc2e40d363103c85a7d2816de448ebdb32

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/n3d_fsync/frame-sync/frame_sync_drv.mk
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/n3d_fsync/frame-sync/frame_sync_drv.mk
@@ -7,8 +7,18 @@ subdir-ccflags-y += \
 
 LOCAL_FSYNC_PATH := $(LOCAL_N3D_PATH)/$(FRAME_SYNC)
 
+ifneq (,$(filter $(CONFIG_MTK_IMGSENSOR), m y))
 imgsensor_isp6s-objs += \
 	$(LOCAL_FSYNC_PATH)/frame_sync.o \
 	$(LOCAL_FSYNC_PATH)/frame_sync_algo.o \
 	$(LOCAL_FSYNC_PATH)/frame_monitor.o \
 
+endif
+
+ifneq (,$(filter $(CONFIG_MTK_IMGSENSOR_ISP6S_LAG), m y))
+imgsensor_isp6s_lag-objs += \
+	$(LOCAL_FSYNC_PATH)/frame_sync.o \
+	$(LOCAL_FSYNC_PATH)/frame_sync_algo.o \
+	$(LOCAL_FSYNC_PATH)/frame_monitor.o \
+
+endif
