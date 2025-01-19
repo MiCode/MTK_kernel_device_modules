c96d008daea2533f2b3419e8ede7437beaa8b26a
Cheng-Jui Wang <cheng-jui.wang@mediatek.com>
2025-01-19 08:58:39 +0800

    [ALPS09112047] irq_mon: Refactor mtk_irq_mon.h to increase compatibility
    [Description]
    - Rewrote functions as static inline definitions when CONFIG is not
      enabled. This allows for type checking of input parameters and avoids
      unused variable warnings.
    - Removed unnecessary line breaks and extern declarations.
    
    This change is primarily aimed at reducing the likelihood of build
    errors when switching configurations
    
    [Test]
    Boot
    
    MTK-Commit-Id: 3bfe4dfa229c25590fd173e508a6b8178577fdc0
    
    Signed-off-by: Cheng-Jui Wang <cheng-jui.wang@mediatek.com>
    CR-Id: ALPS09112047
    Feature: IRQ Monitor
    Change-Id: Ia391573933e70f33657840809e5ac837b09a86fe

--- drivers/misc/mediatek/include/mt-plat/mtk_irq_mon.h
+++ drivers/misc/mediatek/include/mt-plat/mtk_irq_mon.h
@@ -16,20 +16,22 @@ typedef int (*aee_callback_t)(unsigned int irq, enum irq_mon_aee_type type);
 #if IS_ENABLED(CONFIG_MTK_IRQ_MONITOR)
 int irq_mon_aee_callback_register(unsigned int irq, aee_callback_t fn);
 void irq_mon_aee_callback_unregister(unsigned int irq);
-
-extern void mt_aee_dump_irq_info(void);
+void mt_aee_dump_irq_info(void);
 void __irq_log_store(const char *func, int line);
 #define irq_log_store() __irq_log_store(__func__, __LINE__)
-
 #else
-#define irq_mon_aee_callback_register(a, b) do {} while (0)
-#define irq_mon_aee_callback_unregister(a) do {} while (0)
-
+static inline int irq_mon_aee_callback_register(unsigned int irq,
+						aee_callback_t fn)
+{
+	return -EOPNOTSUPP;
+}
+static inline void irq_mon_aee_callback_unregister(unsigned int irq)
+{
+}
 #define mt_aee_dump_irq_info() do {} while (0)
 static inline void __irq_log_store(const char *func, int line)
 {
 }
-
 #define irq_log_store() do {} while (0)
 #endif
 #endif
