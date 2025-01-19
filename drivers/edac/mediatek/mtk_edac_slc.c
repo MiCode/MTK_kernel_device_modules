5f1489f17183d77d94c130be0443153afc594f64
mtk31023 <meng-hsien.lin@mediatek.com>
2025-01-19 09:11:43 +0800

    [ALPS09415707] slc: mt6991: add dfd support
    [Description]
    add dfd support
    
    [Test]
    reboot
    
    MTK-Commit-Id: 8b160116a90547a378a7a98c4c71e57eb6527b97
    
    Signed-off-by: mtk31023 <meng-hsien.lin@mediatek.com>
    CR-Id: ALPS09415707
    Feature: slc api driver
    Change-Id: Id963a20d04dc4cbe3f8051260502a6f7975c695e

--- drivers/edac/mediatek/mtk_edac_slc.c
+++ drivers/edac/mediatek/mtk_edac_slc.c
@@ -53,9 +53,20 @@ struct error_flags_data{
 	int dbg_info_mux_vals_len;
 	unsigned int *dbg_data;
 	int dbg_data_len;
+	unsigned int dfd_enable;
+	struct dfd_data *dfd;
 };
 
-static enum error_type read_parity_status(struct edac_device_ctl_info *dci, unsigned int emi_id)
+struct dfd_data{
+	unsigned int *data_err_status;
+	unsigned int *tag_err_status;
+	unsigned int *latch_data;
+	unsigned int *sram_sel;
+	unsigned int data_num;
+	unsigned int tag_num;
+};
+
+static enum error_type read_parity_status(struct edac_device_ctl_info *dci, unsigned int emi_idx)
 {
 	struct arm_smccc_res smc_res;
 	struct slc_drvdata *drvdata = dci->pvt_info;
@@ -68,21 +79,21 @@ static enum error_type read_parity_status(struct edac_device_ctl_info *dci, unsi
 
 	for (port_idx = 0; port_idx < drvdata->port_num; port_idx++) {
 		arm_smccc_smc(MTK_SIP_EMIMPU_CONTROL, MTK_SLC_PARITY_SELECT,
-				emi_id, port_idx, 0, 0, 0, 0, &smc_res);
+				emi_idx, port_idx, 0, 0, 0, 0, &smc_res);
 		if (smc_res.a0) {
 			pr_info("%s:%d failed to clear slc parity, ret=0x%lx\n",
 				__func__, __LINE__, smc_res.a0);
 		}
 		for (chn_idx = 0; chn_idx < drvdata->chn_num; chn_idx++) {
 			for (cs_idx = 0; cs_idx < drvdata->cs_num; cs_idx++) {
-				content = readl(drvdata->base[emi_id] + drvdata->parity_err_status_offset + ((chn_idx * drvdata->cs_num + cs_idx) << 2));
+				content = readl(drvdata->base[emi_idx] + drvdata->parity_err_status_offset + ((chn_idx * drvdata->cs_num + cs_idx) << 2));
 				ecc_count = 0;
 				for (ecc_idx = 0; ecc_idx < 4; ecc_idx++) {
 					if (content >> ecc_idx)
 						++ecc_count;
 				}
 
-				snprintf(ecc_position, sizeof(ecc_position), "emi: %d, port: %d, chn: %d, cs: %d\n", emi_id, port_idx, chn_idx, cs_idx);
+				snprintf(ecc_position, sizeof(ecc_position), "emi: %d, port: %d, chn: %d, cs: %d\n", emi_idx, port_idx, chn_idx, cs_idx);
 
 				if (ecc_count == 1) {
 					partial_error_type = CORRECTABLE_ERROR;
@@ -106,12 +117,12 @@ static enum error_type read_parity_status(struct edac_device_ctl_info *dci, unsi
 	return total_error_type;
 }
 
-static void slc_clear_violation(unsigned int emi_id)
+static void slc_clear_violation(unsigned int emi_idx)
 {
 	struct arm_smccc_res smc_res;
 
 	arm_smccc_smc(MTK_SIP_EMIMPU_CONTROL, MTK_SLC_PARITY_CLEAR,
-			emi_id, 0, 0, 0, 0, 0, &smc_res);
+			emi_idx, 0, 0, 0, 0, 0, &smc_res);
 	if (smc_res.a0) {
 		pr_info("%s:%d failed to clear slc parity, ret=0x%lx\n",
 			__func__, __LINE__, smc_res.a0);
@@ -129,18 +140,20 @@ static irqreturn_t slc_err_handler(int irq, void *dev_id)
 	unsigned long long parity_err_tol;
 	unsigned int parity_err_value;
 	unsigned int parity_err_ext_value;
-	unsigned int emi_id, i, j, reg_val;
+	unsigned int emi_idx, chn_idx, i, j, reg_val, tag_idx, data_idx;
+	unsigned int sram_type;
+	struct dfd_data *dfd = NULL;
 
-	for (emi_id = 0; emi_id < drvdata->slc_parity_cnt; emi_id++) {
-		parity_err_value = readl(drvdata->base[emi_id] + drvdata->parity_err_offset);
-		parity_err_ext_value = readl(drvdata->base[emi_id] + drvdata->parity_err_ext_offset);
+	for (emi_idx = 0; emi_idx < drvdata->slc_parity_cnt; emi_idx++) {
+		parity_err_value = readl(drvdata->base[emi_idx] + drvdata->parity_err_offset);
+		parity_err_ext_value = readl(drvdata->base[emi_idx] + drvdata->parity_err_ext_offset);
         	parity_err_tol = parity_err_value + ((unsigned long long)parity_err_ext_value << 32);
 		if (parity_err_tol > 0)
-			ecc_mesg_idx = snprintf(ecc_mesg + ecc_mesg_idx, sizeof(ecc_mesg) - ecc_mesg_idx, "emi: %d, overall: %llx\n", emi_id, parity_err_tol);
+			ecc_mesg_idx = snprintf(ecc_mesg + ecc_mesg_idx, sizeof(ecc_mesg) - ecc_mesg_idx, "emi: %d, overall: %llx\n", emi_idx, parity_err_tol);
 	}
 
-	for (emi_id = 0; emi_id < drvdata->slc_parity_cnt; emi_id++) {
-		partial_error_type = read_parity_status(dci, emi_id);
+	for (emi_idx = 0; emi_idx < drvdata->slc_parity_cnt; emi_idx++) {
+		partial_error_type = read_parity_status(dci, emi_idx);
 		if (total_error_type == NO_ERROR) {
 			if (partial_error_type != NO_ERROR)
 				total_error_type = partial_error_type;
@@ -150,8 +163,8 @@ static irqreturn_t slc_err_handler(int irq, void *dev_id)
 		}
 	}
 
-	for (emi_id = 0; emi_id < drvdata->slc_parity_cnt; emi_id++) {
-		slc_clear_violation(emi_id);
+	for (emi_idx = 0; emi_idx < drvdata->slc_parity_cnt; emi_idx++) {
+		slc_clear_violation(emi_idx);
 	}
 
 	if (total_error_type == UNCORRECTABLE_ERROR) {
@@ -159,30 +172,70 @@ static irqreturn_t slc_err_handler(int irq, void *dev_id)
 		aee_kernel_exception("SLC_PARITY", ecc_mesg);
 	}
 
+	//error flag
 	total_error_type = NO_ERROR;
 	if (drvdata->error_flags_enable == 1) {
+		pr_info("error flags\n");
 		//dump error flag
-		for (emi_id = 0; emi_id < drvdata->slc_parity_cnt; emi_id++) {
-			pr_info("emi: %d\n", emi_id);
+		for (emi_idx = 0; emi_idx < drvdata->slc_parity_cnt; emi_idx++) {
+			pr_info("emi: %d\n", emi_idx);
 			for (i=0; i<drvdata->error_flags->int_sts_len; ++i) {
-				reg_val = readl(drvdata->base[emi_id] + drvdata->error_flags->int_sts[i]);
+				reg_val = readl(drvdata->base[emi_idx] + drvdata->error_flags->int_sts[i]);
 				if ((reg_val & drvdata->error_flags->int_sts_msk[i]) != 0 )
 					total_error_type = UNCORRECTABLE_ERROR;
-				pr_info("%x: %x\n", drvdata->error_flags->int_sts[i], reg_val);
+				pr_info("\t%x: %x", drvdata->error_flags->int_sts[i], reg_val);
 			}
+			pr_info("\n");
 			for (i=0; i<drvdata->error_flags->dbg_info_mux_vals_len; ++i) {
-				pr_info("mux: %d\n", drvdata->error_flags->dbg_info_mux_vals[i]);
-				writel(drvdata->error_flags->dbg_info_mux_vals[i], drvdata->base[emi_id] + drvdata->error_flags->dbg_info_mux_sel);
+				pr_info("mux: %d", drvdata->error_flags->dbg_info_mux_vals[i]);
+				writel(drvdata->error_flags->dbg_info_mux_vals[i], drvdata->base[emi_idx] + drvdata->error_flags->dbg_info_mux_sel);
 				for (j=0; j<drvdata->error_flags->dbg_data_len; ++j) {
-					pr_info("\t%x: %x\n", drvdata->error_flags->dbg_data[j], readl(drvdata->base[emi_id] + drvdata->error_flags->dbg_data[j]));
+					pr_info("\t%x: %x", drvdata->error_flags->dbg_data[j], readl(drvdata->base[emi_idx] + drvdata->error_flags->dbg_data[j]));
+				}
+				pr_info("\n");
+			}
+		}
+
+		//dfd
+		if (drvdata->error_flags->dfd_enable == 1) {
+			pr_info("dfd\n");
+			dfd = drvdata->error_flags->dfd;
+			for (emi_idx = 0; emi_idx < drvdata->slc_parity_cnt; emi_idx++) {
+				pr_info("emi: %d\n", emi_idx);
+				for (chn_idx=0; chn_idx < drvdata->chn_num; ++chn_idx) {
+					pr_info("chn: %d\n", chn_idx);
+					//data
+					sram_type = 0;
+					pr_info("data sram\n");
+					reg_val = readl(drvdata->base[emi_idx] + dfd->data_err_status[chn_idx]);
+					for (data_idx = 0; data_idx < dfd->data_num; ++data_idx) {
+						if (((reg_val>>data_idx) & 0x1) == 1) {
+							reg_val = (sram_type << 30) | data_idx;
+							writel(reg_val, drvdata->base[emi_idx] + dfd->sram_sel[chn_idx]);
+							pr_info("\t%x: %x", data_idx, readl(drvdata->base[emi_idx] + dfd->latch_data[chn_idx]));
+						}
+					}
+					pr_info("\n");
+					//tag
+					sram_type = 1;
+					pr_info("tag sram\n");
+					reg_val = readl(drvdata->base[emi_idx] + dfd->tag_err_status[chn_idx]);
+					for (tag_idx = 0; tag_idx < dfd->tag_num; ++tag_idx) {
+						if (((reg_val>>tag_idx) & 0x1) == 1) {
+							reg_val = (sram_type << 30) | tag_idx;
+							writel(reg_val, drvdata->base[emi_idx] + dfd->sram_sel[chn_idx]);
+							pr_info("\t%x: %x", tag_idx, readl(drvdata->base[emi_idx] + dfd->latch_data[chn_idx]));
+						}
+					}
+					pr_info("\n");
 				}
 			}
 		}
 #if 0
 		//clear error flag
-		for (emi_id = 0; emi_id < drvdata->slc_parity_cnt; emi_id++) {
+		for (emi_idx = 0; emi_idx < drvdata->slc_parity_cnt; emi_idx++) {
 			for (i=0; i<drvdata->error_flags->int_sts_clr_len; ++i) {
-				writel(0xffffffff, drvdata->base[emi_id] + drvdata->error_flags->int_sts_clr[i]);
+				writel(0xffffffff, drvdata->base[emi_idx] + drvdata->error_flags->int_sts_clr[i]);
 			}
 		}
 #endif
@@ -212,6 +265,8 @@ static int slc_err_probe(struct platform_device *pdev)
 	int size, ret, i;
 	unsigned int *dump_list;
 	struct device_node *error_flags_node = NULL;
+	struct device_node *dfd_node = NULL;
+	struct dfd_data *dfd = NULL;
 
 	dci = edac_device_alloc_ctl_info(sizeof(*drvdata), "slc", 1, "slc", 1, 0, 0);
 	if (!dci) {
@@ -422,6 +477,86 @@ static int slc_err_probe(struct platform_device *pdev)
                         dev_info(&pdev->dev, "Failed to read dbg-data property\n");
 			goto err2;
                 }
+		dfd_node = of_get_child_by_name(error_flags_node, "dfd");
+		if (dfd_node) {
+			drvdata->error_flags->dfd_enable = 1;
+			dev_info(&pdev->dev, "dfd support\n");
+			drvdata->error_flags->dfd = devm_kzalloc(&pdev->dev, sizeof(*(drvdata->error_flags->dfd)), GFP_KERNEL);
+			if (!drvdata->error_flags->dfd) {
+				dev_info(&pdev->dev, "Failed to allocate memory for dfd\n");
+				ret = -ENOMEM;
+				goto err2;
+			}
+			dfd = drvdata->error_flags->dfd;
+
+			//data-err-status
+			dfd->data_err_status = devm_kzalloc(&pdev->dev, drvdata->chn_num * sizeof(u32), GFP_KERNEL);
+			if (!dfd->data_err_status) {
+				dev_info(&pdev->dev, "Failed to allocate memory for data-err-status\n");
+				ret = -ENOMEM;
+				goto err2;
+			}
+			ret = of_property_read_u32_array(dfd_node, "data-err-status", dfd->data_err_status, drvdata->chn_num);
+			if (ret) {
+				dev_info(&pdev->dev, "Failed to read data-err-status property\n");
+				goto err2;
+			}
+
+			//tag-err-status
+			dfd->tag_err_status = devm_kzalloc(&pdev->dev, drvdata->chn_num * sizeof(u32), GFP_KERNEL);
+			if (!dfd->tag_err_status) {
+				dev_info(&pdev->dev, "Failed to allocate memory for tag-err-status\n");
+				ret = -ENOMEM;
+				goto err2;
+			}
+			ret = of_property_read_u32_array(dfd_node, "tag-err-status", dfd->tag_err_status, drvdata->chn_num);
+			if (ret) {
+				dev_info(&pdev->dev, "Failed to read tag-err-status property\n");
+				goto err2;
+			}
+
+			//data-num
+			ret = of_property_read_u32(dfd_node, "data-num", &(dfd->data_num));
+			if (ret) {
+				dev_info(&pdev->dev, "Unable to get data-num\n");
+				res = -ENXIO;
+				goto err2;
+			}
+
+			//tag-num
+			ret = of_property_read_u32(dfd_node, "tag-num", &(dfd->tag_num));
+			if (ret) {
+				dev_info(&pdev->dev, "Unable to get tag-num\n");
+				res = -ENXIO;
+				goto err2;
+			}
+
+			//latch-data
+			dfd->latch_data = devm_kzalloc(&pdev->dev, drvdata->chn_num * sizeof(u32), GFP_KERNEL);
+			if (!dfd->latch_data) {
+				dev_info(&pdev->dev, "Failed to allocate memory for latch-data\n");
+				ret = -ENOMEM;
+				goto err2;
+			}
+			ret = of_property_read_u32_array(dfd_node, "latch-data", dfd->latch_data, drvdata->chn_num);
+			if (ret) {
+				dev_info(&pdev->dev, "Failed to read latch-data property\n");
+				goto err2;
+			}
+
+			//sram-sel
+			dfd->sram_sel = devm_kzalloc(&pdev->dev, drvdata->chn_num * sizeof(u32), GFP_KERNEL);
+			if (!dfd->sram_sel) {
+				dev_info(&pdev->dev, "Failed to allocate memory for sram-sel\n");
+				ret = -ENOMEM;
+				goto err2;
+			}
+			ret = of_property_read_u32_array(dfd_node, "sram-sel", dfd->sram_sel, drvdata->chn_num);
+			if (ret) {
+				dev_info(&pdev->dev, "Failed to read sram-sel property\n");
+				goto err2;
+			}
+		}
 	} else {
 		drvdata->error_flags_enable = 0;
 		dev_info(&pdev->dev, "error flags not support\n");
