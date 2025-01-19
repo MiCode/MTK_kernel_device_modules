2427a37d10a36baf69de75a5a0f4d52fe0f858ee
Hungwen Chien <hungwen.chien@mediatek.com>
2025-01-19 08:25:35 +0800

    [ALPS08686299] pkvm_tmem: Revise protocol of dyn_hcall
    [Description]
    Due to latest ACK update, need to reivse dyn_hcall protocol
    from kvm_cpu_context to user_pt_regs
    
    [Test]
    Boot and build pass
    
    MTK-Commit-Id: 4fa0737c3728e982ae39352e49f479526ce767b8
    
    Signed-off-by: Hungwen Chien <hungwen.chien@mediatek.com>
    CR-Id: ALPS08686299
    Feature: trusted_mem
    Change-Id: I0749ec81ac9b24baa4f6f621a1a5bdd2f6714aac

--- drivers/misc/mediatek/pkvm_tmem/hyp/pkvm_tmem_hyp.c
+++ drivers/misc/mediatek/pkvm_tmem/hyp/pkvm_tmem_hyp.c
@@ -13,7 +13,7 @@
 #include "hyp_tmem_mpu.h"
 #include "hyp_pmm.h"
 
-#define cpu_reg(ctxt, r)    ((ctxt)->regs.regs[r])
+#define cpu_reg(ctxt, r)    ((ctxt)->regs[r])
 
 static const struct pkvm_module_ops *tmem_ops;
 
@@ -26,33 +26,33 @@ int hyp_tmem_init(const struct pkvm_module_ops *ops)
 	return 0;
 }
 
-void hyp_region_protect(struct kvm_cpu_context *ctx)
+void hyp_region_protect(struct user_pt_regs *regs)
 {
 	tmem_ops->puts("pkvm_tmem: hyp_region_protect\n");
-	cpu_reg(ctx, 1) = enable_region_protection(ctx->regs.regs[1],
-						ctx->regs.regs[2], ctx->regs.regs[3], tmem_ops);
-	cpu_reg(ctx, 0) = SMCCC_RET_SUCCESS;
+	cpu_reg(regs, 1) = enable_region_protection(regs->regs[1],
+						regs->regs[2], regs->regs[3], tmem_ops);
+	cpu_reg(regs, 0) = SMCCC_RET_SUCCESS;
 }
 
-void hyp_region_unprotect(struct kvm_cpu_context *ctx)
+void hyp_region_unprotect(struct user_pt_regs *regs)
 {
 	tmem_ops->puts("pkvm_tmem: hyp_region_unprotect\n");
-	cpu_reg(ctx, 1) = disable_region_protection(ctx->regs.regs[1], tmem_ops);
-	cpu_reg(ctx, 0) = SMCCC_RET_SUCCESS;
+	cpu_reg(regs, 1) = disable_region_protection(regs->regs[1], tmem_ops);
+	cpu_reg(regs, 0) = SMCCC_RET_SUCCESS;
 }
 
-void hyp_page_protect(struct kvm_cpu_context *ctx)
+void hyp_page_protect(struct user_pt_regs *regs)
 {
 	tmem_ops->puts("pkvm_tmem: hyp_page_protect\n");
-	cpu_reg(ctx, 1) = hyp_pmm_secure_pglist(ctx->regs.regs[1],
-						ctx->regs.regs[2], ctx->regs.regs[3], tmem_ops);
-	cpu_reg(ctx, 0) = SMCCC_RET_SUCCESS;
+	cpu_reg(regs, 1) = hyp_pmm_secure_pglist(regs->regs[1],
+						regs->regs[2], regs->regs[3], tmem_ops);
+	cpu_reg(regs, 0) = SMCCC_RET_SUCCESS;
 }
 
-void hyp_page_unprotect(struct kvm_cpu_context *ctx)
+void hyp_page_unprotect(struct user_pt_regs *regs)
 {
 	tmem_ops->puts("pkvm_tmem: hyp_page_unprotect\n");
-	cpu_reg(ctx, 1) = hyp_pmm_unsecure_pglist(ctx->regs.regs[1],
-						ctx->regs.regs[2], ctx->regs.regs[3], tmem_ops);
-	cpu_reg(ctx, 0) = SMCCC_RET_SUCCESS;
+	cpu_reg(regs, 1) = hyp_pmm_unsecure_pglist(regs->regs[1],
+						regs->regs[2], regs->regs[3], tmem_ops);
+	cpu_reg(regs, 0) = SMCCC_RET_SUCCESS;
 }
