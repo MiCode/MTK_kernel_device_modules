d049a24322ccc3ef09cc010d7de37555e5b43a09
Ming-Fan Chen <ming-fan.chen@mediatek.com>
2025-01-19 08:48:53 +0800

    [ALPS09212854] mmdebug: workqueue for mmdvfs dump
    [Description]
    mmdebug workqueue for mmdvfs dump
    
    [Test]
    Sanity pass
    
    MTK-Commit-Id: 8e4545315a891d191b4bf9217e6e3dae9a67a241
    
    CR-Id: ALPS09212854
    Feature: mmdvfs
    Signed-off-by: Ming-Fan Chen <ming-fan.chen@mediatek.com>
    Change-Id: Ib0d878b1c59799137f1b13a0c38c323fbd1dc2b4

--- drivers/misc/mediatek/mmdebug/mtk-mmdebug-vcp.c
+++ drivers/misc/mediatek/mmdebug/mtk-mmdebug-vcp.c
@@ -11,6 +11,7 @@
 #include <linux/module.h>
 #include <linux/of_device.h>
 #include <linux/platform_device.h>
+#include <linux/workqueue.h>
 #include <soc/mediatek/smi.h>
 
 #if IS_ENABLED(CONFIG_MTK_AEE_FEATURE)
@@ -28,6 +29,9 @@ static DEFINE_MUTEX(mmdebug_vcp_pwr_mutex);
 static struct mmdebug_ipi_data mmdebug_vcp_ipi_data;
 
 static bool mmdebug_init_done;
+
+struct workqueue_struct *mmdebug_workq;
+struct work_struct mmdebug_work;
 
 bool mmdebug_is_init_done(void)
 {
@@ -78,6 +82,11 @@ enable_vcp_end:
 	return ret;
 }
 
+static void mmdebug_work_impl(struct work_struct *work)
+{
+	raw_notifier_call_chain(&mmdebug_status_dump_notifier_list, 0, NULL);
+}
+
 static int mmdebug_vcp_ipi_cb(unsigned int ipi_id, void *prdata, void *data,
 	unsigned int len) // vcp > ap
 {
@@ -105,7 +114,11 @@ static int mmdebug_vcp_ipi_cb(unsigned int ipi_id, void *prdata, void *data,
 #endif
 		MMDEBUG_ERR("MMDEBUG kernel warning str:%s idx:%hhu ack:%hhu base:%hhu",
 			kernel_warn_type_str[slot.idx], slot.idx, slot.ack, slot.base);
-		raw_notifier_call_chain(&mmdebug_status_dump_notifier_list, 0, NULL);
+
+		if (!work_pending(&mmdebug_work))
+			queue_work(mmdebug_workq, &mmdebug_work);
+		else
+			MMDEBUG_ERR("queue work fail");
 		break;
 	default:
 		MMDEBUG_ERR("ipi_id:%u func:%hhu idx:%hhu ack:%hhu base:%hhu",
@@ -169,6 +182,10 @@ static int mmdebug_vcp_probe(struct platform_device *pdev)
 	kthr_vcp = kthread_run(mmdebug_vcp_init_thread, NULL, "mmdebug-vcp");
 	if (IS_ERR(kthr_vcp))
 		MMDEBUG_ERR("create kthread mmdebug_vcp_init_thread failed");
+
+	mmdebug_workq = create_singlethread_workqueue("mmdebug_workq");
+	INIT_WORK(&mmdebug_work, mmdebug_work_impl);
+
 	MMDEBUG_DBG("probe success");
 	return 0;
 }
