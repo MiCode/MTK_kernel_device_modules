bb437b33b1ff5f50981094bbb1ae112976d33ec2
Kuanlin Wang <kuan-lin.wang@mediatek.com>
2025-01-19 09:56:48 +0800

    [ALPS09704042] pmic: Add Mbrain parity error count for MT6993
    [Description]
    Add Mbrain parity error count for MT6993
    
    [Test]
    test pass
    
    MTK-Commit-Id: 2ceca2591529ce6699800fd6ceb908e805315e20
    
    Signed-off-by: Kuanlin Wang <kuan-lin.wang@mediatek.com>
    CR-Id: ALPS09704042
    Feature: pmic_driver
    Change-Id: Ic12efc0f08c4f6a1f79ee7ad33769bb79128b9e6

--- drivers/mfd/mtk-spmi-pmic-debug.c
+++ drivers/mfd/mtk-spmi-pmic-debug.c
@@ -2,6 +2,7 @@
 //
 // Copyright (c) 2024 MediaTek Inc.
 
+#include <linux/bitfield.h>
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
@@ -12,8 +13,23 @@
 #include "mtk-spmi-pmic-debug.h"
 
 #define MT6316_SWCID_L_E4_CODE                (0x30)
+#define PARITY_ERROR_CLR_RDY                   0
+/* For mt6688 parity error count */
+#define MT6688_REG_TOP_INFO1		0x00
+#define MT6688_REG_SPMI_DEBUG_SEL	0x21A
+#define MT6688_MASK_VENID		GENMASK(7, 4)
+#define MT6688_MASK_REVISION		GENMASK(3, 0)
+#define MT6688_MASK_RG_PARITY_ERR_CLR	BIT(3)
+#define MT6688_MASK_RG_SPMI_DBGMUX_SEL	GENMASK(5, 0)
+#define MT6688_VENDOR_ID		0x80
+#define MT6688_PARITY_ERROR_TYPE_4	0x38
+#define MT6688_CHIP_REV_E1		0
+#define MT6688_CHIP_REV_E2		2
+#define MT6688_CHIP_REV_E3		3
+
 struct mutex dump_mutex;
 struct mtk_spmi_pmic_debug_data {
+	struct device *dev;
 	struct mutex lock;
 	struct regmap *regmap;
 	unsigned int reg_value;
@@ -26,6 +42,9 @@ struct mtk_spmi_pmic_debug_data {
 	unsigned int spmi_debug_out_l;
 	unsigned int spmi_debug_out_l_mask;
 	unsigned int spmi_debug_out_l_shift;
+	unsigned int spmi_parity_err_clr;
+	unsigned int spmi_parity_err_sts;
+	unsigned int spmi_parity_err_cnt;
 };
 
 static struct mtk_spmi_pmic_debug_data *mtk_spmi_pmic_debug[SPMI_MAX_SLAVE_ID];
@@ -185,6 +204,138 @@ void mtk_spmi_pmic_get_glitch_cnt(u16 *buf)
 }
 EXPORT_SYMBOL_GPL(mtk_spmi_pmic_get_glitch_cnt);
 
+static bool mtk_spmi_pmic_is_mt6688(struct mtk_spmi_pmic_debug_data *data, int *rev)
+{
+	static bool is_mt6688;
+	static int last_rev_val;
+	int ret;
+	u32 val;
+
+	if (!is_mt6688 && data->cid == 0x88 && data->cid_addr_h == 0x66 &&
+	    data->cid_addr_l == 0x88 && strstr(dev_name(data->dev), "mtk-spmi-pmic-debug")) {
+		ret = regmap_read(data->regmap, MT6688_REG_TOP_INFO1, &val);
+		if (ret)
+			return false;
+
+		if ((val & MT6688_MASK_VENID) != MT6688_VENDOR_ID)
+			return false;
+
+		last_rev_val = FIELD_GET(MT6688_MASK_REVISION, val);
+		is_mt6688 = true;
+	}
+
+	if (is_mt6688 && rev)
+		*rev = last_rev_val;
+
+	return is_mt6688;
+}
+
+#if PARITY_ERROR_CLR_RDY
+void mtk_spmi_pmic_clr_parity_err_sts_cnt(void)
+{
+	struct regmap *regmap;
+
+	unsigned int i, clr_addr;
+	struct device *dev;
+	int ret;
+
+	mutex_lock(&dump_mutex);
+	for (i = 2; i <= 15; i++) {
+		if (mtk_spmi_pmic_debug[i] == NULL)
+			continue;
+
+		regmap = mtk_spmi_pmic_debug[i]->regmap;
+		clr_addr = mtk_spmi_pmic_debug[i]->spmi_parity_err_clr;
+		dev = mtk_spmi_pmic_debug[i]->dev;
+		if (mtk_spmi_pmic_debug[i]->spmi_parity_err_cnt >= 0xff) {
+			if ((mtk_spmi_pmic_debug[i]->cid == 0x61)
+			     || (mtk_spmi_pmic_debug[i]->cid == 0x67)) {
+				regmap_write(regmap, clr_addr, 1);
+				regmap_write(regmap, clr_addr, 0);
+			} else if (mtk_spmi_pmic_is_mt6688(mtk_spmi_pmic_debug[i], NULL)) {
+				ret = regmap_write_bits(regmap, clr_addr, MT6688_MASK_RG_PARITY_ERR_CLR,
+							MT6688_MASK_RG_PARITY_ERR_CLR);
+				if (ret)
+					dev_info(dev, "Failed to set mt6688 parity err clr to 1\n");
+
+				ret = regmap_write_bits(regmap, clr_addr, MT6688_MASK_RG_PARITY_ERR_CLR, 0);
+				if (ret)
+					dev_info(dev, "Failed to set mt6688 parity err clr to 0\n");
+			}
+		}
+	}
+	mutex_unlock(&dump_mutex);
+}
+EXPORT_SYMBOL_GPL(mtk_spmi_pmic_clr_parity_err_sts_cnt);
+#endif
+
+void mtk_spmi_pmic_get_parity_err_cnt(u16 *buf)
+{
+	struct regmap *regmap;
+
+	unsigned int i, parity_err_sta = 0, parity_err_cnt = 0, sts_addr, cnt_addr;
+	u16 parity_err_cnt_array[spmi_parity_err_idx_cnt] = {0};
+	struct device *dev;
+	int ret, rev;
+
+	parity_err_cnt_array[0] = 1; //temp hardcode version
+	mutex_lock(&dump_mutex);
+	for (i = 2; i <= 15; i++) {
+		if (mtk_spmi_pmic_debug[i] == NULL)
+			continue;
+		dev = mtk_spmi_pmic_debug[i]->dev;
+		regmap = mtk_spmi_pmic_debug[i]->regmap;
+		sts_addr = mtk_spmi_pmic_debug[i]->spmi_parity_err_sts;
+		cnt_addr = mtk_spmi_pmic_debug[i]->spmi_parity_err_cnt;
+
+		if ((mtk_spmi_pmic_debug[i]->cid == 0x61)
+		     || (mtk_spmi_pmic_debug[i]->cid == 0x67)) {
+			/* dump parity err status */
+			regmap_read(regmap, sts_addr, &parity_err_sta);
+
+			/* dump parity err counter */
+			regmap_read(regmap, cnt_addr, &parity_err_cnt);
+
+			parity_err_cnt_array[2*i+1] = parity_err_sta;
+			parity_err_cnt_array[2*i+2] = parity_err_cnt;
+		} else if (mtk_spmi_pmic_is_mt6688(mtk_spmi_pmic_debug[i], &rev)) {
+			ret = regmap_update_bits(regmap, MT6688_REG_SPMI_DEBUG_SEL,
+						 MT6688_MASK_RG_SPMI_DBGMUX_SEL,
+						 MT6688_PARITY_ERROR_TYPE_4);
+			if (ret) {
+				dev_info(dev, "Failed to select mt6688 dbgmux to parity error type 4\n");
+				continue;
+			}
+			if (rev == MT6688_CHIP_REV_E3) {
+				/* For E3 read parity err status only! */
+				ret = regmap_read(regmap, sts_addr, &parity_err_sta);
+				if (ret)
+					dev_info(dev, "Failed to read mt6688 parity err status\n");
+			}
+			ret = regmap_read(regmap, cnt_addr, &parity_err_cnt);
+			if (ret)
+				dev_info(dev, "Failed to read mt6688 parity err cnt\n");
+
+			parity_err_cnt_array[2*i+1] = parity_err_sta;
+			parity_err_cnt_array[2*i+2] = parity_err_cnt;
+		}
+	}
+
+	if (buf != NULL)
+		memcpy(buf, parity_err_cnt_array, spmi_parity_err_idx_cnt*sizeof(u16));
+	else {
+		for (i = 1; i < spmi_parity_err_idx_cnt; i+=2) {
+			pr_info("%s SLVID(0x%x): parity_err_sta 0x%x, parity_err_cnt 0x%x",
+				__func__, (i-1)/2, parity_err_cnt_array[i], parity_err_cnt_array[i+1]);
+		}
+	}
+	mutex_unlock(&dump_mutex);
+
+	/* hardware issue: clear RG will clear glitch and parity */
+	// mtk_spmi_pmic_clr_parity_err_sts_cnt();
+}
+EXPORT_SYMBOL_GPL(mtk_spmi_pmic_get_parity_err_cnt);
+
 static int mtk_spmi_debug_parse_dt(struct device *dev, struct mtk_spmi_pmic_debug_data *data)
 {
 	struct device_node *node_parent, *node;
@@ -239,6 +390,17 @@ static int mtk_spmi_debug_parse_dt(struct device *dev, struct mtk_spmi_pmic_debu
 		data->spmi_debug_out_l_mask = debug_out_l[1];
 		data->spmi_debug_out_l_shift = debug_out_l[2];
 	}
+	err = of_property_read_u32(node, "spmi-parity-err-clr", &data->spmi_parity_err_clr);
+	if (err)
+		dev_info(dev, "%s slvid 0x%x no spmi-parity-err-clr found\n", __func__, data->usid);
+
+	err = of_property_read_u32(node, "spmi-parity-err-sts", &data->spmi_parity_err_sts);
+	if (err)
+		dev_info(dev, "%s slvid 0x%x no spmi-parity-err-sts found\n", __func__, data->usid);
+
+	err = of_property_read_u32(node, "spmi-parity-err-cnt", &data->spmi_parity_err_cnt);
+	if (err)
+		dev_info(dev, "%s slvid 0x%x no spmi-parity-err-cnt found\n", __func__, data->usid);
 	return data->usid;
 }
 
@@ -252,6 +414,7 @@ static int mtk_spmi_pmic_debug_probe(struct platform_device *pdev)
 	if (!data)
 		return -ENOMEM;
 
+	data->dev = &pdev->dev;
 	mutex_init(&data->lock);
 	mutex_init(&dump_mutex);
 	data->regmap = dev_get_regmap(pdev->dev.parent, NULL);
@@ -289,6 +452,7 @@ static const struct of_device_id mtk_spmi_pmic_debug_of_match[] = {
 	{ .compatible = "mediatek,spmi-pmic-8-debug", },
 	{ .compatible = "mediatek,spmi-pmic-12-debug", },
 	{ .compatible = "mediatek,spmi-pmic-13-debug", },
+	{ .compatible = "mediatek,spmi-pmic-15-debug", },
 	{ }
 };
 MODULE_DEVICE_TABLE(of, mtk_spmi_pmic_debug_of_match);
--- drivers/mfd/mtk-spmi-pmic-debug.h
+++ drivers/mfd/mtk-spmi-pmic-debug.h
@@ -6,7 +6,9 @@
 #define __MTK_SPMI_PMIC_DEBUG_H__
 
 #define spmi_glitch_idx_cnt 65
+#define spmi_parity_err_idx_cnt 33
 extern void mtk_spmi_pmic_get_glitch_cnt(u16 *buf);
+extern void mtk_spmi_pmic_get_parity_err_cnt(u16 *buf);
 
 MODULE_AUTHOR("HS Chien <HS.Chien@mediatek.com>");
 MODULE_DESCRIPTION("Debug driver for MediaTek SPMI PMIC");
