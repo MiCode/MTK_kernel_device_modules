73521d83d4108cecf469e50e50d539b092180ad5
Raymond Lin <yu-jui.lin@mediatek.com>
2025-01-19 09:07:18 +0800

    [ALPS09502333] USB offload index
    [Description]
    - USB offload index development
    
    [Test]
    - MBTF Test
    
    MTK-Commit-Id: 8c7f6e9262d77446eea2b8f2ab3b1094ff242b84
    
    Signed-off-by: Raymond Lin <yu-jui.lin@mediatek.com>
    CR-Id: ALPS09502333
    Feature: MBrain
    Change-Id: I8351b540e65047ddf70a37cbf2fd930abf8edfa0

--- drivers/misc/mediatek/mbraink/mbraink_usb.c
+++ drivers/misc/mediatek/mbraink/mbraink_usb.c
@@ -5,6 +5,7 @@
 
 #include <linux/module.h>
 #include <usb_xhci/quirks.h>
+#include <usb_offload/usb_offload.h>
 
 #include "mbraink_modules_ops_def.h"
 #include "mbraink_usb.h"
@@ -43,11 +44,42 @@ void usb_xhci_enum_info(struct xhci_mbrain data)
 	mbraink_netlink_send_msg(netlink_buf);
 }
 
+void usb_uo_info(struct uo_mbrain data)
+{
+	unsigned int vid = data.vid;
+	unsigned int pid = data.pid;
+	int phase = (int)data.phase;
+	int error = (int)data.error;
+
+	char netlink_buf[NETLINK_EVENT_MESSAGE_SIZE] = {'\0'};
+	int n = 0;
+	int pos = 0;
+	struct timespec64 tv = { 0 };
+	long long timestamp = 0;
+
+	ktime_get_real_ts64(&tv);
+	timestamp = (tv.tv_sec*1000)+(tv.tv_nsec/1000000);
+	n = snprintf(netlink_buf + pos,
+			NETLINK_EVENT_MESSAGE_SIZE - pos,
+			"%s:%llu:%d:%d:%d:%d",
+			NETLINK_EVENT_USB_OFFLOAD,
+			timestamp,
+			vid,
+			pid,
+			phase,
+			error);
+
+	if (n < 0 || n >= NETLINK_EVENT_MESSAGE_SIZE - pos)
+		return;
+	mbraink_netlink_send_msg(netlink_buf);
+}
+
 int mbraink_usb_init(void)
 {
 	int ret = 0;
 
 	ret = register_xhci_enum_mbrain_cb(usb_xhci_enum_info);
+	ret = register_uo_mbrain_cb(usb_uo_info);
 	return ret;
 }
 
@@ -56,5 +88,6 @@ int mbraink_usb_deinit(void)
 	int ret = 0;
 
 	ret = unregister_xhci_enum_mbrain_cb();
+	ret = unregister_uo_mbrain_cb();
 	return ret;
 }
