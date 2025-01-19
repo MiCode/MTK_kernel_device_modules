ec413b34bebd04b40f5ad86a4ca1c33812bc649d
mtk28406 <natalie.yang@mediatek.com>
2025-01-19 09:11:30 +0800

    [ALPS09486326] mt6991: PDMA support multi-context
    [Description]
    1. CID-based resource mangement
    2. Remove most global variable
    3. Refine kernel header for user ioctl
    4. Add pbha managemnt
    
    [Test]
    bootup pass, UT pass
    
    MTK-Commit-Id: ce2a960ad3a43de3669e92195fb6bf187258b442
    
    Signed-off-by: mtk28406 <natalie.yang@mediatek.com>
    CR-Id: ALPS09486326
    Feature: MFGSYS driver
    Change-Id: I0fed471c0801fc3942200e5987683897af2ec5aa

--- drivers/gpu/mediatek/gpu_pdma/include/gpu_pdma.h
+++ drivers/gpu/mediatek/gpu_pdma/include/gpu_pdma.h
@@ -7,23 +7,125 @@
 #define GPU_PDMA_H
 
 #define PDMA_RINGBUF_PA_NUM		8
+
+typedef enum
+{
+	SMART_CACHE_API = 0,
+	COMPUTE_TLS,
+	UNSUPPORTED_MODE
+} CCMD_CACHE_MODE;
+
 /**
- * @in: Input parameters
- * @in.kctx_id: Context ID that locks HW.
- * @in.mode: Switch default policy by mode.
- * @out: Output parameters
- * @out.status: 0: Fail to lock HW. 1: Success to lock HW.
- * @out.base: PA of PDMA Reg base.
- * @out.ringbuf: PA of ring buffer base
- * @out.size: Size of ring buffer (bytes)
- * @out.hw_sem_base: PA of hw semaphore base
- * @out.hw_sem_offset: Offset from hw semaphore base.
- * @out.gid_list_discardable: available discardable GID list for user
- *    with bit-mask expression. e.q. 0xE mean GID 1 to 3 is available.
- *		Support up to 64 GIDs and share with non-discardable list.
- * @out.gid_list_non_disc: available non-discardable GID list for user
- *    with bit-mask expression. Mutally exclusive with gid_list_discardable.
- * @out.sw_ver: Indicate sw version by kernel driver
+ * struct pdma_device       Object representing an instance of PDMA device,
+ *                          allocated from the probe method of pdma driver.
+ *
+ * @dev:                    Pointer to the kernel's representation of the
+ *                          PDMA platform device.
+ * @ctx_list:               A gloal list of existing ccmd_context instances.
+ * @pdma_device_lock:       Protected critical section related to HW lock.
+ * @ccmd_locked_ctx_id:     CCMD context ID which has been used. Bit-wise.
+ * @reg_base:               Base address of CCMD register.
+ * @reg_region:             Range of CCMD register.
+ * @hw_sem_base:            PA of hw semaphore base.
+ * @hw_sem_offset:          Offset from hw semaphore base.
+ * @hw_sem_bit:             Bit shifter of HW semaphore. Set by dts.
+ * @pdma_sram_base:         PA of PDMA SRAM base
+ * @pdma_reg_base_kva:      Kernel virtual address of CCMD base address.
+ * @pdma_hw_sem_base_kva:   Kernel virtual address of CCMD HW semaohore.
+ * @pdma_sram_base_kva:     Kernel virtual address of SRAM base.
+ * @page_order:             4k-based page_order. e.q. 3 for 8 4k-alinged pages.
+ * @pdma_sram_base:         PA of PDMA SRAM base.
+ * @config_mode:            Init CCMD by AP or mirco processor. Set by dts.
+ * @dynamic_mode:           Backup and restore dynamic policy to SRAM.
+ * @max_ctx_cnt:            Maximum supported CCMD context count.
+ * @max_non_api_ctx_cnt:    Maximum supported count for non-Cache-API context.
+ *                          Should be less than or equal to max_ctx_cnt.
+ * @non_api_ctx_cnt:        Number of non-api contexts that have required HW lock
+ * @sw_version:             To identify specific SW version.
+ * @extended_pbha_bits:     Number of bits used for extended PBHA ID
+ */
+
+struct pdma_device {
+	struct device *dev;
+	struct list_head ctx_list;
+	struct mutex pdma_device_lock;
+	struct list_head extened_pbha_pool;
+	u32 ccmd_locked_ctx_id;
+	u64 reg_base;
+	u64 reg_region;
+	u64 hw_sem_base;
+	u64 hw_sem_offset;
+	u32 hw_sem_bit;
+	u64 pdma_sram_base;
+	void __iomem *pdma_reg_base_kva;
+	void __iomem *pdma_hw_sem_base_kva;
+	struct pdma_sram *pdma_sram_base_kva;
+	u32 page_order; /* g_page_order is 4k-based */
+	u32 config_mode;
+	s32 dynamic_mode;
+	u8 max_ctx_cnt;
+	u8 max_non_api_ctx_cnt;
+	u8 non_api_ctx_cnt;
+	u8 sw_version;
+	u8 extended_pbha_bits;
+};
+
+
+/**
+ * struct ccmd_context -    Object representing an instance of CCMD context,
+ *                          allocated on user calls lock HW.
+ *
+ * @pdma_dev:               Pointer to global pdma device struct.
+ * @entry:                  Link to the global ctx list from struct pdma_device
+ * @pbha_list               Record PBHA ID requested by the ccmd context
+ * @kctx_id:                kbase ctx (vk device) that lock the cid.
+ * @cid:                    Unique CCMD context ID
+ * @ringbuf_paddr:          PA of the ring buffer for the context
+ * @ringbuf_paddr:          VA of the ring buffer for the context
+ * @cid_reg_base:           Context view of CCMD register base
+ * @mode:                   Use smart cache API or others. Refer to CCMD_CACHE_MODE.
+ */
+
+struct ccmd_context {
+	struct pdma_device *pdma_dev;
+	struct list_head entry;
+	struct list_head pbha_list;
+	u32 kctx_id;
+	u32 cid;
+	u64 ringbuf_paddr;
+	u64 ringbuf_vaddr;
+	u64 cid_reg_base;
+	u32 mode;
+};
+
+/**
+ * struct extended_pbha -   Object represnting an instance of an extended PBHA.
+ *
+ * @entry                   Link to the global PBHA pool from struct pdma_device
+ * @id                      Extened PBHA ID.
+ */
+
+struct extended_pbha {
+	struct list_head entry;
+	u32 id;
+};
+
+
+/**
+ * @in:                     Input parameters
+ * @in.kctx_id:             Kbase context ID that locks HW.
+ * @in.mode:                API mode or others (ex. Compute-TLS).
+ * @out:                    Output parameters
+ * @out.status: 0:          Fail to lock HW. 1: Success to lock HW.
+ * @out.base:               PA of CCMD Reg base.
+ * @out.region_size:        Size of CCMD register region
+ * @out.ringbuf:            PA of ring buffer base
+ * @out.size:               Size of ring buffer (bytes)
+ * @out.hw_sem_base:        PA of hw semaphore base
+ * @out.hw_sem_offset:      Offset from hw semaphore base.
+ * @out.sw_ver:             Software version for specific HW configuration
+ * @out.cid:                CCMD Context ID. Supported cid is from 0 to 3.
+ * @out.debug_mode:         For debugging propose only.
  */
 
 struct pdma_hw_lock {
@@ -39,9 +141,9 @@ struct pdma_hw_lock {
 		unsigned int size;
 		unsigned long hw_sem_base;
 		unsigned int hw_sem_offset;
-		unsigned long long gid_list_discardable;
-		unsigned long long gid_list_non_disc;
 		unsigned int sw_ver;
+		unsigned int cid;
+		bool debug_mode;
 	} out;
 };
 
@@ -79,5 +181,7 @@ struct pdma_sram {
 
 /* Export function */
 void pdma_lock_reclaim(u32 kctx_id);
+u32 pdma_request_extended_pbha(u32 kctx_id);
+void pdma_release_extended_pbha(u32 kctx_id, u32 pbha_id);
 
 #endif /* GPU_PDMA_H */
