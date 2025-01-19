001241b3fca5903dc9f99358e59b6625be269646
mtk32733 <eric.chien@mediatek.com>
2025-01-19 09:57:49 +0800

    [ALPS09589946] vmm: jayer: ap add cvfs usr
    [Description]
    1. Fix cvfs usr api
    
    [Test]
    Pass.
    
    MTK-Commit-Id: 90ca2735afda8a3855e817c0143f95ce6b475e46
    
    CR-Id: ALPS09589946
    Feature: VMM
    Signed-off-by: mtk32733 <eric.chien@mediatek.com>
    Change-Id: I0b2acb770218d73918239c181a9a286b8e64b66b

--- drivers/misc/mediatek/include/mt-plat/mtk-vmm-notifier.h
+++ drivers/misc/mediatek/include/mt-plat/mtk-vmm-notifier.h
@@ -14,17 +14,39 @@
  */
 
 enum VMM_CVFS_SEL_ID {
-    VMM_CVFS_SEL_ID_START,
-    VMM_CVFS_CAM_SEL = VMM_CVFS_SEL_ID_START,
-    VMM_CVFS_IMG_SEL,
-    VMM_CVFS_IPE_SEL,
-    VMM_CVFS_VDE_SEL,
-    VMM_CVFS_SEL_ID_END,
+	VMM_CVFS_SEL_ID_START,
+	VMM_CVFS_CAM_SEL = VMM_CVFS_SEL_ID_START,
+	VMM_CVFS_IMG_SEL,
+	VMM_CVFS_IPE_SEL,
+	VMM_CVFS_VDE_SEL,
+	VMM_CVFS_SEL_ID_END,
+};
+
+enum {
+	CVFS_NULL_POINTER = -5,
+	CVFS_UPDATE_EXCEPTION = -4,
+	CVFS_INVALID_PARAM = -3,
+	CVFS_NEGATIVE_OVERFLOW = -2,
+	CVFS_POSITIVE_OVERFLOW = -1,
+	CVFS_UPDATE_SUCCESS = 0,
+	CVFS_ENABLE_TOGGLE = 1,
+	CVFS_DISABLE_TOGGLE = 2,
+};
+
+enum VMM_CVFS_USR_ID {
+	VMM_CVFS_USR_START,
+	VMM_CVFS_USR_CAMSYS = VMM_CVFS_USR_START,
+	VMM_CVFS_USR_IMGSYS,
+	VMM_CVFS_USR_PDA,
+	VMM_CVFS_USR_SENINF,
+	VMM_CVFS_USR_UISP,
+	VMM_CVFS_USR_VDE,
+	VMM_CVFS_USR_MAX,
 };
 
 int vmm_isp_ctrl_notify(int openIsp);
-int vmm_enable_cvfs(int user_id, enum VMM_CVFS_SEL_ID vmm_cvfs_sel_id);
-int vmm_disable_cvfs(int user_id, enum VMM_CVFS_SEL_ID vmm_cvfs_sel_id);
+int vmm_enable_cvfs(enum VMM_CVFS_USR_ID user_id, enum VMM_CVFS_SEL_ID vmm_cvfs_sel_id);
+int vmm_disable_cvfs(enum VMM_CVFS_USR_ID user_id, enum VMM_CVFS_SEL_ID vmm_cvfs_sel_id);
 int vmm_cvfs_dump(void);
 
 #endif
