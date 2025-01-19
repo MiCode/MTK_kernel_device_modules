40a8749bb276c3fc583386340e546399e5e48387
mtk21986 <leo-yl.liu@mediatek.com>
2025-01-19 09:00:48 +0800

    [ALPS08733231] Fix api dump by gpu
    [Description]
    remove policy_mode chack
    
    [Test]
    monkey
    
    MTK-Commit-Id: 813d095dc54b623c68135381b70671616b170bc7
    
    CR-Id: ALPS08733231
    Feature: GPU DVFS Policy
    Change-Id: I3b5dcf97da3bb53001f6d02a9aecb6b065b81692
    Signed-off-by: mtk21986 <leo-yl.liu@mediatek.com>

--- drivers/gpu/mediatek/ged/src/ged_dvfs.c
+++ drivers/gpu/mediatek/ged/src/ged_dvfs.c
@@ -4276,9 +4276,6 @@ GED_ERROR ged_dvfs_system_init(void)
 		get_min_oppidx = ged_get_min_oppidx();
 		GED_LOGI("dts support gpueb dvfs v1, min_oppidx=%u", get_min_oppidx);
 		mtk_set_fastdvfs_mode(POLICY_MODE);
-		if (ged_get_eb_default_policy_mode() != POLICY_MODE)
-			aee_kernel_exception("GPU DVFS", "EB policy mode mismatch: AP_set(%u) EB_default(%u)",
-				POLICY_MODE, ged_get_eb_default_policy_mode());
 	} else
 		GED_LOGI("dts not support gpueb dvfs");
 
