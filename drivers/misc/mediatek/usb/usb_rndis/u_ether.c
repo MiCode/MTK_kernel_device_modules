dac480c1f090fc59c633c42450b2b9888d420395
Richard Acayan <mailingradian@gmail.com>
2025-01-19 09:08:58 +0800

    [ALPS09490234] rndis: sync upstream patch 2024/10/25-8
    [Description]
    usb: gadget: u_ether: Re-attach netif device to mirror detachment
    
    In 6.7-rc1, there was a netif_device_detach call added to the
    gether_disconnect function. This clears the __LINK_STATE_PRESENT bit of
    the netif device and suppresses pings (ICMP messages) and TCP connection
    requests from the connected host. If userspace temporarily disconnects
    the gadget, such as by temporarily removing configuration in the gadget
    configfs interface, network activity should continue to be processed
    when the gadget is re-connected. Mirror the netif_device_detach call
    with a netif_device_attach call in gether_connect to fix re-connecting
    gadgets.
    
    Link: https://gitlab.com/postmarketOS/pmaports/-/tree/6002e51b7090aeeb42947e0ca7ec22278d7227d0/main/postmarketos-base-ui/rootfs-usr-lib-NetworkManager-dispatcher.d-50-tethering.sh
    Cc: stable <stable@kernel.org>
    Fixes: ("usb: gadget: u_ether: Replace netif_stop_queue with netif_device_detach")
    [Test]
    USB RNDIS test
    
    MTK-Commit-Id: 0a0f129b5ffc377b8088d27a055d0dbe109fcd68
    
    Signed-off-by: Richard Acayan <mailingradian@gmail.com>
    Tested-by: Luca Weiss <luca@z3ntu.xyz>
    Tested-by: Duje Mihanović <duje.mihanovic@skole.hr>
    Link: https://lore.kernel.org/r/20231218164532.411125-2-mailingradian@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Chih-Chun Wang <Chih-Chun.Wang@mediatek.com>
    CR-Id: ALPS09490234
    Feature: usb_ssusb
    Change-Id: I27a5e314899a3704260bf89dbd43a649366ee423

--- drivers/misc/mediatek/usb/usb_rndis/u_ether.c
+++ drivers/misc/mediatek/usb/usb_rndis/u_ether.c
@@ -1586,6 +1586,8 @@ struct net_device *mtk_gether_connect(struct gether *link)
 		if (netif_running(dev->net))
 			eth_start(dev, GFP_ATOMIC);
 
+		netif_device_attach(dev->net);
+
 	/* on error, disable any endpoints  */
 	} else {
 		(void) usb_ep_disable(link->out_ep);
