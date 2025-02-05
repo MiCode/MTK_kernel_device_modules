f8370ea14d476feeb5a002172f41676b7b1ead87
Easer Su <easer.su@mediatek.com>
2025-02-05 08:34:02 +0800

    [ALPS09486326] mt6993: ccmd: refine ccmd macro
    [Description]
    refine build macro
    [Test]
    Boot pass. CCMD UT
    
    MTK-Commit-Id: 518cb15d23087957776d28ea6cfec5dcaa93c96a
    
    Signed-off-by: Easer Su <easer.su@mediatek.com>
    CR-Id: ALPS09486326
    Feature: MFGSYS driver
    Change-Id: I27258dcd2a754aedd121257a0cf40ff5388ad8e5

--- drivers/gpu/mediatek/gpu_pdma/gpu_pdma_mt6993.c
+++ drivers/gpu/mediatek/gpu_pdma/gpu_pdma_mt6993.c
@@ -54,7 +54,7 @@
 #define CCMD_RESERVED_PBHA_NUM		5
 #define CCMD_UNSUPPORTED_PBHA_ID	0
 #define CCMD_DEBUG_MODE				0
-#define JAYER_HW_SUPPORT			1
+#define CCMD_V2_SUPPORT			1
 
 /* variables */
 static struct pdma_device *g_pdma_dev;
@@ -238,7 +238,7 @@ static void __ccmd_reset_hw(struct pdma_device *pdma_dev,
 	struct ccmd_context *ccmd_ctx)
 {
 //	int *ringbuf_pa0_setting_l;
-#if JAYER_HW_SUPPORT
+#if CCMD_V2_SUPPORT
 	unsigned int pdma_status;
 	unsigned int poll_timeout = 1000;
 #endif
@@ -252,7 +252,7 @@ static void __ccmd_reset_hw(struct pdma_device *pdma_dev,
 //	ringbuf_pa0_setting_l = pdma_dev->pdma_reg_base_kva +
 //		CIDX_RING_BUFFER_PA_0_L(ccmd_ctx->cid);
 
-#if JAYER_HW_SUPPORT
+#if CCMD_V2_SUPPORT
 	/*ch0 is for initializing custom command buffer process.*/
 	/* Disable and poll Ch0*/
 	writel((readl(pdma_dev->pdma_reg_base_kva) & 0xFFFDFFFF),
@@ -271,7 +271,7 @@ static void __ccmd_reset_hw(struct pdma_device *pdma_dev,
 	/* CCB */
 	writel(0x3, (pdma_dev->pdma_reg_base_kva + CCMD_RING_BUFFER_CONTROL));
 
-	/* Jayer CID_COMMAND */
+	/* CID_COMMAND */
 	writel((ccmd_ctx->cid << 8) | (0x3 << 10),
 		pdma_dev->pdma_reg_base_kva + CCMD_CID_COMMAND);
 	/* release GID command of the CID, and reset hrptr&hwptr */
@@ -309,23 +309,9 @@ static int init_ccmd_hw(struct pdma_device *pdma_dev,
 	if (ccmd_power_control(CCMD_POWER_ON))
 		return 1;
 
-#if JAYER_HW_SUPPORT
-	/* Jayer CCMD mode */
+#if CCMD_V2_SUPPORT
+	/* CCMD mode */
 	writel(0x423B8000, pdma_dev->pdma_reg_base_kva);
-
-	/* Jayer Zombie_IRQ_CONTROL */
-	writel(0x90000200, pdma_dev->pdma_reg_base_kva + 0x6C);
-
-	/* Jayer SLC Policy Attr */
-	writel(0x400, (pdma_dev->pdma_reg_base_kva + 0x300));
-	writel(0x402, (pdma_dev->pdma_reg_base_kva + 0x304));
-	writel(0x406, (pdma_dev->pdma_reg_base_kva + 0x308));
-	writel(0x412, (pdma_dev->pdma_reg_base_kva + 0x30C));
-	writel(0x800, (pdma_dev->pdma_reg_base_kva + 0x310));
-	writel(0x840, (pdma_dev->pdma_reg_base_kva + 0x314));
-	writel(0x8c0, (pdma_dev->pdma_reg_base_kva + 0x318));
-	writel(0x000, (pdma_dev->pdma_reg_base_kva + 0x31C));
-	writel(0xCC2, (pdma_dev->pdma_reg_base_kva + 0x320));
 #endif
 
 	for (page_num = 0; page_num < (1 << pdma_dev->page_order); page_num++) {
@@ -1026,7 +1012,7 @@ static int gpu_pdma_probe(struct platform_device *pdev)
 	INIT_LIST_HEAD(&g_pdma_dev->ctx_list);
 	mutex_init(&g_pdma_dev->pdma_device_lock);
 
-#if JAYER_HW_SUPPORT & CCMD_DEBUG_MODE
+#if CCMD_V2_SUPPORT & CCMD_DEBUG_MODE
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 1);
 #else
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
