2be8be078b5c5a361705e5406498e22d8edb1799
pratham.chaudhary <pratham.chaudhary@mediatek.com>
2025-02-25 11:40:25 +0800

    [ALPS09586319] clkbuf: clkbuf porting k612
    [Description]
    porting for k6.12
    
    [Test]
    build pass
    
    MTK-Commit-Id: 4fdc5a8eef7fdc7899f987706a021fb270eaa542
    
    Change-Id: I5efebe67b2fb1ace59ff76b63f70be9a600a58fc
    Signed-off-by: pratham.chaudhary <pratham.chaudhary@mediatek.com>
    CR-Id: ALPS09586319
    Feature: CLKMGR_Kernel

--- drivers/misc/mediatek/clkbuf/src/clkbuf-ctrl.c
+++ drivers/misc/mediatek/clkbuf/src/clkbuf-ctrl.c
@@ -58,6 +58,7 @@ static const struct of_device_id clkbuf_of_match[] = {
 	{ .compatible = "mediatek,mt6768-clkbuf" },
 	{ .compatible = "mediatek,mt6789-clkbuf" },
 	{ .compatible = "mediatek,mt6833-clkbuf" },
+	{ .compatible = "mediatek,mt6855-clkbuf" },
 	{ .compatible = "mediatek,mt6877-clkbuf" },
 	{ .compatible = "mediatek,mt6885-clkbuf" },
 	{ .compatible = "mediatek,mt6897-clkbuf" },
