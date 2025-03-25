085e793bd60178f35a01a1e8bb98945d4881c456
mtk32995 <piyush.jaiswal@mediatek.com>
2025-03-25 11:24:40 +0800

    [ALPS09398040]  use CONFIG_DEVICE_MODULES_DRM_MEDIATEK
    [Description]
    Latest code use CONFIG_DEVICE_MODULES_DRM_MEDIATEK
    replace CONFIG_DRM_MEDIATEK in kernel-6.12
    
    [Test]
    Boot Up
    
    MTK-Commit-Id: 16b885d2824757bfddd0a8d26966a5ffcabe4e65
    
    Signed-off-by: mtk32995 <piyush.jaiswal@mediatek.com>
    CR-Id: ALPS09398040
    Feature: touch_driver
    Change-Id: I0af191487bfc8a809c5b83a51e39556392647fc5

--- drivers/input/touchscreen/GT9886/goodix_ts_core.h
+++ drivers/input/touchscreen/GT9886/goodix_ts_core.h
@@ -42,7 +42,7 @@
 #include <linux/regulator/consumer.h>
 #endif
 
-#if IS_ENABLED(CONFIG_DRM_MEDIATEK)
+#if IS_ENABLED(CONFIG_DEVICE_MODULES_DRM_MEDIATEK)
 #include "mtk_panel_ext.h"
 #include "mtk_disp_notify.h"
 #endif
@@ -456,7 +456,7 @@ struct goodix_ts_core {
 	struct notifier_block ts_notifier;
 	struct goodix_ts_esd ts_esd;
 
-#if IS_ENABLED(CONFIG_DRM_MEDIATEK)
+#if IS_ENABLED(CONFIG_DEVICE_MODULES_DRM_MEDIATEK)
 	struct notifier_block disp_notifier;
 #endif
 };
