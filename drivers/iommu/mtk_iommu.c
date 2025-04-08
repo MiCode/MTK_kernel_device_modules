a0da622eeabc5256295daf2411cec6663c653f7d
Qiming Sun <qiming.sun@mediatek.com>
2025-04-08 22:52:30 +0800

    [ALPS09786545] iommu: add an IOMMU_IDENTITIY_DOMAIN
    [Description]
    This brings back the ops->detach_dev() code that commit
    1b932ceddd19 ("iommu: Remove detach_dev callbacks") deleted and turns it
    into an IDENTITY domain.
    
    Reviewed-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Jerry Snitselaar <jsnitsel@redhat.com>
    Signed-off: Jason Gunthorpe <jgg@nvidia.com>
    Link: https://lore.kernel.org/r/18-v8-81230027b2fa+9d-iommu_all_defdom_jgg@nvidia.com
    Signed-off: Joerg Roedel <jroedel@suse.de>.
    
    [Test]
    Check whether phone can boot to HS.
    
    MTK-Commit-Id: 575d215462d8b163c0eca30c588696d08ca28c98
    
    Signed-off-by: Qiming Sun <qiming.sun@mediatek.com>
    CR-Id: ALPS09786545
    Feature: iommu_kernel
    Change-Id: I8f46839f43531e9dadfb5ce322744b056b61f776

--- drivers/iommu/mtk_iommu.c
+++ drivers/iommu/mtk_iommu.c
@@ -1867,6 +1867,34 @@ out_unlock:
 	return ret;
 }
 
+static int mtk_iommu_identity_attach(struct iommu_domain *identity_domain,
+				     struct device *dev)
+{
+	struct iommu_domain *domain = iommu_get_domain_for_dev(dev);
+	struct mtk_iommu_data *data;
+
+	if (domain == identity_domain || !domain)
+		return 0;
+
+	data = dev_iommu_priv_get(dev);
+	if (!data) {
+		dev_info(dev, "Get iommu data fail\n");
+		return -EINVAL;
+	}
+
+	mtk_iommu_config(data, dev, false, 0);
+	return 0;
+}
+
+static struct iommu_domain_ops mtk_iommu_identity_ops = {
+	.attach_dev = mtk_iommu_identity_attach,
+};
+
+static struct iommu_domain mtk_iommu_identity_domain = {
+	.type = IOMMU_DOMAIN_IDENTITY,
+	.ops = &mtk_iommu_identity_ops,
+};
+
 static int mtk_iommu_map(struct iommu_domain *domain, unsigned long iova,
 			phys_addr_t paddr, size_t pgsize, size_t pgcount,
 			int prot, gfp_t gfp, size_t *mapped)
@@ -2182,6 +2210,7 @@ static void mtk_iommu_get_resv_regions(struct device *dev,
 }
 
 static const struct iommu_ops mtk_iommu_ops = {
+	.identity_domain = &mtk_iommu_identity_domain,
 	.domain_alloc_paging = mtk_iommu_domain_alloc_paging,
 	.probe_device	= mtk_iommu_probe_device,
 	.release_device	= mtk_iommu_release_device,
