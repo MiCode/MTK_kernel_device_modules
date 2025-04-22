69d67e3af6d448d63b3754d31adce27b0b5a7cae
victor-wc.lin <victor-wc.lin@mediatek.com>
2025-04-22 09:59:42 +0800

    [ALPS09864649] interconnect: mediatek: emi: add mt6858 dvfsrc
    [Description]
    Add support of mt6858 dvfsrc interconnect driver
    
    [Test]
    Build pass
    
    MTK-Commit-Id: 8a3fe5938837d4603068427e185b8d18199e168d
    
    Signed-off-by: victor-wc.lin <victor-wc.lin@mediatek.com>
    CR-Id: ALPS09864649
    Feature: VCORE DVFS
    Change-Id: I1ecb27984397c45bd37390ec894455d76198069e

--- drivers/interconnect/mediatek/mtk-emi.c
+++ drivers/interconnect/mediatek/mtk-emi.c
@@ -214,6 +214,7 @@ static const struct of_device_id emi_icc_of_match[] = {
 	{ .compatible = "mediatek,mt6781-dvfsrc", .data = &mt6873_icc },
 	{ .compatible = "mediatek,mt6899-dvfsrc", .data = &mt6873_icc },
 	{ .compatible = "mediatek,mt6993-dvfsrc", .data = &mt6873_icc },
+	{ .compatible = "mediatek,mt6858-dvfsrc", .data = &mt6873_icc },
 	{ },
 };
 MODULE_DEVICE_TABLE(of, emi_icc_of_match);
