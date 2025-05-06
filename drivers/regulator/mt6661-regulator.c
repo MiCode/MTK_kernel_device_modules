4fca7538650c86cd60cfeb0fd328ac3dc2ee2c86
kuan-lin.wang <kuan-lin.wang@mediatek.com>
2025-05-06 15:51:17 +0800

    [ALPS09959403] Revert "pmic: Set UVLO info in cmdline"
    [Description]
    This reverts commit 339e24f4cd8cd70576f99a9646ccb2438d4ea11e.
    Reason for revert: build error
    
    [Test]
    test bypass
    
    MTK-Commit-Id: 36d16d09192001f5fb7cb8b8838a29ba0f507374
    
    Signed-off-by: kuan-lin.wang <kuan-lin.wang@mediatek.com>
    Change-Id: Id352745ebb46109859982c38ba1d93aa3e8a7601
    CR-Id: ALPS09959403
    Feature: pmic_driver

--- drivers/regulator/mt6661-regulator.c
+++ drivers/regulator/mt6661-regulator.c
@@ -899,24 +899,32 @@ static int mt6661_of_parse_cb(struct device_node *np,
 	return 0;
 }
 
-/* Clear UVLO info from LK2 */
+/*
+ * Clear UVLO info from LK2
 static void pmic_clear_uvlo_info(struct regulator_dev *rdev)
 {
-	int ret = 0;
 	unsigned int val = 0;
 
-	ret |= regmap_write(rdev->regmap, MT6661_VRC_CON2, val);
-	ret |= regmap_write(rdev->regmap, MT6661_VRC_CON3, val);
-	if (ret)
-		pr_info("%s: failed to access register.\n", __func__);
+	val = 0x29;
+	regmap_write(rdev->regmap, MT6363_CPSWKEY, val);
+	val = 0x47;
+	regmap_write(rdev->regmap, MT6363_CPSWKEY_H, val);
+
+	val = 0;
+	regmap_write(rdev->regmap, MT6363_CPSDSA31, val);
+	regmap_write(rdev->regmap, MT6363_CPSDSA32, val);
+	regmap_write(rdev->regmap, MT6363_CPSDSA33, val);
+
+	regmap_write(rdev->regmap, MT6363_CPSWKEY, val);
+	regmap_write(rdev->regmap, MT6363_CPSWKEY_H, val);
 }
+*/
 
 static int mt6661_regulator_probe(struct platform_device *pdev)
 {
 	struct regulator_config config = {};
 	struct regulator_dev *rdev;
 	struct mt6661_regulator_info *info;
-	const char *compatible;
 	int i, ret;
 
 	dev_info(&pdev->dev, "%s\n", __func__);
@@ -950,13 +958,9 @@ static int mt6661_regulator_probe(struct platform_device *pdev)
 			continue;
 		}
 	}
-	ret = of_property_read_string(pdev->dev.of_node, "compatible", &compatible);
-	if (ret) {
-		dev_err(&pdev->dev, "Failed to get compatible property, ret=%d\n", ret);
-		return ret;
-	}
-	if (!strcmp(compatible, "mediatek,mt6661-4-regulator"))
-		pmic_clear_uvlo_info(rdev);
+#if 0
+	pmic_clear_uvlo_info(rdev);
+#endif
 
 	return 0;
 }
