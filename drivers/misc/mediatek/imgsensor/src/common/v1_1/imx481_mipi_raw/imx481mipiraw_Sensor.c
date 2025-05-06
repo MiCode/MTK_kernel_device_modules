534617219602707aa75db838a68e9c2d91257109
ot908181 <ot_junjie.tang@mediatek.com>
2025-05-06 15:46:37 +0800

    [ALPS09943513] Cam:Fix its For sensorFusion fail
    [Description]
    Fix mt6781 its For sensorFusion fail
    
    [Test]
    tc29 tc30 pass
    
    MTK-Commit-Id: 7d4351cf1d343336ac3ce439625bedb243ba79e2
    
    Signed-off-by: ot908181 <ot_junjie.tang@mediatek.com>
    CR-Id: ALPS09943513
    Feature: CTS
    Change-Id: I68353705d363d5c6342f1a7c344fefe3329b37b7

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/imx481_mipi_raw/imx481mipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/imx481_mipi_raw/imx481mipiraw_Sensor.c
@@ -2223,6 +2223,8 @@ static kal_uint32 feature_control(MSDK_SENSOR_FEATURE_ENUM feature_id,
 		if (IS_MT6893(g_platform_id) ||
 			IS_MT6885(g_platform_id) || IS_MT6877(g_platform_id))
 			*(MUINT32 *)(uintptr_t)(*(feature_data + 1)) = 1500000;
+		else if (IS_MT6789(g_platform_id))
+			*(MUINT32 *)(uintptr_t)(*(feature_data + 1)) = 531400;
 		break;
 	case SENSOR_FEATURE_GET_PERIOD_BY_SCENARIO:
 		switch (*feature_data) {
