033cff1d238113d207f10baa2c5b064cb8579c77
mtk29394 <naman.arora@mediatek.com>
2025-05-16 12:21:45 +0800

    [ALPS10001385] disp: mt6855: round corner enable
    [Description]
    Enable round corner params from panel
    
    [Test]
    Boot to HS
    VP pass
    Camera Pass
    
    MTK-Commit-Id: 4fd9b5b87e78c927af16f4d03c0cea15ec7a53ce
    
    Signed-off-by: mtk29394 <naman.arora@mediatek.com>
    CR-Id: ALPS10001385
    Feature: display_mobile
    Change-Id: I09a682c589b8abfffc36fc86f97bfac218eab588

--- drivers/gpu/drm/panel/panel-alpha-jdi-nt36672e-vdo-120hz.c
+++ drivers/gpu/drm/panel/panel-alpha-jdi-nt36672e-vdo-120hz.c
@@ -29,6 +29,8 @@
 #include "../mediatek/mediatek_v2/mtk_drm_graphics_base.h"
 #endif
 
+#include "../mediatek/mediatek_v2/mtk_corner_pattern/mtk_data_hw_roundedpattern.h"
+
 #include "../../../misc/mediatek/gate_ic/gate_i2c.h"
 
 /* enable this to check panel self -bist pattern */
@@ -1131,6 +1133,9 @@ static struct mtk_panel_params ext_params = {
 		.hfp = 76,
 		.vfp = 2575,
 	},
+	.corner_pattern_height = ROUND_CORNER_H_TOP,
+	.corner_pattern_tp_size = sizeof(top_rc_pattern),
+	.corner_pattern_lt_addr = (void *)top_rc_pattern,
 };
 
 static struct mtk_panel_params ext_params_90hz = {
@@ -1214,6 +1219,9 @@ static struct mtk_panel_params ext_params_90hz = {
 		.hfp = 76,
 		.vfp = 905,
 	},
+	.corner_pattern_height = ROUND_CORNER_H_TOP,
+	.corner_pattern_tp_size = sizeof(top_rc_pattern),
+	.corner_pattern_lt_addr = (void *)top_rc_pattern,
 };
 
 static struct mtk_panel_params ext_params_120hz = {
@@ -1297,6 +1305,9 @@ static struct mtk_panel_params ext_params_120hz = {
 		.hfp = 76,
 		.vfp = 82,
 	},
+	.corner_pattern_height = ROUND_CORNER_H_TOP,
+	.corner_pattern_tp_size = sizeof(top_rc_pattern),
+	.corner_pattern_lt_addr = (void *)top_rc_pattern,
 };
 
 static int panel_ata_check(struct drm_panel *panel)
@@ -1742,6 +1753,15 @@ static int jdi_probe(struct mipi_dsi_device *dsi)
 	else
 		ctx->gate_ic = value;
 
+	value = 0;
+	ret = of_property_read_u32(dev->of_node, "rc-enable", &value);
+	if (ret < 0)
+		value = 0;
+	else {
+		ext_params.round_corner_en = value;
+		ext_params_90hz.round_corner_en = value;
+		ext_params_120hz.round_corner_en = value;
+	}
 
 	backlight = of_parse_phandle(dev->of_node, "backlight", 0);
 	if (backlight) {
