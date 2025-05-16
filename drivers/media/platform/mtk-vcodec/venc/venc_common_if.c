9033cf743af61877648d59dd43c6522979e81ee2
dipti.sachan <dipti.sachan@mediatek.com>
2025-05-16 12:23:04 +0800

    [ALPS10007392] Fix HEIF encoding failure after taking photos
    [Description]
    Fix HEVC gcesync mode flow
    Set encode flow for gcesync as sync mode
    
    Patch sync from 10295484
    
    [Test]
    Build pass on k69v1_64
    WFD test pass on k69v1_64
    
    MTK-Commit-Id: f02c5d2d424f33ec51d2ab387ec2b35cd8f85d65
    
    Change-Id: Id06c23c76f649ef77f00f60ca44b4a9d0d696b65
    Signed-off-by: dipti.sachan <dipti.sachan@mediatek.com>
    CR-Id: ALPS10007392
    Feature: Video Driver

--- drivers/media/platform/mtk-vcodec/venc/venc_common_if.c
+++ drivers/media/platform/mtk-vcodec/venc/venc_common_if.c
@@ -617,8 +617,12 @@ static int venc_set_param(unsigned long handle,
 
 		break;
 	case VENC_SET_PARAM_PREPEND_HEADER:
+		if (inst->vsi == NULL)
+			return -EINVAL;
 		inst->prepend_hdr = 1;
 		ret = vcu_enc_set_param(&inst->vcu_inst, type, enc_prm);
+		inst->ctx->async_mode = !(inst->vsi->sync_mode);
+		mtk_vcodec_debug(inst, "VENC_SET_PARAM_PREPEND_HEADER: async mode:%d", inst->ctx->async_mode);
 		break;
 	case VENC_SET_PARAM_COLOR_DESC:
 		if (inst->vsi == NULL)
