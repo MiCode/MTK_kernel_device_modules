5d4a86e5fda99eb1a737b4dab668e62d47b99509
Qiming Sun <qiming.sun@mediatek.com>
2025-05-27 09:22:19 +0800

    [ALPS09822132] iommu: mt6858: add iommu flag PGTABLE_PA_35_EN
    [Description]
    Add PGTABLE_PA_35_EN to allow the PA of pgtables support up to bit35.
    
    [Test]
    Check whether can boot to home screen normally.
    
    MTK-Commit-Id: 3b9596868b1fdad0bf638e3fbe0419c7c27aebd0
    
    Signed-off-by: Qiming Sun <qiming.sun@mediatek.com>
    CR-Id: ALPS09822132
    Feature: iommu_kernel
    Change-Id: I599bc0cebc7087386f68f345927525c929852eec

--- drivers/iommu/mtk_iommu.c
+++ drivers/iommu/mtk_iommu.c
@@ -3763,7 +3763,7 @@ static const struct mtk_iommu_plat_data mt6858_data_disp = {
 	.flags          = OUT_ORDER_WR_EN | GET_DOM_ID_LEGACY |
 			  NOT_STD_AXI_MODE | TLB_SYNC_EN |/* IOMMU_SEC_EN |*/
 			  SKIP_CFG_PORT | IOVA_34_EN | SMI_DEV_LINK_SKIP |
-			  HAS_SMI_SUB_COMM | SAME_SUBSYS |/* PGTABLE_PA_35_EN |*/
+			  HAS_SMI_SUB_COMM | SAME_SUBSYS | PGTABLE_PA_35_EN |
 			  PM_OPS_SKIP | PM_DOMAIN_SKIP | IOMMU_CLK_AO_EN,
 	.hw_list        = &mm_iommu_list,
 	.inv_sel_reg    = REG_MMU_INV_SEL_GEN2,
