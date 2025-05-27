b33de7d9f950338ccc69dfe435d2853d692dfeb0
Qiming Sun <qiming.sun@mediatek.com>
2025-05-27 09:25:38 +0800

    [ALPS09822132] iommu: mt6858: disable iommu flag SMI_DEV_LINK_SKIP
    [Description]
    Disable the SMI_DEV_LINK_SKIP for enable smi flow.
    
    [Test]
    Check whether phone can boot to HS.
    
    MTK-Commit-Id: 0f17e9a9ce03da6b5f484b003e411293f9a356a9
    
    Signed-off-by: Qiming Sun <qiming.sun@mediatek.com>
    CR-Id: ALPS09822132
    Feature: iommu_kernel
    Change-Id: Ib596a059ac05a8791f7c0dbef990853ddacbc813

--- drivers/iommu/mtk_iommu.c
+++ drivers/iommu/mtk_iommu.c
@@ -3763,7 +3763,7 @@ static const struct mtk_iommu_plat_data mt6858_data_disp = {
 	.m4u_plat	= M4U_MT6858,
 	.flags          = OUT_ORDER_WR_EN | GET_DOM_ID_LEGACY |
 			  NOT_STD_AXI_MODE | TLB_SYNC_EN | IOMMU_SEC_EN |
-			  SKIP_CFG_PORT | IOVA_34_EN | SMI_DEV_LINK_SKIP |
+			  SKIP_CFG_PORT | IOVA_34_EN |
 			  HAS_SMI_SUB_COMM | SAME_SUBSYS | PGTABLE_PA_35_EN |
 			  PM_OPS_SKIP | PM_DOMAIN_SKIP | IOMMU_CLK_AO_EN,
 	.hw_list        = &mm_iommu_list,
