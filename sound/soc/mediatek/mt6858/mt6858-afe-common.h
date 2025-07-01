5ff43538dae87cdf6217c4bd60335d7a1a7320a7
Harvey Huang <harvey.huang@mediatek.com>
2025-07-01 11:40:36 +0800

    [ALPS10113517] audio: enable USB via Audio SRAM
    [Description]
    Removed the SKIP_SB_USB_OFFLOAD definition from the mt6858-afe-
    common.h file to configure features for the k6858v1_64(_adsp) load.
    This change ensures that the USB offload feature is not skipped during
    the configuration process.
    
    [Test]
    build OK
    
    MTK-Commit-Id: a5597f12c68ab968169a8748e4a1add9704db6d8
    
    Signed-off-by: Harvey Huang <harvey.huang@mediatek.com>
    CR-Id: ALPS10113517
    Feature: USB type C audio offload
    Change-Id: I3fbcb069e28b9964e98251ef7aa2734110f0d72d
    (cherry-pick from 24bcf752c1bfddc7c13e6dec97696e8abc130761)

--- sound/soc/mediatek/mt6858/mt6858-afe-common.h
+++ sound/soc/mediatek/mt6858/mt6858-afe-common.h
@@ -53,7 +53,7 @@
 #define SKIP_SB_PBM
 /* #define SKIP_SB_SMCC */
 /* #define SKIP_SB_ULTRA */
-#define SKIP_SB_USB_OFFLOAD
+/* #define SKIP_SB_USB_OFFLOAD */
 /* #define SKIP_SB_VOW */
 #define SKIP_VIP
 #endif
