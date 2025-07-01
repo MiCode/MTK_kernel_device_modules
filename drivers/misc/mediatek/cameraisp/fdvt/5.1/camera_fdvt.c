b37c26632b2c02daa35d89e2cfa24552a3710dca
Ya-Wen Hsu <Ya-Wen.Hsu@mediatek.com>
2025-07-01 11:39:54 +0800

    [ALPS10033928] FDVT: Remove SECURE_TAG
    [Description]
    Remove SECURE_TAG
    
    [Test]
    FDVT SECURE UT
    
    MTK-Commit-Id: 353b44b183052607327029cf36208772f7b8c795
    
    CR-Id: ALPS10033928
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: Ya-Wen Hsu <Ya-Wen.Hsu@mediatek.com>
    Change-Id: I907e2aa348feab1441d99ad983243e7911933d3d

--- drivers/misc/mediatek/cameraisp/fdvt/5.1/camera_fdvt.c
+++ drivers/misc/mediatek/cameraisp/fdvt/5.1/camera_fdvt.c
@@ -35,9 +35,7 @@
 #include <linux/dma-buf.h>
 #include <soc/mediatek/smi.h>
 #include "linux/soc/mediatek/mtk-cmdq-ext.h"
-#if IS_ENABLED(CONFIG_MTK_CAM_SECURE_FD_SUPPORT)
 #include <cmdq-sec.h>
-#endif
 #include "mtk_heap.h"
 #include <linux/suspend.h>
 #include <linux/rtc.h>
@@ -269,10 +267,8 @@ pr_debug(FDTAG "[%s] " format, __func__, ##args)
 static irqreturn_t isp_irq_fdvt(signed int irq, void *device_id);
 static bool config_fdvt(void);
 static signed int config_fdvt_hw(struct fdvt_config *basic_config);
-#if IS_ENABLED(CONFIG_MTK_CAM_SECURE_FD_SUPPORT)
 static signed int config_secure_fdvt_hw(struct fdvt_config *basic_config,
 							struct FDVT_MEM_RECORD *dmabuf);
-#endif
 static void fdvt_schedule_work(struct work_struct *data);
 static signed int fdvt_dump_reg(void);
 
@@ -1258,7 +1254,6 @@ static inline void fdvt_reset_every_frame(void)
 /*****************************************************************************
  *
  *****************************************************************************/
-#if IS_ENABLED(CONFIG_MTK_CAM_SECURE_FD_SUPPORT)
 static void fdvt_sec_fd2handler(struct fdvt_config *basic_config,
 				 struct FDVT_MEM_RECORD *dmabuf)
 {
@@ -1312,7 +1307,7 @@ static void fdvt_sec_fd2handler(struct fdvt_config *basic_config,
 				dmabuf_to_secure_handle(dmabuf->ImgSrcUV.dmabuf);
 	}
 }
-#endif
+
 static bool config_fdvt_request(signed int req_idx)
 {
 #ifdef FDVT_USE_GCE
@@ -1335,18 +1330,15 @@ static bool config_fdvt_request(signed int req_idx)
 				request->fdvt_frame_status[j] =
 					FDVT_FRAME_STATUS_RUNNING;
 				spin_unlock_irqrestore(spinlock_lrq_ptr, flags);
-				#if IS_ENABLED(CONFIG_MTK_CAM_SECURE_FD_SUPPORT)
+
 				if (request->frame_config[j].FDVT_METADATA_TO_GCE.SecMemType
 					== 3 && request->frame_config[j].FDVT_IS_SECURE)
 					fdvt_sec_fd2handler(
 					&request->frame_config[j], &request->frame_dmabuf[j]);
-				#endif
 
 				if (request->frame_config[j].FDVT_IS_SECURE) {
-					#if IS_ENABLED(CONFIG_MTK_CAM_SECURE_FD_SUPPORT)
 					config_secure_fdvt_hw(
 					&request->frame_config[j], &request->frame_dmabuf[j]);
-					#endif
 				} else
 					config_fdvt_hw(&request->frame_config[j]);
 				spin_lock_irqsave(spinlock_lrq_ptr, flags);
@@ -1395,17 +1387,15 @@ static bool config_fdvt(void)
 						FDVT_FRAME_STATUS_RUNNING;
 					spin_unlock_irqrestore(spinlock_lrq_ptr,
 							       flags);
-					#if IS_ENABLED(CONFIG_MTK_CAM_SECURE_FD_SUPPORT)
+
 					if (request->frame_config[j].FDVT_METADATA_TO_GCE.SecMemType
 						== 3 && request->frame_config[j].FDVT_IS_SECURE)
 						fdvt_sec_fd2handler(
 					&request->frame_config[j], &request->frame_dmabuf[j]);
-					#endif
+
 					if (request->frame_config[j].FDVT_IS_SECURE) {
-						#if IS_ENABLED(CONFIG_MTK_CAM_SECURE_FD_SUPPORT)
 						config_secure_fdvt_hw(
 					    &request->frame_config[j], &request->frame_dmabuf[j]);
-						#endif
 					} else {
 						config_fdvt_hw(
 							&request->frame_config[j]);
@@ -1475,9 +1465,7 @@ static bool config_fdvt(void)
 				request->fdvt_frame_status[j] =
 					FDVT_FRAME_STATUS_RUNNING;
 				if (request->frame_config[j].FDVT_IS_SECURE) {
-					#if IS_ENABLED(CONFIG_MTK_CAM_SECURE_FD_SUPPORT)
 					config_secure_fdvt_hw(&request->frame_config[j]);
-					#endif
 				} else
 					config_fdvt_hw(&request->frame_config[j]);
 				return MTRUE;
@@ -2045,7 +2033,7 @@ static signed int config_fdvt_hw(struct fdvt_config *basic_config)
 	return 0;
 }
 #endif
-#if IS_ENABLED(CONFIG_MTK_CAM_SECURE_FD_SUPPORT)
+
 static void fdvt_tzmp2(struct fdvt_config *basic_config, struct FDVT_MEM_RECORD *dmabuf,
 			struct FDVT_SEC_MetaDataToGCE *dmabuf_metadata)
 {
@@ -2397,7 +2385,7 @@ static signed int config_secure_fdvt_hw(struct fdvt_config *basic_config,
 	return 0;
 }
 #endif
-#endif
+
 
 #ifndef FDVT_USE_GCE
 
@@ -4104,10 +4092,10 @@ static signed int FDVT_open(struct inode *pInode, struct file *pFile)
 	/* Enable clock */
 	fdvt_enable_clock(MTRUE);
 	cmdq_mbox_enable(fdvt_clt->chan);
-	#if IS_ENABLED(CONFIG_MTK_CAM_SECURE_FD_SUPPORT)
+
 	if (fdvt_secure_clt)
 		cmdq_sec_mbox_enable(fdvt_secure_clt->chan);
-	#endif
+
 	fdvt_count = 0;
 	log_dbg("FDVT open clock_enable_count: %d", clock_enable_count);
 	/*  */
@@ -4195,10 +4183,10 @@ static signed int FDVT_release(struct inode *pInode, struct file *pFile)
 	fdvt_sec_dma.handler_first_time = 0;
 
 	cmdq_mbox_disable(fdvt_clt->chan);
-	#if IS_ENABLED(CONFIG_MTK_CAM_SECURE_FD_SUPPORT)
+
 	if (fdvt_secure_clt)
 		cmdq_sec_mbox_disable(fdvt_secure_clt->chan);
-	#endif
+
 	fdvt_enable_clock(MFALSE);
 	log_dbg("FDVT release clock_enable_count: %d", clock_enable_count);
 	/*  */
