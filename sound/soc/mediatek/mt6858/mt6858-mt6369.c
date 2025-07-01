6c49aac08f96479270bcc13d7f31ca07a9efd3ce
mtk32322 <wade.yao@mediatek.com>
2025-07-01 11:44:32 +0800

    [ALPS10118769] audio: add DL to VUL24
    [Description]
    add DL to VUL24, let ap and dsp can get echo data concurrently
    
    [Test]
    ASR Record
    
    MTK-Commit-Id: f15fc2122a6873050312af3f8b5629c2189e3355
    
    Signed-off-by: mtk32322 <wade.yao@mediatek.com>
    CR-Id: ALPS10118769
    Feature: alsa_sound_soc
    Change-Id: I2b37c5eec9dc0d1d085c6987a5f79de50742741a

--- sound/soc/mediatek/mt6858/mt6858-mt6369.c
+++ sound/soc/mediatek/mt6858/mt6858-mt6369.c
@@ -911,6 +911,10 @@ SND_SOC_DAILINK_DEFS(hostless_ul4,
 	DAILINK_COMP_ARRAY(COMP_CPU("Hostless_UL4 DAI")),
 	DAILINK_COMP_ARRAY(COMP_DUMMY()),
 	DAILINK_COMP_ARRAY(COMP_EMPTY()));
+SND_SOC_DAILINK_DEFS(hostless_ul24,
+	DAILINK_COMP_ARRAY(COMP_CPU("Hostless_UL24 DAI")),
+	DAILINK_COMP_ARRAY(COMP_DUMMY()),
+	DAILINK_COMP_ARRAY(COMP_EMPTY()));
 SND_SOC_DAILINK_DEFS(hostless_dsp_dl,
 	DAILINK_COMP_ARRAY(COMP_CPU("Hostless_DSP_DL DAI")),
 	DAILINK_COMP_ARRAY(COMP_DUMMY()),
@@ -1708,6 +1712,13 @@ static struct snd_soc_dai_link mt6858_mt6369_dai_links[] = {
 		.dpcm_capture = 1,
 		.ignore_suspend = 1,
 		SND_SOC_DAILINK_REG(hostless_ul4),
+	},
+	{
+		.name = "Hostless_UL24",
+		.no_pcm = 1,
+		.dpcm_capture = 1,
+		.ignore_suspend = 1,
+		SND_SOC_DAILINK_REG(hostless_ul24),
 	},
 	{
 		.name = "Hostless_DSP_DL",
