9e2ba1d7069922ab1bf5e40eefd0a40486aa1ee6
Roy-fp Wu <roy-fp.wu@mediatek.com>
2025-07-01 11:41:17 +0800

    [ALPS10117490] ACCDET: Add custom wakelock time for mt6858
    [Description]
    1. Add custom accdet wakelock time option.
    2. Set default app-wakelock-time to 1sec.
    
    [Test]
    Build pass
    
    MTK-Commit-Id: c85b75c305b3748fb4f5150305760cfe9b5204fd
    
    Signed-off-by: Roy-fp Wu <roy-fp.wu@mediatek.com>
    CR-Id: ALPS10117490
    Feature: accdet
    Change-Id: I715d8f0136fa7ccc0c9799969c213b61d50ee5de
    (cherry-pick from 065dc207c1851c6e2e01d2df3f02fb78bc7f581c)

--- sound/soc/codecs/mt6369-accdet.c
+++ sound/soc/codecs/mt6369-accdet.c
@@ -56,6 +56,9 @@
 #define EINT_PLUG_IN			(1)
 #define EINT_MOISTURE_DETECTED	(2)
 
+/* Define the minimum allowed wakelock time in ms */
+#define MIN_APP_WAKELOCK_TIME_MS	(1000)
+
 struct mt6369_accdet_data {
 	struct snd_soc_jack jack;
 	struct platform_device *pdev;
@@ -1860,7 +1863,7 @@ static void accdet_work_callback(struct work_struct *work)
 {
 	u32 pre_cable_type = accdet->cable_type;
 
-	__pm_stay_awake(accdet->wake_lock);
+	__pm_wakeup_event(accdet->wake_lock, accdet_dts.app_wakelock_time);
 	check_cable_type();
 
 	mutex_lock(&accdet->res_lock);
@@ -1870,7 +1873,6 @@ static void accdet_work_callback(struct work_struct *work)
 	}
 	mutex_unlock(&accdet->res_lock);
 
-	__pm_relax(accdet->wake_lock);
 }
 
 static void accdet_queue_work(void)
@@ -2448,6 +2450,14 @@ static int accdet_get_dts_data(void)
 		pr_info("Moisture_INT support water_r=%d, int_r=%d\n",
 		     accdet->water_r, accdet->moisture_int_r);
 	}
+
+	ret = of_property_read_u32(node, "app-wakelock-time",
+		&accdet_dts.app_wakelock_time);
+
+	/* make sure app-wakelock-time >= 1sec to avoid known issue */
+	if (ret || (accdet_dts.app_wakelock_time < MIN_APP_WAKELOCK_TIME_MS))
+		accdet_dts.app_wakelock_time = MIN_APP_WAKELOCK_TIME_MS;
+	pr_notice("(%s) app_wakelock_time=%d ms\n", __func__, accdet_dts.app_wakelock_time);
 	return 0;
 }
 
