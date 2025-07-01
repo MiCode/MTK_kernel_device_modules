1092bceb2c028619242bfdc041220d21f7b6af58
howard-hh.chang <howard-hh.chang@mediatek.com>
2025-07-01 11:36:10 +0800

    [ALPS10113966] thermal: Add support for MT6858 virtual tskin
    [Description]
    Added MT6858 compatible entry to the device match table for virtual
    tskin thermal management, enabling proper thermal handling for MT6858.
    
    [Test]
    UT/IT
    
    MTK-Commit-Id: 479a698b1ca18773c0ba667a8582dc07dd9eb7d0
    
    Signed-off-by: howard-hh.chang <howard-hh.chang@mediatek.com>
    CR-Id: ALPS10113966
    Feature: Thermal Management
    Change-Id: I65f142978c55bfb570e28e3bb4bec8a5b8828cc5
    (cherry-pick from 08408f6e523e4731030e0108957fbbccb2719a11)

--- drivers/thermal/mediatek/vtskin_temp.c
+++ drivers/thermal/mediatek/vtskin_temp.c
@@ -383,6 +383,10 @@ static const struct of_device_id vtskin_of_match[] = {
 		.compatible = "mediatek,mt6993-virtual-tskin",
 		.data = (void *)&common_vtskin_data,
 	},
+	{
+		.compatible = "mediatek,mt6858-virtual-tskin",
+		.data = (void *)&common_vtskin_data,
+	},
 	{},
 };
 MODULE_DEVICE_TABLE(of, vtskin_of_match);
