f8ae9d2f0c03d73d86d835dbdd8afe42d076f00a
Shawn Sung <shawn.sung@mediatek.com>
2025-07-01 11:38:29 +0800

    [ALPS10114077] ASoC: Enable SMCC
    [Description]
    Comment out `SKIP_SB_SMCC` to enable SMCC.
    
    [Test]
    Passed Audio SMT.
    
    MTK-Commit-Id: 02334c168fafcd426546e56d2b70a9fcd23a59f7
    
    CR-Id: ALPS10114077
    Feature: alsa_sound_soc
    Signed-off-by: Shawn Sung <shawn.sung@mediatek.com>
    Change-Id: Ia5264ba8ba4fb6ae39b9358cef721dec4ad3f064
    (cherry-pick from 4607208e970a3b1463187f04c7d78d30cb58bb19)

--- sound/soc/mediatek/mt6858/mt6858-afe-common.h
+++ sound/soc/mediatek/mt6858/mt6858-afe-common.h
@@ -51,7 +51,7 @@
 /* #define SKIP_SB_GPIO */
 #define SKIP_SB_OFFLOAD
 #define SKIP_SB_PBM
-#define SKIP_SB_SMCC
+/* #define SKIP_SB_SMCC */
 /* #define SKIP_SB_ULTRA */
 #define SKIP_SB_USB_OFFLOAD
 #define SKIP_SB_VOW
