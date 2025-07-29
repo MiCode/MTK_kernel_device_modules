23471c8568243aa7336b8cb5bb759b5daa5a2194
mtk18404 <ryan.chiu@mediatek.com>
2025-07-29 10:13:32 +0800

    [ALPS10162465] mt6858: disp: Adjust Ultra/ Urgent Threshold
    [Description]
    Adjust Ultra/ Urgent Threshold
    DE Simulation for Legacy CHIP: Factor = 18 / 32
    Ultra Low Threshold: FIFO Time * Factor * 80%
    Ultra High Threshold: FIFO Time * Factor * 90%
    Urgent Low Threshold: FIFO Time * Factor * 50%
    Urgent High Threshold: FIFO Time * Factor * 80%
    
    [Test]
    Self-Test
    
    MTK-Commit-Id: 28809fc192225ef445221a56f69bd8a7584305eb
    
    Signed-off-by: mtk18404 <ryan.chiu@mediatek.com>
    CR-Id: ALPS10162465
    Feature: display_mobile
    Change-Id: I4186fb8556bd16891c136f42790b66f666fa2c6d

--- drivers/gpu/drm/mediatek/mediatek_v2/v1/mtk_dsi.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/v1/mtk_dsi.c
@@ -3124,11 +3124,11 @@ static void mtk_dsi_tx_buf_rw(struct mtk_dsi *dsi)
 		} else {
 			u32 fifo_us = 0;
 
-			fifo_us = DIV_ROUND_UP(buf_con * 1000 * 7, consume_rate * 10);
+			fifo_us = DIV_ROUND_UP(buf_con * 1000 * 18, consume_rate * 32);
 			ultra_lo_fifo_us = DIV_ROUND_UP(fifo_us * 8, 10);
 			ultra_hi_fifo_us = DIV_ROUND_UP(fifo_us * 9, 10);
-			urgent_lo_fifo_us = DIV_ROUND_UP(fifo_us * 4, 10);
-			urgent_hi_fifo_us = DIV_ROUND_UP(fifo_us * 6, 10);
+			urgent_lo_fifo_us = DIV_ROUND_UP(fifo_us * 5, 10);
+			urgent_hi_fifo_us = DIV_ROUND_UP(fifo_us * 8, 10);
 		}
 		ultra_lo_fifo_us = dbg_ultra_low ? dbg_ultra_low : ultra_lo_fifo_us;
 		ultra_hi_fifo_us = dbg_ultra_high? dbg_ultra_high : ultra_hi_fifo_us;
