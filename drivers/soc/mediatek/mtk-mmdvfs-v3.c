64f12efcace44632e5cdc6e8e831ce86f112af6b
Ming-Fan Chen <ming-fan.chen@mediatek.com>
2025-08-26 11:02:57 +0800

    [ALPS10274762] mmdvfs: check idx for adb vcp test
    [Description]
    check idx for adb vcp test
    
    [Test]
    pdas pass
    
    MTK-Commit-Id: 6d749f3ca2954ff7cec204a4aef0500d003cb25d
    
    CR-Id: ALPS10274762
    Feature: mmdvfs
    Signed-off-by: Ming-Fan Chen <ming-fan.chen@mediatek.com>
    Change-Id: I77f03c51e856123a623aaa7be7dcbb0fb2eca1f5

--- drivers/soc/mediatek/mtk-mmdvfs-v3.c
+++ drivers/soc/mediatek/mtk-mmdvfs-v3.c
@@ -1527,9 +1527,15 @@ int mmdvfs_set_vcp_test(const char *val, const struct kernel_param *kp)
 		return -EINVAL;
 	}
 
+	if (func == TEST_SET_RATE)
+		if (idx >= MMDVFS_USER_NUM) {
+			MMDVFS_ERR("func:%hhu invalid idx:%hhu opp:%d", func, idx, opp);
+			return -EINVAL;
+		}
+
 	if (func == TEST_AP_SET_OPP || func == TEST_AP_SET_USER_RATE) {
 		if (idx >= MMDVFS_USER_NUM) {
-			MMDVFS_ERR("invalid idx:%hhu opp:%d", idx, opp);
+			MMDVFS_ERR("func:%hhu invalid idx:%hhu opp:%d", func, idx, opp);
 			return -EINVAL;
 		}
 
