3c08febc85dd4cb055b3c83d36cdd34c7a83a8b2
Walter-zh Wu <walter-zh.wu@mediatek.com>
2025-11-04 15:26:51 +0800

    [ALPS10472700] tmem: fix the upprotection fail
    [Description]
    fix the upprotection fail
    
    [Test]
    boot pass
    
    MTK-Commit-Id: b54bef965855be77a87066ed0c251f5e2f7efad6
    
    Signed-off-by: Walter-zh Wu <walter-zh.wu@mediatek.com>
    CR-Id: ALPS10472700
    Feature: trusted_mem
    Change-Id: Ife57c025584493b8dcb18f53aee36e8189b2e66d

--- drivers/misc/mediatek/trusted_mem/mtee_impl/mtee_ops.c
+++ drivers/misc/mediatek/trusted_mem/mtee_impl/mtee_ops.c
@@ -247,9 +247,10 @@ static int pkvm_mtee_mem_reg_remove(void *peer_data, void *dev_desc)
 		hvc_id = res.a1;
 		if (hvc_id != 0) {
 			ret = pkvm_el2_mod_call(hvc_id, mtee_dev_desc->mtee_chunks_id);
-			if (ret != 0)
+			if (ret != 0) {
 				pr_info("pKVM release reg mem failed:%d\n", ret);
 				return TMEM_MOCK_RELEASE_MEMORY_FAILED;
+			}
 		} else
 			pr_info("%s: hvc is invalid\n", __func__);
 	}
