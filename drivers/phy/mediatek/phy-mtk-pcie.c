d1429975e9345af3802c753d16875d1d883c0980
ot_jingming.jiang <ot_jingming.jiang@mediatek.com>
2025-11-19 12:56:47 +0800

    [ALPS10540844] PCI: phy: Add compatible and phy init
    [Description]
    Add compatible and phy init.
    
    [Test]
    Build pass.
    
    MTK-Commit-Id: 4db75ce1ba346e68bff324f5572e15dccc9ecb06
    
    Signed-off-by: ot_jingming.jiang <ot_jingming.jiang@mediatek.com>
    CR-Id: ALPS10540844
    Feature: pcie
    Change-Id: I6ad19f06b34ec372387888324962802596994efb

--- drivers/phy/mediatek/phy-mtk-pcie.c
+++ drivers/phy/mediatek/phy-mtk-pcie.c
@@ -68,6 +68,26 @@
 #define HIGH_VCO_FREQ			0x0
 
 /* PHY ANA GLB registers */
+#define PEXTP_DIG_LN_TRX_18		0x3018
+#define RG_XTP_GEN1_LTD0_PPATH_DVN	GENMASK(7, 0)
+#define CDR_GEN1_LTD0_PPATH_DVN_TO_6	0x6
+#define RG_XTP_GEN2_LTD0_PPATH_DVN	GENMASK(15, 8)
+#define CDR_GEN2_LTD0_PPATH_DVN_TO_20	0x14
+#define RG_XTP_GEN3_LTD0_PPATH_DVN	GENMASK(23, 16)
+#define CDR_GEN3_LTD0_PPATH_DVN_TO_38	0x2a
+#define RG_XTP_GEN4_LTD0_PPATH_DVN	GENMASK(31, 24)
+#define CDR_GEN4_LTD0_PPATH_DVN_TO_28	0x1c
+
+#define PEXTP_DIG_LN_TRX_1C		0x301c
+#define RG_XTP_GEN1_LTD1_PPATH_DVN	GENMASK(7, 0)
+#define CDR_GEN1_LTD1_PPATH_DVN_TO_24	0x18
+#define RG_XTP_GEN2_LTD1_PPATH_DVN	GENMASK(15, 8)
+#define CDR_GEN2_LTD1_PPATH_DVN_TO_80	0x50
+#define RG_XTP_GEN3_LTD1_PPATH_DVN	GENMASK(23, 16)
+#define CDR_GEN3_LTD1_PPATH_DVN_TO_112	0x70
+#define RG_XTP_GEN4_LTD1_PPATH_DVN	GENMASK(31, 24)
+#define CDR_GEN4_LTD1_PPATH_DVN_TO_112	0x70
+
 #define PEXTP_DIG_LN_TRX_70		0x3070
 #define RG_XTP_LN_FRC_RX_AEQ_DFETP5	BIT(21)
 #define RG_XTP_LN_FRC_RX_AEQ_DFETP4	BIT(29)
@@ -693,6 +713,45 @@ err_probe:
 	return ret;
 }
 
+static int mtk_pcie_phy_init_6881(struct phy *phy)
+{
+	struct mtk_pcie_phy *pcie_phy = phy_get_drvdata(phy);
+
+	mtk_phy_update_bits(pcie_phy->sif_base + PEXTP_DIG_LN_TRX_18,
+			    RG_XTP_GEN1_LTD0_PPATH_DVN,
+			    CDR_GEN1_LTD0_PPATH_DVN_TO_6);
+
+	mtk_phy_update_bits(pcie_phy->sif_base + PEXTP_DIG_LN_TRX_18,
+			    RG_XTP_GEN2_LTD0_PPATH_DVN,
+			    CDR_GEN2_LTD0_PPATH_DVN_TO_20);
+
+	mtk_phy_update_bits(pcie_phy->sif_base + PEXTP_DIG_LN_TRX_18,
+			    RG_XTP_GEN3_LTD0_PPATH_DVN,
+			    CDR_GEN3_LTD0_PPATH_DVN_TO_38);
+
+	mtk_phy_update_bits(pcie_phy->sif_base + PEXTP_DIG_LN_TRX_18,
+			    RG_XTP_GEN4_LTD0_PPATH_DVN,
+			    CDR_GEN4_LTD0_PPATH_DVN_TO_28);
+
+	mtk_phy_update_bits(pcie_phy->sif_base + PEXTP_DIG_LN_TRX_1C,
+			    RG_XTP_GEN1_LTD1_PPATH_DVN,
+			    CDR_GEN1_LTD1_PPATH_DVN_TO_24);
+
+	mtk_phy_update_bits(pcie_phy->sif_base + PEXTP_DIG_LN_TRX_1C,
+			    RG_XTP_GEN2_LTD1_PPATH_DVN,
+			    CDR_GEN2_LTD1_PPATH_DVN_TO_80);
+
+	mtk_phy_update_bits(pcie_phy->sif_base + PEXTP_DIG_LN_TRX_1C,
+			    RG_XTP_GEN3_LTD1_PPATH_DVN,
+			    CDR_GEN3_LTD1_PPATH_DVN_TO_112);
+
+	mtk_phy_update_bits(pcie_phy->sif_base + PEXTP_DIG_LN_TRX_1C,
+			    RG_XTP_GEN4_LTD1_PPATH_DVN,
+			    CDR_GEN4_LTD1_PPATH_DVN_TO_112);
+
+	return 0;
+}
+
 static int mtk_pcie_phy_init_6985(struct phy *phy)
 {
 	struct mtk_pcie_phy *pcie_phy = phy_get_drvdata(phy);
@@ -1309,6 +1368,11 @@ static const struct mtk_pcie_phy_data mt8195_data = {
 	.sw_efuse_supported = true,
 };
 
+static const struct mtk_pcie_phy_data mt6881_data = {
+	.sw_efuse_supported = false,
+	.phy_init = mtk_pcie_phy_init_6881,
+};
+
 static const struct mtk_pcie_phy_data mt6985_data = {
 	.sw_efuse_supported = false,
 	.phy_init = mtk_pcie_phy_init_6985,
@@ -1335,6 +1399,7 @@ static const struct mtk_pcie_phy_data mt6993_data = {
 
 static const struct of_device_id mtk_pcie_phy_of_match[] = {
 	{ .compatible = "mediatek,mt8195-pcie-phy", .data = &mt8195_data },
+	{ .compatible = "mediatek,mt6881-pcie-phy", .data = &mt6881_data },
 	{ .compatible = "mediatek,mt6985-pcie-phy", .data = &mt6985_data },
 	{ .compatible = "mediatek,mt6989-pcie-phy", .data = &mt6989_data },
 	{ .compatible = "mediatek,mt6991-pcie-phy", .data = &mt6991_data },
