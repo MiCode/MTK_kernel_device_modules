d21254e38229722254b1f22c1cac68c76265a6c3
wendy-st.lin <wendy-st.lin@mediatek.com>
2025-11-25 08:41:11 +0800

    [ALPS10447460] mmqos: mt6881: mmqos porting
    [Description]
    mmqos porting for mt6881
    
    [Test]
    mmqos ut
    
    MTK-Commit-Id: fa9057514c34a14c3fa34e87bd18454e6a78b4c1
    
    Signed-off-by: wendy-st.lin <wendy-st.lin@mediatek.com>
    CR-Id: ALPS10447460
    Feature: MMQoS
    Change-Id: I7cf53465287f18c450c823d5107181f21e97177d

--- drivers/misc/mediatek/mmqos/mmqos_wrapper.c
+++ drivers/misc/mediatek/mmqos/mmqos_wrapper.c
@@ -104,6 +104,10 @@ static struct wrapper_data wrapper_data_mt6858 = {
 	.max_ostd = 40,
 	.icc_dst_id = SLAVE_COMMON(0),
 };
+static struct wrapper_data wrapper_data_mt6881 = {
+	.max_ostd = 40,
+	.icc_dst_id = SLAVE_COMMON(0),
+};
 static struct wrapper_data *mmqos_wrapper;
 static struct device *dev;
 static BLOCKING_NOTIFIER_HEAD(hrt_bw_throttle_notifier);
@@ -352,6 +356,10 @@ static const struct of_device_id of_mmqos_wrapper_match_tbl[] = {
 	{
 		.compatible = "mediatek,mt6858-mmqos-wrapper",
 		.data = &wrapper_data_mt6858,
+	},
+	{
+		.compatible = "mediatek,mt6881-mmqos-wrapper",
+		.data = &wrapper_data_mt6881,
 	},
 	{}
 };
