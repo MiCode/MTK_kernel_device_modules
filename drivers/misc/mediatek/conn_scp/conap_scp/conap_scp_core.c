2966745852381f6616f47cbf4e92dabede51f436
Han Liu <han.liu@mediatek.com>
2025-11-25 08:42:50 +0800

    [ALPS10538659] Add SCP feature for 8875
    [Description]
    Add SCP feature for 8875
    
    [Test]
    build pass
    
    MTK-Commit-Id: 37dcc9aba6079901c8d05e99d182c1376dffdca7
    
    Signed-off-by: Han Liu <han.liu@mediatek.com>
    CR-Id: ALPS10538659
    Feature: conn_infra
    Change-Id: I83728de89a087fab413ee2d4244cbe2fccb08ed3

--- drivers/misc/mediatek/conn_scp/conap_scp/conap_scp_core.c
+++ drivers/misc/mediatek/conn_scp/conap_scp/conap_scp_core.c
@@ -62,6 +62,12 @@ const struct of_device_id conn_scp_of_ids[] = {
 		.data = NULL,
 	#endif
 	},
+	{
+		.compatible = "mediatek,mt8875-conn_scp",
+	#if IS_ENABLED(CONFIG_MTK_COMBO_CHIP_CONSYS_8875)
+		.data = NULL,
+	#endif
+	},
 	{}
 };
 
