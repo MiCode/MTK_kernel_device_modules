167338e14b9e4b708f43d85c01295796ee3e6a47
amy zhang <Amy.Zhang@mediatek.com>
2025-12-02 17:50:34 +0800

    [ALPS10686052] disp: fix auto ioctl fail
    [Description]
    fix auto layering rule ioctl fail
    
    [Test]
    test ok
    
    MTK-Commit-Id: 18ce24a9eb13f63ae68479573f1b9dc256430048
    
    Signed-off-by: amy zhang <Amy.Zhang@mediatek.com>
    CR-Id: ALPS10686052
    Feature: display_mobile
    Change-Id: I986fe179b8a88927bd2e8f24d8dec69eb77bb759

--- include/uapi/drm/mediatek_drm.h
+++ include/uapi/drm/mediatek_drm.h
@@ -633,7 +633,11 @@ struct dbi_count_info {
 	__u32 slice_size, slice_num;
 };
 
+#ifdef CONFIG_MTK_AUTO_SUPPORT
+#define LYE_CRTC 6
+#else
 #define LYE_CRTC 4
+#endif
 struct drm_mtk_layering_info {
 	struct drm_mtk_layer_config *input_config[LYE_CRTC];
 	int disp_mode[LYE_CRTC];
